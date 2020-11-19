3b2b7a732c90ca25b7ae5be0e86474bdf081b894
Victor Breno <victor.brns2@gmail.com>
2020-11-19 18:41:17 -0300

    estilização do header

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ function entrar() {
 
 const btnEntrar = document.getElementById('button-login');
 btnEntrar.addEventListener('click', entrar);
+
+function checkForms() { // Exibir uma mensagem "Campos inválidos" dentro do formulário caso pelo menos um campo não esteja preenchido
+  const btnSubmit = document.getElementById('facebook-register')
+  const formsContent = document.querySelector('right-content').children;
+  btnSubmit.addEventListener('click', function () {
+    for (let i = 0; i < formsContent.length; i += 1) {
+      if (formsContent[i].value === '') {
+        formsContent[i].value = "Campos Inválidos"
+      }
+    }
+  })
+}

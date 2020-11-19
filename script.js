36c60a2c0b66ed0744944de3288cc91eebc51e9a
Luiz Reina <luizreeina@gmail.com>
2020-11-19 17:36:19 -0300

    requirements 18 and 20 - forms validation

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
 const buttonLogin = document.querySelector('#button-login');
 const buttonCustomGender = document.querySelector('#Personalizado');
-
+const buttonRegister = document.querySelector('#facebook-register');
 
 buttonLogin.addEventListener('click', () => {
   const emailValue = document.querySelector('#user-email-phone');
   alert(emailValue.value);
 });
 
-function plotInvalid() {
-  const formField = document.querySelector('.create-account');
-  const msgError = document.createElement('p');
-  msgError.textContent = 'Campos inválidos';
-  formField.appendChild(msgError);
-}
-
-buttonCustomGender.addEventListener('click', createCustomGender);
-
 function createCustomGender() {
   const genderCustom = document.querySelector('.hidden-input');
   genderCustom.innerHTML = '<input name="gender-custom" placeholder="Gênero (opcional)" type="text">';
 }
+
+function plotText() {
+  const formField = document.querySelector('.create-account');
+  const formValues = new FormData(formField);
+  const boxText = document.querySelector('.right-content');
+  boxText.innerHTML = '';
+  boxText.innerHTML = `<p> Olá, ${formValues.get('firstname')} ${formValues.get('lastname')}
+  ${formValues.get('phone_email')} ${formValues.get('birthdate')} ${formValues.get('gender')}
+  </p>`;
+}
+
+function validateForm() {
+  let firstName = document.forms["myForm"]["firstname"].value;
+  let lastName = document.forms["myForm"]["lastname"].value;
+  let phoneEmail = document.forms["myForm"]["phone_email"].value;
+  let password = document.forms["myForm"]["password"].value;
+  let birthDate = document.forms["myForm"]["birthdate"].value;
+  let gender = document.forms["myForm"]["gender"].value;
+
+  if (firstName == "" || lastName == "" || phoneEmail == "" || password == "" || birthDate == "" || gender == "") {
+    const formField = document.querySelector('.create-account');
+    const msgError = document.createElement('p');
+    msgError.textContent = 'Campos inválidos';
+    formField.appendChild(msgError);
+  } else {
+    plotText();
+  }
+}
+
+buttonCustomGender.addEventListener('click', createCustomGender);
+buttonRegister.addEventListener('click', validateForm)

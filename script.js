b6f785c93ca1f272e903077b9af6903edffdb959
VitorLimaRios <vitorlimarios999@gmail.com>
2020-11-19 19:29:06 -0300

    corrigindo erros e adicionando requisito 19

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
-const button = document.querySelector('#button-login');
+const buttonLogin = document.querySelector('#button-login');
 
 function formsAlert() {
   const emailOrPhoneNumber = document.querySelector('#user-email-phone').value;
   alert(emailOrPhoneNumber);
 }
 
-button.addEventListener('click', formsAlert);
+buttonLogin.addEventListener('click', formsAlert);
+
+const buttonRegister = document.querySelector('#facebook-register');
+
+function verifyFields() {
+  const firstname = document.querySelector('.firstname').value;
+  const lastname = document.querySelector('.lastname').value;
+  const phone_email = document.querySelector('.phone_email').value;
+  const password = document.querySelector('.password').value;
+  const birthdate = document.querySelector('.birthdate').value;
+  const fields = [firstname, lastname, phone_email, password, birthdate]
+  for (let i = 0; i < fields.length; i += 1) {
+    if (fields[i] === '') {
+      alert('Campos inválidos');
+      break;
+    }
+  }
+}
+
+buttonRegister.addEventListener('click', verifyFields);
+
+const custom = document.querySelector('#custom');
+const inputCustom = document.querySelector('.custom');
+
+function customGender() {
+  if (custom.checked) {
+    inputCustom.style.display = 'contents';
+  } else {
+    inputCustom.style.display = 'none';
+  }
+}
+
+custom.addEventListener('change', customGender)

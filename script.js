522246672c94755622334f0c09aca3171a2745bc
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2020-11-19 20:16:31 -0300

    ate requisito 18

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const registerButton = document.getElementById("facebook-register");
-const InputAll = document.querySelectorAll("#signup-container .input");
+const InputAll = document.querySelectorAll("#signup-container input");
 registerButton.addEventListener("click", function() {
     for (let index in InputAll) {
-        if (InputAll[index].value === "") {
+        if (InputAll[index].value) {
             alert("Campos inválidos");
             return true;
         }

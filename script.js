06576735274bddd239a70f49bb2ca8675633d847
Rafael Greco <rafaelmgreco@hotmail.com>
2020-11-19 19:46:45 -0300

    consertando cc

--- script.js
+++ script.js
@@ -3,23 +3,23 @@ btnFacebookReg.addEventListener('submit', function (evt) {
   evt.preventDefault();
 });
 
-btnRadioPersonalizado = document.getElementsByName('gender')[2];
+const btnRadioPersonalizado = document.getElementsByName('gender')[2];
 btnRadioPersonalizado.addEventListener('click', function () {
   document.getElementsByClassName('hide')[0].style.display = 'inline-block';
 });
-btnRadioFem = document.getElementsByName('gender')[0];
+const btnRadioFem = document.getElementsByName('gender')[0];
 btnRadioFem.addEventListener('click', function () {
   document.getElementsByClassName('hide')[0].style.display = 'none';
 });
-btnRadioMasc = document.getElementsByName('gender')[1];
+const btnRadioMasc = document.getElementsByName('gender')[1];
 btnRadioMasc.addEventListener('click', function () {
   document.getElementsByClassName('hide')[0].style.display = 'none';
 });
 function btnLogin() {
-  let login = document.getElementById('button-logion');
+  const login = document.getElementById('button-logion');
   login.addEventListener('click', function () {
-    let alerta = document.querySelector('#user-email-phone').value;
-    alert(alerta)
+    const alerta = document.querySelector('#user-email-phone').value;
+    alert(alerta);
   });
 }
 

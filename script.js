198a4581afb3452ce3d58c2a2114524388d8cf25
Douglas Fernando <doug.fernando95@gmail.com>
2020-11-19 19:27:58 -0300

    Req 18

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const enterBtn = document.querySelector('.form-control');
 const customGender = document.querySelector('#custom');
+const formInputs = document.querySelectorAll('.signup-input');
+const registerBtn = document.querySelector('#facebook-register');
 
 enterBtn.addEventListener('click', (event) => {
   event.preventDefault();
@@ -11,4 +13,14 @@ enterBtn.addEventListener('click', (event) => {
 customGender.addEventListener('change', () => {
   const genderInput = document.querySelector('.gender-custom');
   genderInput.style.display = 'block';
+});
+
+
+registerBtn.addEventListener('click', (event) => {
+  for(let index = 0; index < formInputs.length; index += 1){
+    if(formInputs[index].value === ''){
+      event.preventDefault();
+      formInputs[index].value = 'Campos inválidos';
+    }
+  }
 });

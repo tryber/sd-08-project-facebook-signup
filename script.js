ec6205a986f41d23275544b1af4563a63cf1efa3
Marcela <marcela9908@gmail.com>
2020-11-19 17:22:34 -0300

    Requisito 8 e correcao cc

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const buttonEntrar = document.querySelector('#button-login');
 const emailOuTelefone = document.querySelector('#user-email-phone');
 
-buttonEntrar.addEventListener("click", function () {
+buttonEntrar.addEventListener('click', function () {
   alert(emailOuTelefone.value);
 });

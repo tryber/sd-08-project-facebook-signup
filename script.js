3e430c3e080a9ef53e0ccf4b0bbab1508490a59c
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2020-11-19 18:56:04 -0300

    adicionando requisito 19

--- script.js
+++ script.js
@@ -6,4 +6,26 @@ function loginButtonAlert() {
   });
 }
 
+function createGenderField() {
+  const dadElement = document.querySelector('#gender-input');
+  const input = document.createElement('input');
+  input.type = 'text';
+  input.name = 'gender-custom';
+  input.id = 'genero-personalizado';
+  input.placeholder = 'Gênero (opcional)';
+  dadElement.appendChild(input);
+}
+
+function checkRadioButtons() {
+  const radioButtons = document.getElementsByName('gender');
+  for (let index = 0; index < radioButtons.length; index += 1) {
+    radioButtons[index].addEventListener('change', function () {
+      if (radioButtons[index].value === 'Personalizado') {
+        createGenderField();
+      }
+    });
+  }
+}
+
 loginButtonAlert();
+checkRadioButtons();

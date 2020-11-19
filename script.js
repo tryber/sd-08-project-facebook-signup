3e275f382cb237d16a68f00791ae9e14a190f4ce
maym4y <jennim.almeida@gmail.com>
2020-11-19 20:00:17 -0300

    Requisito 18 feito

--- script.js
+++ script.js
@@ -5,3 +5,32 @@ function alertaLogin() {
 
 const botaoLogin = document.querySelector('#button-login');
 botaoLogin.addEventListener('click', alertaLogin);
+
+function checaValidacao() {
+  const input = document.querySelectorAll('input');
+  for (let i = 0; i < input.length; i += 1) {
+    if (input[i].value.length === 0) {
+      input[i].setCustomValidity('Campos inválidos');
+    } else {
+      input[i].setCustomValidity('');
+    }
+  }
+}
+
+const submit = document.querySelector('#facebook-register');
+submit.addEventListener('click', checaValidacao);
+
+/* function checaValidacao(e) {
+  let nome = document.getElementById('nome');
+  
+  nome.addEventListener('input', function (event) {
+    if (nome.value === "") {
+      nome.setCustomValidity('Campos inválidos');
+    } else {
+      nome.setCustomValidity('');
+    }
+  })
+}
+
+let submit = document.querySelector('#facebook-register');
+submit.addEventListener('click', checaValidacao); */

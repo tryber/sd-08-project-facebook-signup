06665cdf6362ed4b403733a5058f17576a01e98d
Silvio Dayube <scarige@gmail.com>
2020-11-19 18:26:57 -0300

    Requisito 19

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 const loginButton = document.getElementById('button-login');
 const emailInput = document.getElementById('user-email-phone');
+const customGenderDiv = document.querySelector('.genero-personalizado');
+const customGenderRadio = document.getElementById('Personalizado');
+const femaleGenderRadio = document.getElementById('Feminino');
+const maleGenderRadio = document.getElementById('Masculino');
 
 function alertLogin() {
   alert(emailInput.value);
 }
 
 loginButton.addEventListener('click', alertLogin);
+
+function createCustomGender() {
+  if (!customGenderDiv.children.length) {
+    const genderInput = document.createElement('input');
+    genderInput.name = 'gender-custom';
+    genderInput.placeholder = 'Gênero (opcional)';
+    customGenderDiv.appendChild(genderInput);
+  }
+}
+
+function deleteCustomGender() {
+  if (customGenderDiv.children.length) {
+    const child = customGenderDiv.children[0];
+    customGenderDiv.removeChild(child);
+  }
+}
+
+customGenderRadio.addEventListener('click', createCustomGender);
+femaleGenderRadio.addEventListener('click', deleteCustomGender);
+maleGenderRadio.addEventListener('click', deleteCustomGender);

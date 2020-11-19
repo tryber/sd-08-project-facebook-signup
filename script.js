31d0e4b64c8d7e02202b10ed21167f21d089fef8
Pedro Piquini <piquini.pedro@gmail.com>
2020-11-19 19:29:06 -0300

    Adcionando requistos 10 a 18 e fazendo merge com código do Jessandro

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
+const submitButton = document.getElementById('facebook-register');
+const formFields = ['firstname','lastname','phone_email','password','birthdate','gender'];
+const formValidation = document.forms['cadastro'];
+const personButton = document.getElementById('personalizado');
+const opcionalInput = document.getElementById('campo-opicional');
+
 function sendAlert() {
   alert(document.getElementById('user-email-phone').value);
 }
 
 const getEnter = document.querySelector('#button-login');
 getEnter.addEventListener('click', sendAlert);
+
+submitButton.addEventListener('click', function (event) {
+  for (let i = 0; i < formFields.length; i += 1) {
+    if (formValidation[formFields[i]].value == 0) {
+      formValidation[formFields[i]].value = 'Campos inválidos';
+      event.preventDefault();
+      break
+    }
+  }
+}, false);
+
+personButton.addEventListener('click', function (event) {
+  let campoOpicional = document.createElement('input');
+  campoOpicional.type = 'text';
+  campoOpicional.name = 'gender-custom';
+  campoOpicional.placeholder = 'Gênero (opcional)';
+  opcionalInput.appendChild(campoOpicional);
+}, false);

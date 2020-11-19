864ddb10e3fe3be6f98ac5d14c4c24579a629b7d
Rodrigo de Souza <rdesouza2008@gmail.com>
2020-11-19 17:46:04 -0300

    code-climate

--- script.js
+++ script.js
@@ -3,9 +3,9 @@
 const buttonEntrar = document.querySelector('#button-login');
 
 function addAlert() {
-buttonEntrar.addEventListener('click', function() {
-  const inputEmail = document.querySelector('#user-email-phone');
-  alert(inputEmail.value);
+  buttonEntrar.addEventListener('click', function () {
+    const inputEmail = document.querySelector('#user-email-phone');
+    alert(inputEmail.value);
 });
 }
 addAlert();

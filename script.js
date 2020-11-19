f47d8e0cb225750cfb1bc06a66e649155bbceaca
Daniel <danieldsrd@gmail.com>
2020-11-19 19:30:16 -0300

    code climate

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function alerta() {
   const btn = document.getElementById('button-login');
-  btn.addEventListener('click', function() {
+  btn.addEventListener('click', function () {
     const email = document.getElementById('user-email-phone');
     alert(email.value);
   });

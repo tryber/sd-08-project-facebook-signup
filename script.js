4462b7014711cebe98c199a150f4963712358767
MEianelli <filipelouzano@gmail.com>
2020-11-19 18:21:22 -0300

    Primeira correcao do code climate

--- script.js
+++ script.js
@@ -1,39 +1,13 @@
-let allInputs = document.querySelectorAll('.right-content input');
-
-document.querySelector('#button-login').addEventListener('click', () => {
-  alert(document.querySelector('#user-email-phone').value);
-});
-
-document.querySelector('#facebook-register').addEventListener('click', () => {
-  for (let i = 0; i < allInputs.length - 1; i++) {
-    if (allInputs[i].value === '') {
-      return alert('Campos inválidos');
-    }
-  }
-  mudarRightContainer();
-});
-
-for (let i = 5; i < 7; i++) {
-  allInputs[i].addEventListener('change', () => {
-    if ((allInputs[i].checked = true)) {
-      allInputs[8].style.display = 'none';
-    }
-  });
-}
-allInputs[7].addEventListener('change', () => {
-  if ((allInputs[7].checked = true)) {
-    allInputs[8].style.display = 'block';
-  }
-});
+const allInputs = document.querySelectorAll('.right-content input');
 
 function mudarRightContainer() {
-  let nome = allInputs[0].value;
-  let sobrenome = allInputs[1].value;
-  let email = allInputs[2].value;
-  let data = allInputs[4].value;
-  let genero = document.querySelector('input[type="radio"]:checked').value;
+  const nome = allInputs[0].value;
+  const sobrenome = allInputs[1].value;
+  const email = allInputs[2].value;
+  const data = allInputs[4].value;
+  const genero = document.querySelector('input[type="radio"]:checked').value;
 
-  let rightContainer = document.querySelector('.right-content');
+  const rightContainer = document.querySelector('.right-content');
   rightContainer.innerHTML = `
       <p>Olá, ${nome} ${sobrenome}</p>
       <p>Email: ${email}</p>
@@ -41,3 +15,30 @@ function mudarRightContainer() {
       <p>Genero: ${genero}</p>
   `;
 }
+
+document.querySelector('#button-login').addEventListener('click', () => {
+  alert(document.querySelector('#user-email-phone').value);
+});
+
+document.querySelector('#facebook-register').addEventListener('click', () => {
+  for (let i = 0; i < allInputs.length - 1; i += 1) {
+    if (allInputs[i].value === '') {
+      return alert('Campos inválidos');
+    }
+  }
+  mudarRightContainer();
+  return null;
+});
+
+for (let i = 5; i < 7; i += 1) {
+  allInputs[i].addEventListener('change', () => {
+    if (allInputs[i].checked === true) {
+      allInputs[8].style.display = 'none';
+    }
+  });
+}
+allInputs[7].addEventListener('change', () => {
+  if (allInputs[7].checked === true) {
+    allInputs[8].style.display = 'block';
+  }
+});

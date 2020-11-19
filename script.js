21e56bbc24cfa60db64d8d673b4635c567a1367e
Rodrigo de Souza <rdesouza2008@gmail.com>
2020-11-19 18:11:19 -0300

    resolvendo requisito 7 e 16

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const buttonEntrar = document.querySelector('#button-login');
 function addAlert() {
   buttonEntrar.addEventListener('click', function () {
     const inputEmail = document.querySelector('#user-email-phone');
-    alert(inputEmail.value);
+    alert('Email ou telefone');
   });
 }
 addAlert();

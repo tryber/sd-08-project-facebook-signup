68cc3cd73ddb86e482c45f2768930d46abfb40e4
Felipe Gomes Belarmino <epilefgomes1@gmail.com>
2020-11-19 18:52:25 -0300

    Troquei a ordem dos radio buttons Masculino para Feminino

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-document
-  .getElementById('button-login')
-  .addEventListener('click', function () {
-    alert(alert(document.getElementById('user-email-phone').value));
-  });
+let form = document.getElementById("facebook-login");
+form.addEventListener("submit", validateAndSubmit, false);
+document.getElementById("button-login").addEventListener("click", function () {
+  alert(alert(document.getElementById("user-email-phone").value));
+});

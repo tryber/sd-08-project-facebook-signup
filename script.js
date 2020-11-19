36ecb0dc2cc0d1d8c8005d45683304dc48123c15
Douglas Ferreira <dodofp.df@gmail.com>
2020-11-19 19:33:51 -0300

    Push final do dia

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function alerta() {
   alert("'Email ou telefone'");
 }
 entrar.addEventListener('click', alerta);
+
+const cadastre = document.getElementById('facebook-register')
+
+const form = document.querySelectorAll('.form input div')
+
+cadastre.addEventListener('click', function() {
+  for(let index = 0; index < form.length; index+=1) {
+    if(form[index].innerText === ''){
+      alert('Campos inválidos')
+      break;
+    }else{
+      alert('Cadastro efetuado')
+    }
+  }
+})

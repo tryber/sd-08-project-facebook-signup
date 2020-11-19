a76f37fd214e0dfde4217d5509b6743bbef6460f
siebenei5 <siebenei7@gmail.com>
2020-11-19 20:26:09 -0300

    Test Codeclimate

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const buttonID = document.getElementById('button-login');
 buttonID.addEventListener('click', function () {
   const inputValue = document.getElementById('user-email-phone').value;
 
-    alert(inputValue);
+  alert(inputValue);
 });

00ab74e4a7b8131f8f114bc463eba60359f425dd
victor Farias <victorfarias.tv@gmail.com>
2020-11-19 16:42:09 -0300

    Arrumando codeclimates requisito 7

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const loginButton = document.getElementById('button-login');
 const emailTelefone = document.getElementById('user-email-phone');
 
-loginButton.addEventListener('click', login)
-
 function login() {
   alert(emailTelefone.value);
-};
+}
+
+loginButton.addEventListener('click', login);

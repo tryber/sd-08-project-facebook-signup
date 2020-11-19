ebcd7a949d499436bd3cfee3c49b57b34e6cfc0b
Luis Felipe Avila <lfelipeavila@outlook.com>
2020-11-19 17:11:09 -0300

    resolvendo issues restantes

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const loginButton = document.getElementById('button-login');
-loginButton.addEventListener('click', sendForm);
 
 function sendForm() {
   const emailPhone = document.getElementById('user-email-phone');
   alert(emailPhone.value);
-};
+}
+
+loginButton.addEventListener('click', sendForm);

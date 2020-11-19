df9987f54362ca917793cbc208935ab0abc604ab
Max <gmaxgomes@gmail.com>
2020-11-19 19:23:43 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -7,3 +7,32 @@ function loginButtonAlert() {
 }
 
 loginButtonAlert();
+
+function isEverythingFilled() {
+
+  let registerButton = document.getElementById('facebook-register');
+
+  registerButton.addEventListener('click', function () {
+
+    let caInputList = document.querySelectorAll('.ca-input');
+    let caRadioInputList = document.querySelectorAll('.ca-radio-input');
+
+    for (let index = 0; index < caInputList.length; index += 1) {
+
+      let caInputListItem = caInputList[index];
+
+      if (caInputListItem.value = '') {
+        alert('Campos inválidos');
+      }
+
+    }
+
+    if (caRadioInputList[0].checked == false && caRadioInputList[1].checked == false && caRadioInputList[2].checked == false) {
+      alert('Campos inválidos');
+    }
+
+
+  });
+
+}
+isEverythingFilled();

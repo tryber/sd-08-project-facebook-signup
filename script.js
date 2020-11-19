8d8c2cf652062178c4bfa87cad8023f342cbe5de
antonioarieiro <sfgfffs@gmail.com>
2020-11-19 17:38:41 -0300

    js email ou telefone questao 7

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+  const buttonEnter = document.querySelector('#button-login');
+  buttonEnter.addEventListener('click', function () {
+    const emailPhone = document.querySelector('#user-email-phone');
+    alert(emailPhone.value);
+  });
+}
+

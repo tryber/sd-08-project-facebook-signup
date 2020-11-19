7bbf461ba0af445179e83bc89ec674adfa5c8b26
victor718 <victorfelipeoliveira1@gmail.com>
2020-11-19 17:24:27 -0300

    review codeclimate

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function login (){
-    let getForm = document.querySelector('.facebook-login');
-    getForm.addEventListener('submit', (event) => {
-        event.preventDefault()
-        let emailPhone = getForm.emailPhone.value;
-        console.log(emailPhone)
-        alert(emailPhone);
-    })
-}
+function login() {
+  const getForm = document.querySelector('.facebook-login');
+  getForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let emailPhone = getForm.emailPhone.value;
+    console.log(emailPhone);
+    alert(emailPhone);
+  });
+};
 
 function main() {
     login();
-}
+};
 
 main();

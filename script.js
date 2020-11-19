23de5e8a755200052bd89c4ae322f4db3d57a5df
Adriel Falcao Telito <adriel.telito@pampa.compasso>
2020-11-19 15:56:51 -0300

    Subindo mudanças de oraganização do codigo

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Requisito 7 - Evento alert para o botão entrar
 const buttonLogin = document.getElementById('button-login');
 const loginValue = document.getElementById('user-email-phone');
-//Espaço
+// Espaço
 buttonLogin.addEventListener('click', function () {
-  alert(loginValue.value)
+  alert(loginValue.value);
 });

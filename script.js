7a4ac035c503ba7def20b0f0c9b2fb0fa67a9310
Víctor Cançado <victor.cancado@gmail.com>
2020-11-19 19:26:54 -0300

    Req. 9 - Falta incluir a imagem

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function buttonAlert() {
-    alert("Email ou telefone");
     let buttonClick = document.getElementById("button-login");
-    buttonClick.addEventListener("click", bu)
+    buttonClick.addEventListener("click", buttonClickAlert);
+}
+buttonAlert();
+function buttonClickAlert() {
+    alert("Email ou telefone");
 }

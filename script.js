cf510828c380775274574df512954ec8273c0da0
Bruno Wedley <brunowedley27@gmail.com>
2020-11-19 19:01:43 -0300

    altera id do evento pela const do botão

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const button = document.getElementById('button-login');
 const label = document.getElementById('user-email-phone');
 
 addEventListener('click', (event) => {
-  if (event.target.id === 'button-login') {
+  if (event.target === button) {
     alert(label.value);
   }
 });

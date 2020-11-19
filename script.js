dd30d7df4dee6fce7cfb3a506a9b313478ff8aed
Alvaro Braga <alvaro.braga.job@gmail.com>
2020-11-19 16:37:13 -0300

    Requisite 08

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function alertLogin() {
   const button = document.getElementById('button-login');
   const emailPhone = document.getElementById('user-email-phone');
-  button.addEventListener('click', function() {
+  button.addEventListener('click', function () {
     alert(emailPhone.value);
   });
 }

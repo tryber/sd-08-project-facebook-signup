ce96e2153f72831b57d4fa1e329de1f47f9997c1
Nícolas Godek <nicolasgodek@gmail.com>
2020-11-19 19:26:44 -0300

    tentando resolver problemas no code climate parte 2

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ function buttonLogin() {
   const button = document.querySelector('#button-login');
   button.addEventListener('click', function () {
     const email = document.querySelector('#user-email-phone-label').value;
-    const msg = 'email ou telefone: ' + email;
-    alert(msg);
-  })
+    alert(`Email ou telefone ${email}`);
+  })()
 }
 
 buttonLogin();

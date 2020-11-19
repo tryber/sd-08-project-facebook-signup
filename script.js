b75d9a9df2a7175cf5d98d351b2da816443d9d5a
Seu nome <luiza.dilly@gmail.com>
2020-11-19 17:41:03 -0300

    REQ7 - adiciona funçao ao botao de login e redimensiona

---
+++ script.js
@@ -0,0 +1,7 @@
+const buttonLogin = document.getElementById('button-login');
+
+buttonLogin.addEventListener('click', function(){
+    const emailAndPhone = document.getElementById('user-email-phone').value;
+    alert(emailAndPhone);
+});
+

c8ab2f20fd90a813d7923ff1e4f14a61978b179f
rafasysop <rafaelcostamoura@gmail.com>
2020-11-19 15:46:57 -0300

    resolvendo code climate

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ const btnLogin = document.querySelector('#button-login');
 btnLogin.addEventListener('click', () => {
   if (emailLogin.value !== '') alert(emailLogin.value);
 });
-function erroCampos(var1) {
+function erroCampos() {
   const gender = document.querySelector('input[name="gender"]:checked');
   if (gender === null || gender === undefined) {
     alert('Campos inválidos: Gênero');
   }
 }
-const nome = document.querySelector('#firstname');
-const sobrenome = document.querySelector('#lastname');
-const emailPhone = document.querySelector('#phone_email');
-const novasenha = document.querySelector('#password');
-const dataNascimento = document.querySelector('#birthdate');
-const btnCadastro = document.querySelector('#facebook-register');
-btnCadastro.addEventListener('click', () => {
-  erroCampos(campos);
-});
+// const nome = document.querySelector('#firstname');
+// const sobrenome = document.querySelector('#lastname');
+// const emailPhone = document.querySelector('#phone_email');
+// const novasenha = document.querySelector('#password');
+// const dataNascimento = document.querySelector('#birthdate');
+// const btnCadastro = document.querySelector('#facebook-register');
+// btnCadastro.addEventListener('click', () => {
+
+// });

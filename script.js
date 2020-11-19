7200dc1b08879203b774d0a50ad07a73f58ecb83
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2020-11-19 20:55:57 -0300

    adding requisit 16

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ const botaoLogin = document.getElementById('button-login');
 botaoLogin.addEventListener('click', function () {
   alert('Email ou telefone');
 });
+
+let inputsText = document.querySelectorAll("input");
+console.log(inputsText)

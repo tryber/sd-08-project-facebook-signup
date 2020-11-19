f14210c4102243839029b7982cf2f834396727ad
Aline Debastiani <debastianialine@gmail.com>
2020-11-19 19:15:06 -0300

    cumpre requisito 7

---
+++ script.js
@@ -0,0 +1,8 @@
+fucntion alerta() {
+    let btn = document.getElementById('button-login');
+    btn.addEventListener('click', function() {
+        let email = document.getElementById('user-email-phone');
+        alert(email.value);
+    });
+}
+alerta();

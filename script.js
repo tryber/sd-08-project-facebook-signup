20ea3cb1e31adcbf5b4a892f08c2832aabcae00b
Max <gmaxgomes@gmail.com>
2020-11-19 19:32:17 -0300

    Fix

--- script.js
+++ script.js
@@ -30,17 +30,17 @@ function checkRadioButtons() {
 checkRadioButtons();
 
 function isEverythingFilled() {
-  let registerButton = document.getElementById('facebook-register');
+  const registerButton = document.getElementById('facebook-register');
   registerButton.addEventListener('click', function () {
-    let caInputList = document.querySelectorAll('.ca-input');
-    let caRadioInputList = document.querySelectorAll('.ca-radio-input');
+    const caInputList = document.querySelectorAll('.ca-input');
+    const caRadioInputList = document.querySelectorAll('.ca-radio-input');
     for (let index = 0; index < caInputList.length; index += 1) {
-      let caInputListItem = caInputList[index];
-      if (caInputListItem.value = '') {
+      const caInputListItem = caInputList[index];
+      if (caInputListItem.value === '') {
         alert('Campos inválidos');
       }
     }
-    if (caRadioInputList[0].checked == false && caRadioInputList[1].checked == false && caRadioInputList[2].checked == false) {
+    if (caRadioInputList[0].checked === false && caRadioInputList[1].checked === false && caRadioInputList[2].checked === false) {
       alert('Campos inválidos');
     }
   });

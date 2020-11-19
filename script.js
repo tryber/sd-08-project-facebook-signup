4ec2a2b90477ab9c6ed0e49f2418f86c7aeb4abe
rafasysop <rafaelcostamoura@gmail.com>
2020-11-19 15:31:33 -0300

    resolvendo code climate

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const emailLogin = document.querySelector('#user-email-phone');
 const btnLogin = document.querySelector('#button-login');
 btnLogin.addEventListener('click', () => {
-  if (emailLogin.value != '')  alert(emailLogin.value);
+  if (emailLogin.value !== '') alert(emailLogin.value);
 });
 function erroCampos(var1) {
   const gender = document.querySelector('input[name="gender"]:checked');
-  console.log(var1);
   if (gender === null || gender === undefined) {
     alert('Campos inválidos: Gênero');
   }
@@ -15,8 +14,6 @@ const sobrenome = document.querySelector('#lastname');
 const emailPhone = document.querySelector('#phone_email');
 const novasenha = document.querySelector('#password');
 const dataNascimento = document.querySelector('#birthdate');
-console.log(nome.value, sobrenome.value, emailPhone.value, novasenha.value, dataNascimento.value);
-const campos = [nome.value, sobrenome.value, emailPhone.value, novasenha.value, dataNascimento.value]
 const btnCadastro = document.querySelector('#facebook-register');
 btnCadastro.addEventListener('click', () => {
   erroCampos(campos);

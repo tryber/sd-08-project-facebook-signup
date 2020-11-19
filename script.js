984bdb49c29e1ec5c4b3cd0653a97731f7a33528
Mario Pardo <mariio.pardojr@gmail.com>
2020-11-19 17:34:23 -0400

    Add button e alert

---
+++ script.js
@@ -0,0 +1,5 @@
+let btnLogin = document.getElementById('button-login');
+let inputEmailPhone = document.getElementById('user-email-phone');
+btnLogin.addEventListener('click', function () {
+  alert(inputEmailPhone.value);
+})

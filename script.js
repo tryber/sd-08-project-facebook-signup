273ff5cebfb171d2dff2f6b0e1fba2dc0a88ec56
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-11-19 18:35:45 -0300

    Req 7, tentativa 1

---
+++ script.js
@@ -0,0 +1,8 @@
+let buttonLogin = document.getElementById('button-login');
+
+
+function funcaoAlert() {
+  window.alert("Eu sou um alert!");
+}
+
+buttonLogin.addEventListener('click',funcaoAlert);

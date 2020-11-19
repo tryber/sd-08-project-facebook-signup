0f883b374c4faa80603d4e18b366a30738881ad2
Hugo Andrade Braga <hugobrr@gmail.com>
2020-11-19 20:11:21 -0300

    reduzindo os erros do CC com 75% dos requisitos

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const buttonEntrar = document.querySelector('#button-login');
 const emailOuTelefone = document.querySelector('#user-email-phone');
-const buttonCadastro = document.querySelector('#facebook-register');
+//const buttonCadastro = document.querySelector('#facebook-register');
 const nome = document.querySelector('.firstname');
 const sobreNome = document.querySelector('.lastname');
 const telefone = document.querySelector('.phone_email');
@@ -8,19 +8,18 @@ const novaSenha = document.querySelector('.password');
 const genero = document.querySelector('.gender');
 const aniversario = document.querySelector('.birthdate');
 const rapidoFacil = document.querySelector('.quick-easy');
-var variaveis = { nome , sobreNome , telefone , novaSenha , genero , aniversario};
+let variaveis = { nome, sobreNome, telefone, novaSenha, genero, aniversario };
 buttonEntrar.addEventListener('click', function () {
   alert(emailOuTelefone.value);
 });
 
-function cadastro(){
-  console.log("foi");
-  for (const valores in variaveis){
-    if(valores.value == ''){
-      console.log("ENtreiii");
-      var mensagemErro = document.createTextNode('Campos inválidos');
+function cadastro() {
+  let mensagemErro = document.createTextNode('Campos inválidos');
+  for ( const valores in variaveis ){
+    if (valores.value === '') {
       rapidoFacil.appendChild(mensagemErro);
       return false;
     }
   }
+  return true;
 }

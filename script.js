87882ed48cb0b74b74cf87c5b1de4f742887ee61
Daniel <danieldsrd@gmail.com>
2020-11-19 19:25:58 -0300

    code climate e requisito 8,9,10

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fucntion alerta() {
+function alerta() {
     let btn = document.getElementById('button-login');
     btn.addEventListener('click', function() {
         let email = document.getElementById('user-email-phone');

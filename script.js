4deeda4f12914314a510534a47aeb9e85731ec70
Alvaro Braga <alvaro.braga.job@gmail.com>
2020-11-19 16:32:59 -0300

    Codeclimate correction

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-
 function alertLogin() {
-  let button = document.getElementById('button-login');
-  let emailPhone = document.getElementById('user-email-phone');
-  button.addEventListener('click', function (){
-    alert(emailPhone.innerText);
+  const button = document.getElementById('button-login');
+  const emailPhone = document.getElementById('user-email-phone');
+  button.addEventListener('click', function() {
+    alert(emailPhone.value);
   });
 }
-
+alertLogin();

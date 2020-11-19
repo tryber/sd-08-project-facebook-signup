36cf4bf42dcdedae6a75d14deb3c5b0f2356e9c0
Seu nome <luiza.dilly@gmail.com>
2020-11-19 18:24:31 -0300

    resolvendo conflitos, REQ 8/9/10 - adiciona left e right content

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const buttonLogin = document.getElementById('button-login');
 
-buttonLogin.addEventListener('click', function () {
-  const emailAndPhone = document.getElementById('user-email-phone').value;
-  alert(emailAndPhone);
+buttonLogin.addEventListener('click', function (){
+    const emailAndPhone = document.getElementById('user-email-phone').value;
+    alert(emailAndPhone);
 });

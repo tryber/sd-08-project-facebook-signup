f461eb4c04e9893cc63d281dc4163686e995f36e
Victor Leone de Oliveira <vileoleone@gmail.com>
2020-11-19 18:39:50 -0300

    Requisito 17 e 19

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ const loginValue = document.getElementById('user-email-phone');
 buttonLogin.addEventListener('click', function () {
   alert(loginValue.value);
 });
+// Adicionando evento do requisito 19 - Campo de texto para opção personalizado
+const personalizadoRadioButton = document.getElementsByClassName('genderType')[2];
+const containerForGender = document.getElementById('genderContainer')
+// Espaço
+personalizadoRadioButton.addEventListener('click', function () {
+  let textForPersonalized = document.createElement('input');
+  textForPersonalized.type = 'text';
+  textForPersonalized.name = 'gender-custom';
+  textForPersonalized.placeholder = 'Gênero (opcional)';
+  containerForGender.appendChild(textForPersonalized);
+})

ea35df00056461026e017dee141f4a13c453f361
Wendrick <wendrick.b.c1@gmail.com>
2020-11-19 17:18:48 -0300

    Consertando alguns requisitos

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 const buttonLogin = document.getElementById('button-login');
+
 buttonLogin.addEventListener('click', function () {
   const userEmailPhone = document.getElementById('user-email-phone');
   alert(userEmailPhone.value);
+});
+
+const buttonRegister = document.getElementById('facebook-register');
+
+buttonRegister.addEventListener('click', () => {
+  const input = document.getElementsByClassName('input');
+  const gender = document.getElementsByClassName('gender');
+  let helper = false;
+  
+  for (let i = 0; i < gender.length; i += 1) {
+    if (gender[i].checked) {
+      helper = true;
+      break;
+    }
+  }
+
+  if (!helper) {
+    alert('Campos inválidos!');
+  }
+
+  for (let i = 0; i < input.length; i += 1) {
+    if (input[i].value === '') {
+      alert('Campos inválidos!');
+      break;
+    }
+  }
 });

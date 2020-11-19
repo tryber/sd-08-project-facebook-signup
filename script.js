a9f467988233ea28f927f5242acf903a84337f19
Victor Miwa <victoryukimiwa@hotmail.com>
2020-11-19 17:17:57 -0300

    requisito 7 botão com alert do email ou telefone

---
+++ script.js
@@ -0,0 +1,10 @@
+function alertEmail() {
+  const button = document.querySelector('#button-login');
+
+  button.addEventListener('click', function() {
+    const emailPhone = document.querySelector('#user-email-phone').value;
+    alert(emailPhone);
+  });
+}
+
+alertEmail();

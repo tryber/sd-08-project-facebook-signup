387e7833b110d20b6293cc0c3c63c5eccfe4f919
enicio <jessandro42@gmail.com>
2020-11-19 18:46:36 -0300

    Requisito 7 - Adicionado semi-colon

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const getEnter = document.querySelector('#button-login')
-getEnter.addEventListener('click', sendAlert)
-
-function sendAlert(e) {
-  alert(document.getElementById('user-email-phone').value)
+function sendAlert() {
+  alert(document.getElementById('user-email-phone').value);
 }
+
+const getEnter = document.querySelector('#button-login');
+getEnter.addEventListener('click', sendAlert);
+
+

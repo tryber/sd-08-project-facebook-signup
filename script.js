149551ccfe2702bd26a18667fedb65e1f8a40d0a
JRGCast <joao.ralph1@gmail.com>
2020-11-19 20:32:27 -0300

    adicionado arquivo prettierrc, e incluido no gitignore, para trocar todas as aspas duplas por simples

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 function entrar() {
-  const email = document.getElementById("user-email-phone").value;
+  const email = document.getElementById('user-email-phone').value;
   alert(email);
 }
 
 function loginCheck() {
-  const btnEntrar = document.getElementById("button-login");
-  btnEntrar.addEventListener("click", entrar);
+  const btnEntrar = document.getElementById('button-login');
+  btnEntrar.addEventListener('click', entrar);
 }
 loginCheck();
 
-const rightContent = document.querySelector(".right-content");
-const formsContent = rightContent.querySelectorAll("input");
+const rightContent = document.querySelector('.right-content');
+const formsContent = rightContent.querySelectorAll('input');
 function formsCheck() {
-  const btnSubmit = document.getElementById("facebook-register");
-  btnSubmit.addEventListener("click", function (event) {
+  const btnSubmit = document.getElementById('facebook-register');
+  btnSubmit.addEventListener('click', function (event) {
     for (let i = 0; i < formsContent.length; i += 1) {
-      if (formsContent[i].value === "") {
-        formsContent[i].value = "Campo inválido";
+      if (formsContent[i].value === '') {
+        formsContent[i].value = 'Campo inválido';
         event.preventDefault();
       }
     }

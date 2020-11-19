26f7fa8c362fa36ec6573124eca3357582dace98
Natanael Neto <nesn.gamedev@gmail.com>
2020-11-19 17:10:55 -0300

    C4 - Req 7 enviado, 35% do projeto feito!

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// JS da Página
+const btnLogin = document.getElementById('button-login');
+let userId;
+
+btnLogin.addEventListener('click', function () {
+  userId = document.getElementById('user-email-phone');
+  alert(userId.value);
+});

c713c70f7832ad7bf2d4a888e3bda3d6cd7b6077
Wendrick <wendrick.b.c1@gmail.com>
2020-11-19 15:52:14 -0300

    Parte 10 do projeto

---
+++ script.js
@@ -0,0 +1,5 @@
+let buttonLogin = document.getElementById('button-login');
+buttonLogin.addEventListener('click', function () {
+  let userEmailPhone = document.getElementById('user-email-phone');
+  alert(userEmailPhone.value);
+});

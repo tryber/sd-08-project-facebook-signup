fd4e9e70a9e81eceaa6af9afe80c77b6378df424
Rodrigo de Souza <rdesouza2008@gmail.com>
2020-11-19 16:11:48 -0300

    do 1 ao 7

---
+++ script.js
@@ -0,0 +1,8 @@
+//  req 7 - criar alert para o botão;
+
+let buttonEntrar = document.querySelector('#button-login');
+
+buttonEntrar.addEventListener('click', function () {
+  alert('Email ou telefone')
+})
+

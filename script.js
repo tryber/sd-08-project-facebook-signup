986912daea6038b0c1f84ffc02a79884ec2abf11
momentts <marcosd3wow@gmail.com>
2020-11-19 17:16:37 -0300

    mais resoluçoes do climate

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-function entrar (){
+const submit = document.getElementById('button-login');
+submit.addEventListener('click', entrar());
+function entrar () {
   alert('Digite seu Email ou telefone');
 }

cd335f6eea1bdfd39695b826d9b766bfcad36363
Paulo Simões <paulohbsimoes@gmail.com>
2020-11-19 17:38:03 -0300

    Requisito 7 concluido

---
+++ script.js
@@ -0,0 +1,6 @@
+const buttonLogin = document.getElementById('button-login');
+const inputUserEmailTelefone = document.getElementById('user-email-phone');
+
+buttonLogin.addEventListener('click', function() {
+  alert(inputUserEmailTelefone.value);
+});

e015e733875444cdadde64e9371532ac97d22fbb
JRGCast <joao.ralph1@gmail.com>
2020-11-19 20:25:24 -0300

    adicionada funcao formsCheck para colocar Campos invalido nos campos em que nao tenham nada

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 function entrar() {
-  const email = document.getElementById('user-email-phone').value;
+  const email = document.getElementById("user-email-phone").value;
   alert(email);
 }
 
-const btnEntrar = document.getElementById('button-login');
-btnEntrar.addEventListener('click', entrar);
+function loginCheck() {
+  const btnEntrar = document.getElementById("button-login");
+  btnEntrar.addEventListener("click", entrar);
+}
+loginCheck();
 
-const btnSubmit = document.getElementById('facebook-register');
-const rightContent = document.querySelector('.right-content');
-const formsContent = rightContent.querySelectorAll('input');
-
-btnSubmit.addEventListener('click', function () {
-  for (let i = 0; i < formsContent.length; i += 1) {
-    if (formsContent[i].value === '') {
-      alert('Campo inválido');
-      break;
+const rightContent = document.querySelector(".right-content");
+const formsContent = rightContent.querySelectorAll("input");
+function formsCheck() {
+  const btnSubmit = document.getElementById("facebook-register");
+  btnSubmit.addEventListener("click", function (event) {
+    for (let i = 0; i < formsContent.length; i += 1) {
+      if (formsContent[i].value === "") {
+        formsContent[i].value = "Campo inválido";
+        event.preventDefault();
+      }
     }
-  }
-});
+  });
+}
+formsCheck();

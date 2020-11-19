4b76812874e4807c18fb507cb90ef0df9cea04d6
Thiago Ferreira <thiagocristhian@gmail.com>
2020-11-19 19:15:14 -0300

    adicionando requisitos 1 ao 17

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const btnLogin = document.querySelector('#button-login');
+
+btnLogin.addEventListener('click', function() {
+  const userEmailPhone = document.querySelector('#user-email-phone').value;
+
+  alert(userEmailPhone);
+});

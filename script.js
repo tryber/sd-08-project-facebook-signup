229752e2ca2442a4fd768553ffd2f805ca4c6f10
Bruno <brunocmoraes97@gmail.com>
2020-11-19 17:34:40 -0300

    Correção code climate

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function alertEmail() {
   const button = document.querySelector('#button-login');
 
-  button.addEventListener('click', function() {
+  button.addEventListener('click', function () {
     const emailPhone = document.querySelector('#user-email-phone').value;
     alert(emailPhone);
   });

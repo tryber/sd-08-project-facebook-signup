3d67e288f19c1ea35a265e77487c94d82bb03705
siebenei5 <siebenei7@gmail.com>
2020-11-19 17:55:53 -0300

    Add script.js Request 7 Alert

---
+++ script.js
@@ -0,0 +1,8 @@
+let buttonID = document.getElementById("button-login");
+
+buttonID.addEventListener("click", function()
+{
+    let inputValue = document.getElementById("user-email-phone").value;
+
+    alert(inputValue);
+})

f669b93a93281430496bf19be176a24d458a32a0
Luiz Reina <luizreeina@gmail.com>
2020-11-19 17:06:12 -0300

    requirements 17 and 19 - customGender and submit button

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const buttonLogin = document.querySelector('#button-login');
+const buttonCustomGender = document.querySelector('#Personalizado');
+
 
 buttonLogin.addEventListener('click', () => {
   const emailValue = document.querySelector('#user-email-phone');
   alert(emailValue.value);
 });
+
+function plotInvalid() {
+  const formField = document.querySelector('.create-account');
+  const msgError = document.createElement('p');
+  msgError.textContent = 'Campos inválidos';
+  formField.appendChild(msgError);
+}
+
+buttonCustomGender.addEventListener('click', createCustomGender);
+
+function createCustomGender() {
+  const genderCustom = document.querySelector('.hidden-input');
+  genderCustom.innerHTML = '<input name="gender-custom" placeholder="Gênero (opcional)" type="text">';
+}

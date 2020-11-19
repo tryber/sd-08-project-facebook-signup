2dcd12d3b40a79596c219177f6c0c332944f6bd2
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2020-11-19 19:15:38 -0300

    requisito 18 js

---
+++ script.js
@@ -0,0 +1,10 @@
+const registerButton = document.getElementById("facebook-register");
+const InputAll = document.querySelectorAll("#signup-container .input");
+registerButton.addEventListener("click", function() {
+    for (let index in InputAll) {
+        if (InputAll[index].value === "") {
+            alert("Campos inválidos");
+            return true;
+        }
+    }
+});

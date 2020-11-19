066bfe78821abb748120ef0a8ea5baf39b2d86ed
Rafael <rafaelmaltez89@gmail.com>
2020-11-19 18:14:41 -0300

    Requisite 18

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ function alertLogin() {
   });
 }
 alertLogin();
+
+function validateForm() {
+  const button = document.getElementById('facebook-register');
+  button.addEventListener('click', function () {
+    const input = document.querySelectorAll('.cadastro input');
+    for (let index = 0; index < input.length; index +=1){
+      input[index].setCustomValidity('Campos inválidos');
+    }
+  })
+}
+validateForm();
+// setCustomValidity retirada do artigo: https://www.devmedia.com.br/html5-validator-validando-formularios-com-html5/28785

0db44e27aa1674130215dfa086d7ee59a288109b
Ruben Albertassi <albertassi88@gmail.com>
2020-11-19 19:10:19 -0300

    Inclusao do requisito 18

--- script.js
+++ script.js
@@ -2,3 +2,30 @@ const btnLogin = document.querySelector('#button-login');
 btnLogin.addEventListener('click', function () {
   alert('Email ou telefone');
 });
+
+let buttonCadastrar = document.querySelector("#facebook-register");
+
+function cadastrar(){
+  buttonCadastrar.addEventListener("click", function(event){
+    event.preventDefault();
+    let inputs = document.querySelectorAll(".form-user input[type=text]");
+    let resultado = "";
+    for (let index=0; index<inputs.length; index++){
+      let arrayInput = inputs[index];
+      if (arrayInput.value == "") {
+        arrayInput.className = "erro";
+        resultado += `Campos inválidos ${arrayInput.name}.\n`;
+      }else {
+       arrayInput.className = ""; 
+      }    
+    }
+    if (resultado !== ""){
+      alert(resultado);            
+    }else {            
+      form.reset();
+    }
+  });
+}
+cadastrar();
+
+

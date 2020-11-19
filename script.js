3c3ceb1e4867b4bc74a54ffc7b7066f15c8f8294
Gabriel Reis Barros Santana <gsantanaj18@gmail.com>
2020-11-19 19:28:27 -0300

    Correção do code climate até requisito 14

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function userAlert() {
-  let loginBtn = document.querySelector('#button-login');
-  let userLogin = document.querySelector('#user-email-phone');
+  const loginBtn = document.querySelector('#button-login');
+  const userLogin = document.querySelector('#user-email-phone');
 
   loginBtn.addEventListener('click', function () {
     alert(userLogin.value);
-  })
+  });
 }
-
-userAlert(); 
+userAlert();

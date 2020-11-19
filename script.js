fb2577305fdbf38a2b98c430c41b5e18f4eb8761
Fabio <fabiohigoralm2@gmail.com>
2020-11-19 19:06:21 -0300

    fazendo em dupla

---
+++ script.js
@@ -0,0 +1,8 @@
+let btEntrar=document.getElementById("button-login")
+
+
+btEntrar.addEventListener('click', function(){
+let m=document.getElementById("user-email-phone").value;
+alert(m);
+    
+});

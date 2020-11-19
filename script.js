00e9771988eabd459a07a716e88ed9d8e970b4c6
Victor Breno <victor.brns2@gmail.com>
2020-11-19 19:09:15 -0300

    correções codeclimate

--- script.js
+++ script.js
@@ -6,14 +6,19 @@ function entrar() {
 const btnEntrar = document.getElementById('button-login');
 btnEntrar.addEventListener('click', entrar);
 
-function checkForms() { // Exibir uma mensagem "Campos inválidos" dentro do formulário caso pelo menos um campo não esteja preenchido
-  const btnSubmit = document.getElementById('facebook-register')
-  const formsContent = document.querySelector('right-content').children;
-  btnSubmit.addEventListener('click', function () {
-    for (let i = 0; i < formsContent.length; i += 1) {
-      if (formsContent[i].value === '') {
-        formsContent[i].value = "Campos Inválidos"
-      }
+// Exibir uma mensagem "Campos inválidos" dentro do formulário caso pelo menos 
+// um campo não esteja preenchido
+const btnSubmit = document.getElementById('facebook-register');
+const rightContent = document.querySelector('.right-content')
+const formsContent = rightContent.querySelectorAll('input');
+
+btnSubmit.addEventListener('click', function () {
+  for (let i = 0; i < formsContent.length; i += 1) {
+    if (formsContent[i].value === '') {
+      alert('Campo inválido');
+      break
     }
-  })
-}
+  }
+});
+
+

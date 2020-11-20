6d84056a610a7565d6352163cb93ddfebcc1718b
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2020-11-20 14:01:12 -0300

    arranging code climate

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 const botaoLogin = document.getElementById('button-login');
 const emailOuTelefone = document.getElementById('user-email-phone');
 
-function alertBotao () { 
+function alertBotao() { 
   alert(emailOuTelefone.value);
 }
 botaoLogin.addEventListener('click', alertBotao);
 
 const botaoEnviar = document.querySelector('#facebook-register');
 
-botaoEnviar.addEventListener('click', function () {
+botaoEnviar.addEventListener('click', function() {
 
   const inputsText = document.getElementsByClassName('inputs');
 
-  for (let index = 0; index < inputsText.length; index +=1 ) {
-
+  for (let index = 0;index < inputsText.length;index += 1) {
+    
     const value = inputsText[index];
-
     if (value.required) {
       if (value.value === '') {
         alert('Campos invalidos');
@@ -25,18 +24,16 @@ botaoEnviar.addEventListener('click', function () {
   }
 });
 
-const persona = document.getElementById('botaopersonalizado');
+const persona = document.querySelector('.botaopersonalizado');
 const radioPerso = document.getElementsByClassName('radioperso');
 
-for (let index = 0; index < radioPerso.length; index +=1 ) {
-  radioPerso[index].addEventListener('click', function () {
-
+for (let index = 0;index < radioPerso.length;index += 1) {
+  radioPerso[index].addEventListener('click', function() {
     if (radioPerso[2].checked === true) {
-
       persona.style.display = 'block';
     }
     else {
       persona.style.display = 'none';
     }
-  })
+  });
 }

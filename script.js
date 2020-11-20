77edf719904aab5903254f6af9fe0e9d22af490d
Lucas Gama <lucasbg@protonmail.com>
2020-11-19 21:30:05 -0300

    Requisito 18: validacao dos campos preenchidos com o botao submit

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const emailTel = document.getElementById('user-email-phone');
 btnLogin.addEventListener('click', function () {
   window.alert(emailTel.value);
 });
+
+const filedForms = document.getElementById('forms');
+const btnSubmited = document.getElementById('facebook-register');
+
+btnSubmited.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (!filedForms.checkValidity()) {
+    window.alert('Campos inválidos');
+  }
+});

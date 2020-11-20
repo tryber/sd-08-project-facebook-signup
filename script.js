7606a8f278c608deb4299ff1d269754acb3ccc05
Ivanildo Cândido <systechtecnologia@gmail.com>
2020-11-20 17:42:35 -0300

    Adicionar evento ao clicar em radio outro genero

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ const entradaEmailTelefone = document.getElementById('user-email-phone');
 botaoLogin.addEventListener('click', function () {
   alert(entradaEmailTelefone.value);
 });
+
+function otherGender() {
+  const radio = document.getElementById('personal');
+  radio.addEventListener('change', function () {
+    const gender = document.createElement('input');
+    const divGender = document.querySelector('.other-gender');
+    if (radio.checked) {
+      gender.type = 'text';
+      gender.name = 'gender-custom';
+      gender.placeholder = 'Gênero';
+      divGender.appendChild(gender);
+    }
+  });
+}
+otherGender();

c03a35c0784ad66096822dd924e1288f48aa73a1
Nícolas Godek <nicolasgodek@gmail.com>
2020-11-20 16:50:12 -0300

    tentando resolver problemas nas questões 5, 6 e 7 parte 2

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function buttonLogin() {
   const button = document.querySelector('#button-login');
   button.addEventListener('click', function () {
     const email = document.querySelector('#user-email-phone-label').value;
-    alert(`Email ou telefone ${email}`);
+    alert(email);
   });
 }
 

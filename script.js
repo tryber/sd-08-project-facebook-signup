53b2b4d5e375c2ca03ab9fd9ff4ae1014644c6f1
Felipe Gomes Belarmino <epilefgomes1@gmail.com>
2020-11-19 21:12:22 -0300

    validei campos preenchidos

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 document.getElementById('button-login').addEventListener('click', () => {
   (alert(document.getElementById('user-email-phone').value));
 });
+
+document.getElementById('facebook-register').addEventListener('click', () => {
+  const nome = document.getElementById('nome').value;
+  const sobrenome = document.getElementById('sobrenome').value;
+  const cel_mail = document.getElementById('cel_mail').value;
+  const senha = document.getElementById('senha').value;
+  const data_nascimento = document.getElementById('birthdate').value;
+  if (!nome || !senha || !sobrenome || !cel_mail || !data_nascimento){
+    alert('Preencha ao menos um campo')
+  }
+})

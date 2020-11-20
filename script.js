eb6893c46fc6ee80f61373078b14ad50a5955b4d
Clênio Galvão <clenio.cgm@gmail.com>
2020-11-20 15:24:27 -0300

    altera o quesito 18 pra ficar mais bonito

--- script.js
+++ script.js
@@ -26,5 +26,5 @@ for (let index = 0; index < formInputLocal.length; index += 1) {
 const btnSalvar = document.getElementById('facebook-register');
 const form2Local = document.getElementsByClassName('form-group2');
 btnSalvar.addEventListener('click', function () {
-  form2Local[0].innerHTML = 'Campos inválidos';
+  form2Local[0].insertAdjacentHTML('afterbegin', 'Campos inválidos');
 });

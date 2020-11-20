02a1538eb234ef02c0de56d640d9c0ee6dc10092
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2020-11-20 15:04:23 -0300

    adjusting code climate again one million times later

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const botaoLogin = document.getElementById('button-login');
 const emailOuTelefone = document.getElementById('user-email-phone');
 
-function alertBotao() { 
+function alertBotao() {
   alert(emailOuTelefone.value);
 }
 botaoLogin.addEventListener('click', alertBotao);
@@ -9,11 +9,9 @@ botaoLogin.addEventListener('click', alertBotao);
 const botaoEnviar = document.querySelector('#facebook-register');
 
 botaoEnviar.addEventListener('click', function () {
-
   const inputsText = document.getElementsByClassName('inputs');
 
   for (let index = 0; index < inputsText.length; index += 1) {
-    
     const value = inputsText[index];
     if (value.required) {
       if (value.value === '') {
@@ -31,8 +29,7 @@ for (let index = 0; index < radioPerso.length; index += 1) {
   radioPerso[index].addEventListener('click', function () {
     if (radioPerso[2].checked === true) {
       persona.style.display = 'block';
-    }
-    else {
+    } else {
       persona.style.display = 'none';
     }
   });

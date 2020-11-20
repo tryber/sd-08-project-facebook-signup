7a4a2b6220dcf5b03c1bb34d6143467c711e0911
jnpefo <jnpefo@gmail.com>
2020-11-20 16:57:36 -0300

    Req 19, tentativa 1

--- script.js
+++ script.js
@@ -4,15 +4,35 @@ function funcaoAlert() {
 
 document.getElementById('button-login').addEventListener('click', funcaoAlert);
 
+const allInputs = document.querySelectorAll('.right-content input');
 
-// const allInputs = document.querySelectorAll('.right-content input');
+document.querySelector('#facebook-register').addEventListener('click', function (event) {
+  for (let i = 0; i < allInputs.length - 1; i += 1) {
+    if (allInputs[i].value === '') {
+      showInvalidos();
+      event.preventDefault();
+    }
+  }
+});
 
-// document.querySelector('#facebook-register').addEventListener('click', function () {
-//   for (let i = 0; i < allInputs.length - 1; i += 1) {
-//     if (allInputs[i].value === '') {
+function showInvalidos() {
+  document.querySelector('.mensagem-invalidos').style.display = 'block';
+}
 
-//       return;
-//     }
-//   }
+allInputs[7].addEventListener('change', function () {
+  if (allInputs[7].checked === true) {
+    document.querySelector(".custom").style.display = 'flex';
+  }
+});
 
-// });
+allInputs[6].addEventListener('change', function () {
+  if (allInputs[6].checked === true) {
+    document.querySelector(".custom").style.display = 'none';
+  }
+});
+
+allInputs[5].addEventListener('change', function () {
+  if (allInputs[5].checked === true) {
+    document.querySelector(".custom").style.display = 'none';
+  }
+});

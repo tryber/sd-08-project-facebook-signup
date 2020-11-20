63d244e17ede56e799f98c994a46d87f4b8d94ce
Mateus Coury <mateuscoury@gmail.com>
2020-11-19 21:40:09 -0300

    alteracoes cc 3

--- script.js
+++ script.js
@@ -8,11 +8,9 @@ botaoLogin.addEventListener('click', function () {
 const botaoenviar = document.querySelector('#facebook-register');
 
 botaoenviar.addEventListener('click', function () {
-
   const inputsText = document.getElementsByClassName('input');
-
   for (let index = 0; index < inputsText.length; index += 1) {
-    let input = inputsText[index];
+    const input = inputsText[index];
     if (input.required) {
       if (input.value === '') {
         alert('Campos inválidos');

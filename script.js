23150d79f690040cc525db1457832f82fa7bd41d
Felipe Gomes Belarmino <epilefgomes1@gmail.com>
2020-11-19 21:29:17 -0300

    campos inválidos

--- script.js
+++ script.js
@@ -2,13 +2,8 @@ document.getElementById('button-login').addEventListener('click', () => {
   (alert(document.getElementById('user-email-phone').value));
 });
 
-document.getElementById('facebook-register').addEventListener('click', () => {
-  const nome = document.getElementById('nome').value;
-  const sobrenome = document.getElementById('sobrenome').value;
-  const celMail = document.getElementById('cel_mail').value;
-  const senha = document.getElementById('senha').value;
-  const dataNascimento = document.getElementById('birthdate').value;
-  if (!nome || !senha || !sobrenome || !celMail || !dataNascimento) {
-    alert('Campos inválidos');
-  }
-});
+document.getElementById('nome').setCustomValidity('Campos inválidos');
+document.getElementById('sobrenome').setCustomValidity('Campos inválidos');
+document.getElementById('cel_mail').setCustomValidity('Campos inválidos');
+document.getElementById('senha').setCustomValidity('Campos inválidos');
+document.getElementById('birthdate').setCustomValidity('Campos inválidos');

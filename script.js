6fe439587e4be9ddc3d90b98b49307543910bd1d
Aline Debastiani <debastianialine@gmail.com>
2020-11-19 21:46:16 -0300

    cumpre requisito 18

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ function alerta() {
   });
 }
 alerta();
+
+let btnCadastreSe = document.getElementById('facebook-register');
+
+btnCadastreSe.addEventListener('click', function(event) {
+  event.preventDefault();
+  let erros = '';
+  let inputsText = document.querySelectorAll('input[type=text], input[type=radio], input[type=password]');
+  for (let index = 0; index < inputsText.length; index++) {
+    let input = inputsText[index];
+    if (input.value === '') {      
+      erros += `Campos inválidos\n`;
+    }
+  }
+  if (erros !== '') {
+    alert(erros);
+  }  
+});

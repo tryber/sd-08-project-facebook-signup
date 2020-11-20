b59efe583b91cd163d5551fb0b84667895eceee8
Marconi <marconieng@gmail.com>
2020-11-19 21:50:44 -0300

    requisito 18: adiciona evento ao botao cadastre-se

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ botaoEntrar.addEventListener('click', function () {
   const loginEmailOuTel = document.getElementById('user-email-phone').value;
   alert(loginEmailOuTel);
 });
+
+const inputs = document.querySelectorAll('input[type=text], input[type=radio], input[type=password]');
+const botaoCadastrar = document.getElementById('facebook-register');
+function verificaCamposInvalidos() {
+    for (let index = 0; index < inputs.length; index += 1) {
+        const input = inputs[index];
+        if (input.value === '') {
+            alert('Campos inválidos');
+        }
+    }
+}
+
+botaoCadastrar.addEventListener('click', verificaCamposInvalidos);

d96c123311d6bd77099da5244b12518b2e6e81d7
amandaos <amanda.775.souza@gmail.com>
2020-11-20 19:34:20 -0300

    inicia os desafios do js

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function alertButton() {
   alert(document.getElementById('user-email-phone').value);
 }
+function checkForm() {
+  const nome = document.querySelector(".nome").value;
+
+
+  //Check input Fields Should not be blanks.
+  if (nome === '') {
+    alert("Por favor preencha todos os campos.");
+  }
+}
 
 window.onload = () => {
   const botaoEnvia = document.getElementById('button-login');
   botaoEnvia.onclick = alertButton;
-};
+
+  const botaoCadastro = document.getElementById('facebook-register');
+  botaoCadastro.onclick = checkForm;
+}

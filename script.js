8ed17b49f58f4f63e59979d5a5d1d479581a5200
Bruno <brunocmoraes97@gmail.com>
2020-11-20 14:03:22 -0300

    Requisito 18. Adiciona funcionalidade do botão cadastre-se, que valida se os inputs estão preenchidos

--- script.js
+++ script.js
@@ -8,3 +8,32 @@ function alertEmail() {
 }
 
 alertEmail();
+
+function signIt() {
+  let inputs = document.querySelectorAll('.inputC');
+  let register = document.querySelector('#facebook-register');
+  register.addEventListener('click', function () {
+    for (let index =0; index < inputs.length; index +=1){
+      if (inputs[index].value == ""){
+        alert('Campos inválidos');
+      }
+    }
+  })
+}
+
+signIt();
+
+function checkRadio() {
+  let inputRadio = document.querySelectorAll('.inputRadio');
+  let register = document.querySelector('#facebook-register');
+  register.addEventListener('click', function () {
+    for(let index = 0; index < inputRadio.length; index+=1){
+      if(!inputRadio[index].checked){
+        alert('Campos inválidos');
+      }else {
+        break;
+      }
+    }
+  })
+}
+checkRadio();

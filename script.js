256884ff1b1dfa38119bb20f7bfbd91a193fb3da
Thiago Ferreira <thiagocristhian@gmail.com>
2020-11-19 21:30:43 -0300

    corrigindo codeclimate - negrito no html e tirando alguns espacos

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 const btnLogin = document.querySelector('#button-login');
 
-btnLogin.addEventListener('click', function() {
+btnLogin.addEventListener('click', function () {
   const userEmailPhone = document.querySelector('#user-email-phone').value;
 
   alert(userEmailPhone);

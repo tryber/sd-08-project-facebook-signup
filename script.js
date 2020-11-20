e7b2b4d3bd54a8384e61d3658f4d00bafc27da09
jnpefo <jnpefo@gmail.com>
2020-11-20 18:12:36 -0300

    Receber as informaçoes do 19

--- script.js
+++ script.js
@@ -10,14 +10,16 @@ function showInvalidos() {
   document.querySelector('.mensagem-invalidos').style.display = 'block';
 }
 
-document.querySelector('#facebook-register').addEventListener('click', function (event) {
-  for (let i = 0; i < allInputs.length - 1; i += 1) {
-    if (allInputs[i].value === '') {
-      showInvalidos();
-      event.preventDefault();
+document
+  .querySelector('#facebook-register')
+  .addEventListener('click', function (event) {
+    for (let i = 0; i < allInputs.length - 1; i += 1) {
+      if (allInputs[i].value === "") {
+        showInvalidos();
+        event.preventDefault();
+      }
     }
-  }
-});
+  });
 
 allInputs[7].addEventListener('change', function () {
   if (allInputs[7].checked === true) {

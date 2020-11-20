e730d686b873c1f5022af286ba70ad056abdf58a
Max <gmaxgomes@gmail.com>
2020-11-20 10:05:54 -0300

    Requisito 20

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ function loginButtonAlert() {
     alert(emailPhone);
   });
 }
-
 loginButtonAlert();
 
 function createGenderField() {
@@ -30,23 +29,88 @@ function checkRadioButtons() {
 }
 checkRadioButtons();
 
+function fillAfterAcContent() {
+  const helloUser = document.getElementById('hello-user');
+  const name = document.getElementById('user-ca-name');
+  const surname = document.getElementById('user-ca-lastname');
+
+  const userEmailOrPhone = document.getElementById('user-ca-phone-email');
+  const emailOrPhone = document.getElementById('email-or-phone');
+
+  const birthDate = document.getElementById('birth-date');
+  const userBirthDate = document.getElementById('user-ca-birthdate');
+
+  const gender = document.getElementById('gender');
+  const f = document.getElementById('F');
+  const m = document.getElementById('M');
+  const p = document.getElementById('P');
+
+  helloUser.innerHTML = `Olá, ${name.value} ${surname.value}`;
+  emailOrPhone.innerHTML = userEmailOrPhone.value;
+  birthDate.innerHTML = userBirthDate.value;
+
+  if (f.checked === true) {
+    gender.innerHTML = 'Feminino';
+  } else if (m.checked === true) {
+    gender.innerHTML = 'Masculino';
+  } else if (p.checked === true) {
+    gender.innerHTML = 'Personalizado';
+  }
+
+};
+
+function verifyRadio(item, value) {
+
+  for (let index = 0; index < value; index += 1) {
+    if (item[index] === true) {
+
+    }
+  }
+
+};
+
+function verifyRadio(){
+  const caRadioInputList = document.querySelectorAll('.ca-radio-input');
+  if (caRadioInputList[0].checked === true) {
+    return false
+  } else if (caRadioInputList[1].checked === true) {
+    return false;
+  } else if (caRadioInputList[2].checked === true) {
+    return false;
+  }
+  return true;
+}
+
 function isEverythingFilled() {
   const registerButton = document.getElementById('facebook-register');
   registerButton.addEventListener('click', function () {
+    fillAfterAcContent();
     const caInputList = document.querySelectorAll('.ca-input');
-    const caRadioInputList = document.querySelectorAll('.ca-radio-input');
     let isntFilledCount = 0;
+
     for (let index = 0; index < caInputList.length; index += 1) {
       if (caInputList[index].value === '') {
         isntFilledCount += 1;
       }
     }
-    if (caRadioInputList[0].checked === false && caRadioInputList[1].checked === false && caRadioInputList[2].checked === false) {
+    if (verifyRadio()) {
       isntFilledCount += 1;
     }
     if (isntFilledCount !== 0) {
       alert('Campos inválidos');
+    } else {
+      fillAfterAcContent();
+      changeRightContentElementDisplay();
     }
   });
-}
+};
 isEverythingFilled();
+
+function changeRightContentElementDisplay() {
+  const contentBeforeAc = document.querySelector('.before-ac');
+
+  contentBeforeAc.parentNode.removeChild(contentBeforeAc);
+};
+
+
+

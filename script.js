af276770b1ca4c25541c70845313d45229ca3007
Clênio Galvão <clenio.cgm@gmail.com>
2020-11-20 14:50:06 -0300

    altera o quesito 18 e resolvo code climate

--- script.js
+++ script.js
@@ -8,17 +8,17 @@ function loginCheck() {
 }
 loginCheck();
 
-  //modelo modificado do site http://gabsferreira.com/alterando-a-validacao-padrao-do-html5/
+// modelo modificado do site http://gabsferreira.com/alterando-a-validacao-padrao-do-html5/
 
-const formInputLocal = document.querySelectorAll('.form-input')
+const formInputLocal = document.querySelectorAll('.form-input');
 for (let index = 0; index < formInputLocal.length; index += 1) {
-  formInputLocal[index].oninvalid  = function () {
+  formInputLocal[index].oninvalid = function () {
     // remove mensagens de erro padrão
-    this.setCustomValidity("");
+    this.setCustomValidity('');
     // faz a validação novamente
     if (!this.validity.valid) {
       // se estiver inválido, coloca a mensagem
-      this.setCustomValidity("Campos inválidos");
+      this.setCustomValidity('Campos inválidos');
     }
-  }
+  };
 }

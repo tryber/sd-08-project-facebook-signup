d25a5803f79306fc611e77b9353e90221fc78219
Ediberto Oliveira <edibertooliveira@aol.com>
2020-11-20 12:29:37 -0300

    feat: 1/2, requisito 20

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 const getButtonLogin = document.getElementById('button-login');
 const getInputLogin = document.getElementById('user-email-phone');
 const getButtonSubmit = document.getElementById('facebook-register');
+const getRightContent = document.getElementById('right-content');
 const getGenderPersonalizado = document.getElementById('personalizado');
 const getGenderPersonalizadoContainer = document.getElementById('genero-personalizado');
 const setMessageInvalid = document.getElementById('message-invalid');
+const formInputName = {
+  firstname: 'null',
+  lastname: 'null',
+  email: 'null',
+  date: 'null',
+  gender: 'null',
+  password: 'null',
+};
 
+function submitFormCreate() {
+  const template =
+  `<h1>Olá, ${formInputName[0]} ${formInputName[1]}</h1>
+  <span> ${formInputName[2]} </span>
+  <span> ${formInputName[3]} </span>
+  <span> ${formInputName[4]} </span>
+  `;
+  getRightContent.innerHTML += template;
+}
 function createInputGenderPersonalizado() {
   const genderCustom = '<input name="gender-custom" type="text" placeholder="Gênero (opcional)"></input>';
   getGenderPersonalizadoContainer.innerHTML += genderCustom;
@@ -18,10 +36,14 @@ getButtonSubmit.onclick = function (event) {
   event.preventDefault();
   const inputForm = document.querySelectorAll('#form-create input');
   let check = false;
-  inputForm.forEach((element) => {
+  inputForm.forEach((element, index) => {
     const valueElement = element.value;
     if (valueElement === '') check = true;
+    formInputName[index] = element.value;
   });
-  if (check) setMessageInvalid.style.display = 'block';
+  if (check) {
+    return (setMessageInvalid.style.display = 'block');
+  }
+  return submitFormCreate();
 };
 getGenderPersonalizado.onclick = createInputGenderPersonalizado;

fe1f0e98b0d3ae59ba803bb6215dda7f6031f1d6
Douglas Ferreira <dodofp.df@gmail.com>
2020-11-20 18:01:10 -0300

    Requisito 7 novamente

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
 const entrar = document.getElementById('button-login');
 
-function alerta() {
-  alert("Email ou telefone");
-}
-entrar.addEventListener('click', alerta);
+entrar.addEventListener('click', function () {
+  const email = document.getElementById('user-email-phone');
+  alert(email.value);
+});
 
-const cadastre = document.getElementById('facebook-register');
+// const cadastre = document.getElementById('facebook-register');
 
-const form = document.querySelectorAll('.form input');
+// const form = document.querySelectorAll('.form input');
 
+// cadastre.addEventListener('click', function () {
+//   for (let index = 0; index < form.length ; index +=1 ) {
+//     var contadorBranco = 0
+//     if (form[index].value == 0) {
+//       contadorBranco = contadorBranco + 1;
 
-
-cadastre.addEventListener('click', function () {
-  for (let index = 0; index < form.length ; index +=1 ) {
-    var contadorBranco = 0
-    if (form[index].value == 0) {
-      contadorBranco = contadorBranco + 1;
-
-    }
-  }
-  if(contadorBranco != 0){
-    alert('Cadastro Efetuado')
-  } else {
-    alert('Cadastro inválido')
-  }
-})
+//     }
+//   }
+//   if(contadorBranco != 0){
+//     alert('Cadastro Efetuado')
+//   } else {
+//     alert('Cadastro inválido')
+//   }
+// })
 
 // cadastre.addEventListener('click', function (){
 //   if(form[0].length < 0 ){

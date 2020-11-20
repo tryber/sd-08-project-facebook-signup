337b0df979c0a6bcf74da2a0bde1235a12b84aa8
amandaos <amanda.775.souza@gmail.com>
2020-11-19 21:09:21 -0300

    Code Climate item 7

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function alertButton() {
-  alert (document.getElementById("user-email-phone").value);
+  alert(document.getElementById('user-email-phone').value);
 }
 
 window.onload = () => {
-  let botaoEnvia= document.getElementById("button-login");
+  const botaoEnvia = document.getElementById('button-login');
   botaoEnvia.onclick = alertButton;
-}
+};

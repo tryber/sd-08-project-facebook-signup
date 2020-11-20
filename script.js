41dee15512222bad45f6fb653503d7fb56c47aa9
Bruno Martins <bruno.martins217@gmail.com>
2020-11-19 21:13:25 -0300

    feat: Validar se todos os campos foram preenchidos, Requisito 18

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const getButtonLogin = document.getElementById('button-login');
 const getInputLogin = document.getElementById('user-email-phone');
+const getButtonSubmit = document.getElementById('facebook-register');
+const getFormCreate = document.getElementById('form-create');
 
 getButtonLogin.onclick = function () {
   const login = getInputLogin.value;
   alert(login);
 };
+
+getButtonSubmit.onclick = function (event) {
+  event.preventDefault()
+  const inputForm = document.querySelectorAll('#form-create input');
+  let check = false
+  inputForm.forEach((element) =>{
+    if (element.value === ''){
+      check = true;
+    }
+    })
+    if (check){
+      alert('Campos inválidos')
+    }
+}

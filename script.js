b87a8ead7ac6bf4e36793196a6ea552e2e26748a
Daniel <danieldsrd@gmail.com>
2020-11-19 23:05:08 -0300

    Validador dentro do forms - code climate

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ btnCadastreSe.addEventListener('click', function (event) {
   if (document.querySelectorAll('p').length === 3) {
     document.querySelectorAll('form')[1].removeChild(document.querySelectorAll('p')[2]);
   }
-  let erros = "";
+  let erros = '';
   const AllInputs = document.querySelectorAll('input[type=text], input[type=password]');
   for (let index = 2; index < AllInputs.length; index += 1) {
     const input = AllInputs[index];
@@ -25,15 +25,15 @@ btnCadastreSe.addEventListener('click', function (event) {
   if (erros !== '') {
     // alert('Campos inválidos');
     const pInvalid = document.createElement('p');
-    pInvalid.innerHTML = "Campos inválidos";
+    pInvalid.innerHTML = 'Campos inválidos';
     document.querySelectorAll('form')[1].appendChild(pInvalid);
   }
 });
 
-document.querySelector('#custom').addEventListener('click', function (){
+document.querySelector('#custom').addEventListener('click', function () {
   const newInput = document.createElement('input');
-  newInput.name = "gender-custom";
-  newInput.placeholder = "Gênero (opcional)";
+  newInput.name = 'gender-custom';
+  newInput.placeholder = 'Gênero (opcional)';
   document.querySelector('.form-gender').appendChild(newInput);
 });
 

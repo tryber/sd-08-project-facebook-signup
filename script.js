1bb547c7aa3ab7c1e7675b5dd2b634faaa11e491
brutibbc <brutibbc@gmail.com>
2020-11-20 18:38:32 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const btnLogin = document.getElementById('button-login');
 const rbPers = document.getElementById('personalizado');
 const persInput = document.getElementById('persInput');
+const firstname = document.getElementById('firstname');
+const lastname = document.getElementById('lastname');
+const phone_email = document.getElementById('phone_email');
+const password = document.getElementById('password');
+const birthdate = document.getElementById('birthdate');
+const btnRegister = document.getElementById('facebook-register');
 let userId;
 
 btnLogin.addEventListener('click', function () {
   userId = document.getElementById('user-email-phone');
   alert(userId.value);
+});
+
+btnRegister.addEventListener('click',function () {
+  if (firstname.value == "" || lastname.value == "" || phone_email == "" || password == "" || birthdate == "") {
+    alert('Campos inválidos');
+  }
 });
 
 function DisableInput() {

5bb67c00b22b0a77cb61cb996660f51fdacf3049
robson widnef <robsonbatistawidnef2@gmail.com>
2020-11-19 22:42:41 -0300

    :sparkles: Finalizado Requisito 18

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-const btnEntrar = document.getElementById('button-login');
-const txtEmail = document.getElementById('user-email-phone');
+const btnEntrar = document.getElementById("button-login");
+const btnCadastre = document.getElementById("facebook-register");
+const divMensagemInvalida = document.getElementById("mensagem-invalida");
+const txtEmail = document.getElementById("user-email-phone");
 
 function alertConteudoEmail() {
   alert(txtEmail.value);
 }
 
-btnEntrar.addEventListener('click', alertConteudoEmail);
+btnEntrar.addEventListener("click", alertConteudoEmail);
+
+function validaCadastro() {
+  const camposValidacao = document.querySelectorAll(".validator");
+  for (let campo = 0; campo < camposValidacao.length; campo += 1) {
+    if (camposValidacao[campo].value === "") {
+      divMensagemInvalida.style.display = "block";
+      return;
+    }
+  }
+}
+
+btnCadastre.addEventListener("click", validaCadastro);

24c99a1d38e3b3dfea99a3cfe9bc69af57b56673
Ronald Jr <rpintojunior@gmail.com>
2020-11-20 15:27:09 -0300

    Coloca item 19

--- script.js
+++ script.js
@@ -6,3 +6,73 @@ function alertEntrar() {
   });
 }
 alertEntrar();
+
+const inputGender = document.querySelector('#gender-custom');
+const radioGender = document.querySelector('#per');
+
+radioGender.addEventListener('change', () => {
+  if (inputGender.style.display === '' || inputGender.style.display === 'none') {
+    inputGender.style.display = 'block';
+ } else {
+    inputGender.style.display = 'none';
+  }
+});
+
+function checkRadio() {
+  if (inputGender.style.display === 'block') {
+  inputGender.style.display = 'none';
+  }
+}
+
+const mas = document.querySelector('#mas');
+mas.addEventListener('change', checkRadio);
+
+const fem = document.querySelector('#fem');
+fem.addEventListener('change', checkRadio);
+
+// const getAllInputs = getFormNewAccount.getElementsByTagName('input');
+// const getFormNewAccount = document.querySelector('#register-form');
+
+// function checkInputValues() {
+//   for (let index = 0; index < getAllInputs.length; index += 1) {
+//     if (getAllInputs[index].type === 'text' && getAllInputs[index].value === '' && getAllInputs[index].id !== 'gender-custom') {
+//       p.innerHTML = 'Campos inválidos';
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// function checkRadioSelected() {
+//   for (let index = 0; index < getAllInputs.length; index += 1) {
+//     if (getAllInputs[index].type === 'radio' && getAllInputs[index].checked) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
+
+// function validateForm() {
+//   const allInputFilled = checkInputValues();
+//   const radioSelected = checkRadioSelected();
+
+//   if (!allInputFilled || !radioSelected) {
+//     p.innerHTML = 'Campos inválidos';
+//     return false;
+//   }
+
+//   createNewUserInfo();
+//   return false;
+// }
+
+// const requiredInputs = getFormNewAccount.querySelectorAll('[required]');
+
+// function checkFieldsValidity() {
+//   for (let index = 0; index < requiredInputs.length; index += 1) {
+//     const currentInput = requiredInputs[index];
+//     if (!currentInput.checkValidity()) {
+//       alert("Campos inválidos!")
+//       return;
+//     }
+//   }
+// }

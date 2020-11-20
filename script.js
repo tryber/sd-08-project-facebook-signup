3602949ad50e864c04d471b21a6c3b7598d09341
Joao <joaocarlitoneto@gmail.com>
2020-11-19 22:42:51 -0300

    Arrumando Branch

--- script.js
+++ script.js
@@ -3,25 +3,25 @@ btnLogin.addEventListener('click', function () {
   alert('Email ou telefone');
 });
 
-let buttonCadastrar = document.querySelector("#facebook-register");
+const buttonCadastrar = document.querySelector('#facebook-register');
 
-function cadastrar(){
-  buttonCadastrar.addEventListener("click", function(event){
+function cadastrar() {
+  buttonCadastrar.addEventListener('click', function (event) {
     event.preventDefault();
-    let inputs = document.querySelectorAll(".form-user input[type=text]");
-    let resultado = "";
-    for (let index=0; index<inputs.length; index++){
-      let arrayInput = inputs[index];
-      if (arrayInput.value == "") {
-        arrayInput.className = "erro";
+    const inputs = document.querySelectorAll('.form-user input[type=text]');
+    let resultado = '';
+    for (let index = 0; index < inputs.length; index += 1) {
+      const arrayInput = inputs[index];
+      if (arrayInput.value === '') {
+        arrayInput.className = 'erro';
         resultado += `Campos inválidos ${arrayInput.name}.\n`;
-      }else {
-       arrayInput.className = ""; 
-      }    
-    }
-    if (resultado !== ""){
-      alert(resultado);            
-    }else {            
+      } else {
+       arrayInput.className = '';
+        }
+      }
+    if (resultado !== '') {
+      alert(resultado);       
+    } else { 
       form.reset();
     }
   });

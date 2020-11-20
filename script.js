c17f17373f9c6b1e258d10378d1e222e2ca4ceaf
Antônio Schappo <antonioschappo@gmail.com>
2020-11-20 16:52:14 -0300

    Fix codeclimate(1)

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 const loginButton = document.getElementById('button-login');
 const emailTelefone = document.getElementById('user-email-phone');
-const inputArray = document.querySelectorAll('.right-content input')
+const inputArray = document.querySelectorAll('.right-content input');
 loginButton.addEventListener('click', function () {
   alert(emailTelefone.value);
 });
 const registerButton = document.getElementById('facebook-register');
 
 registerButton.addEventListener('click', function () {
-  for(let index = 0; index < inputArray.length; index+=1){
-    let input = inputArray[index];
-    let check = input.checkValidity();
-    if(check){
-      input.setCustomValidity("");
-    }else {
-      let form = document.getElementsByClassName('form-right-content')[0];
-      let message = document.createElement('p');
+  for (let index = 0; index < inputArray.length; index += 1){
+    const input = inputArray[index];
+    const check = input.checkValidity();
+    if (check) {
+      input.setCustomValidity('');
+    } else {
+      const form = document.getElementsByClassName('form-right-content')[0];
+      const message = document.createElement('p');
       message.className = 'error-message';
-      message.innerHTML = "Campos inválidos";
+      message.innerHTML = 'Campos inválidos';
       form.appendChild(message);
       break;
-    };
-  };
+    }
+  }
 });

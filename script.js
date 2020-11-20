b67c291a5bace3fe7825576846baa585b10df07c
Joao <joaocarlitoneto@gmail.com>
2020-11-19 23:41:10 -0300

    Corrigindo Issues

--- script.js
+++ script.js
@@ -3,30 +3,30 @@ btnLogin.addEventListener('click', function () {
   alert('Email ou telefone');
 });
 
-let buttonCadastrar = document.querySelector("#facebook-register");
-let form = document.querySelector(".form-user");
-let right = document.querySelector(".right-content");
+const buttonCadastrar = document.querySelector('#facebook-register');
+const form = document.querySelector('.form-user');
+const right = document.querySelector('.right-content');
 
 function cadastrar() {
   buttonCadastrar.addEventListener('click', function (event) {
     event.preventDefault();
 
-    let inputs = document.querySelectorAll(".form-user input[type=text]");
-    let resultado = "";
-    let resultadoUsuario = "";
-    for (let index=0; index<inputs.length; index++){
-      let arrayInput = inputs[index];
-      if (arrayInput.value == "") {
-        arrayInput.className = "erro";
-        resultado += `Campos inválidos ${arrayInput.name}.\n`;        
-      }else {
-       arrayInput.className = "";
-       resultadoUsuario += arrayInput.value;
-      }    
+    const inputs = document.querySelectorAll('.form-user input[type=text]');
+    let resultado = '';
+    let resultadoUsuario = '';
+    for (let index = 0; index < inputs.length; index += 1) {
+      const arrayInput = inputs[index];
+      if (arrayInput.value === '') {
+        arrayInput.className = 'erro';
+        resultado += `Campos inválidos ${arrayInput.name}.\n`;    
+      } else {
+        arrayInput.className = '';
+        resultadoUsuario += arrayInput.value;
+      }
     }
-    if (resultado !== ""){
-      alert(resultado);            
-    }else {            
+    if (resultado !== '') {
+      alert(resultado);         
+    } else {         
       right.innerHTML = resultadoUsuario;
       form.reset();
     }
@@ -34,18 +34,17 @@ function cadastrar() {
 }
 cadastrar();
 
-let radioPersonalizado = document.querySelector("#pers");
-let box4 = document.querySelector(".box4");
-radioPersonalizado.addEventListener("click", function(){
-  let input = document.createElement("input");
-  input.type = "radio";
-  input.id = "opcional"
-  input.name = "gender-custom"
-  input.placeholder = "Gênero (opcional)";
-  let label = document.createElement("label");
-  label.for = "opcional";
-  label.innerHTML = "Gênero (opcional)";
+const radioPersonalizado = document.querySelector('#pers');
+const box4 = document.querySelector('.box4');
+radioPersonalizado.addEventListener('click', function () {
+  const input = document.createElement('input');
+  input.type = 'radio';
+  input.id = 'opcional';
+  input.name = 'gender-custom';
+  input.placeholder = 'Gênero (opcional)';
+  const label = document.createElement('label');
+  label.for = 'opcional';
+  label.innerHTML = 'Gênero (opcional)';
   box4.append(input);
-  box4.append(label); 
+  box4.append(label);
 });
-

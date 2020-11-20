20f8a483483ddc29c12ca56694045fbee05d1708
Bruno Martins <bruno.martins217@gmail.com>
2020-11-20 13:08:32 -0300

    feat: Substituir o conteúdo do container com a classe right-content, Requisito 20

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-const getButtonLogin = document.getElementById('button-login');
-const getInputLogin = document.getElementById('user-email-phone');
-const getButtonSubmit = document.getElementById('facebook-register');
-const getRightContent = document.getElementById('right-content');
-const getGenderPersonalizado = document.getElementById('personalizado');
-const getGenderPersonalizadoContainer = document.getElementById('genero-personalizado');
-const setMessageInvalid = document.getElementById('message-invalid');
-const formInputName = {
-  firstname: 'null',
-  lastname: 'null',
-  email: 'null',
-  date: 'null',
-  gender: 'null',
-  password: 'null',
-};
+const getButtonLogin = document.getElementById("button-login");
+const getInputLogin = document.getElementById("user-email-phone");
+const getButtonSubmit = document.getElementById("facebook-register");
+const getRightContent = document.getElementById("right-content");
+const getGenderPersonalizado = document.getElementById("personalizado");
+const getGenderPersonalizadoContainer = document.getElementById(
+  "genero-personalizado"
+);
+const setMessageInvalid = document.getElementById("message-invalid");
+const formInputName = [];
+
+function rightContentClear() {
+  while (getRightContent.hasChildNodes()) {
+    getRightContent.removeChild(getRightContent.firstChild);
+  }
+}
 
 function submitFormCreate() {
-  const template =
-  `<h1>Olá, ${formInputName[0]} ${formInputName[1]}</h1>
+  rightContentClear();
+  const template = `<h1>Olá, ${formInputName[0]} ${formInputName[1]}</h1>
   <span> ${formInputName[2]} </span>
-  <span> ${formInputName[3]} </span>
   <span> ${formInputName[4]} </span>
+  <span> ${formInputName[5]} </span>
   `;
   getRightContent.innerHTML += template;
 }
 function createInputGenderPersonalizado() {
-  const genderCustom = '<input name="gender-custom" type="text" placeholder="Gênero (opcional)"></input>';
+  const genderCustom =
+    '<input name="gender-custom" type="text" placeholder="Gênero (opcional)"></input>';
   getGenderPersonalizadoContainer.innerHTML += genderCustom;
 }
 
@@ -34,15 +36,23 @@ getButtonLogin.onclick = function () {
 };
 getButtonSubmit.onclick = function (event) {
   event.preventDefault();
-  const inputForm = document.querySelectorAll('#form-create input');
+  const inputForm = document.querySelectorAll("#form-create input");
   let check = false;
   inputForm.forEach((element, index) => {
     const valueElement = element.value;
-    if (valueElement === '') check = true;
-    formInputName[index] = element.value;
+    if (valueElement === "") {
+      check = true;
+    }
+    if (element.name === "gender") {
+      if (element.checked) {
+        formInputName.push(element.value);
+      }
+      return false;
+    }
+    formInputName.push(element.value);
   });
   if (check) {
-    return (setMessageInvalid.style.display = 'block');
+    return (setMessageInvalid.style.display = "block");
   }
   return submitFormCreate();
 };

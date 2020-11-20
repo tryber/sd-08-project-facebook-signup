fb2c60e28118b2fc8e7b65dca04e0adc79a29ca2
momentts <marcosd3wow@gmail.com>
2020-11-20 15:29:47 -0300

    verificando o requisito 18 tentativa 18

--- script.js
+++ script.js
@@ -7,15 +7,3 @@ function loginCheck() {
   btnEntrar.addEventListener('click', entrar);
 }
 loginCheck();
-const cadastro = document.getElementById('facebook-register');
-const form = document.querySelector('.form-group2');
-cadastro.addEventListener('click', function () {
-  for (let index = 0; index < form.length; index += 1) {
-    if (form[index].innerText === '') {
-      alert('Campos inválidos');
-      break;
-    } else {
-      alert('Cadastro efetuado');
-    }
-  }
-});

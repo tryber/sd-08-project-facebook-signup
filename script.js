ee861deb83167aa47a75294fac0489a3e4bd4bc1
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2020-11-19 21:51:36 -0300

    Codeclimate errors

--- script.js
+++ script.js
@@ -44,3 +44,10 @@ function removeCustomGenderInput() {
 }
 
 removeCustomGenderInput();
+
+function errorMessage() {
+  const createAccountButton = document.querySelector('#facebook-register');
+  createAccountButton.addEventListener('click', checkAllInputs(){
+
+  });
+}

c2beda33cdda7474da303a06dbfb63a012fa8e49
jnpefo <jnpefo@gmail.com>
2020-11-20 17:03:26 -0300

    Req 19, tentativa 2

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ document.getElementById('button-login').addEventListener('click', funcaoAlert);
 
 const allInputs = document.querySelectorAll('.right-content input');
 
+function showInvalidos() {
+  document.querySelector('.mensagem-invalidos').style.display = 'block';
+}
+
 document.querySelector('#facebook-register').addEventListener('click', function (event) {
   for (let i = 0; i < allInputs.length - 1; i += 1) {
     if (allInputs[i].value === '') {
@@ -15,24 +19,20 @@ document.querySelector('#facebook-register').addEventListener('click', function 
   }
 });
 
-function showInvalidos() {
-  document.querySelector('.mensagem-invalidos').style.display = 'block';
-}
-
 allInputs[7].addEventListener('change', function () {
   if (allInputs[7].checked === true) {
-    document.querySelector(".custom").style.display = 'flex';
+    document.querySelector('.custom').style.display = 'flex';
   }
 });
 
 allInputs[6].addEventListener('change', function () {
   if (allInputs[6].checked === true) {
-    document.querySelector(".custom").style.display = 'none';
+    document.querySelector('.custom').style.display = 'none';
   }
 });
 
 allInputs[5].addEventListener('change', function () {
   if (allInputs[5].checked === true) {
-    document.querySelector(".custom").style.display = 'none';
+    document.querySelector('.custom').style.display = 'none';
   }
 });

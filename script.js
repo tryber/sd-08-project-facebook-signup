5bf408c789b14bd90e01edc329f2e4b7a3bc282a
Lucas Rodrigues <lucas.movimento@gmail.com>
2020-11-20 16:50:13 -0300

    acertos codeclimate

--- script.js
+++ script.js
@@ -1,70 +1,74 @@
-let btEntrar = document.getElementById("button-login");
+const btEntrar = document.getElementById('button-login');
 
 btEntrar.addEventListener('click', function () {
-    let m = document.getElementById("user-email-phone").value;
-    alert(m);
-
+  const m = document.getElementById('user-email-phone').value;
+  alert(m);
 });
 
-function showForm (value) {
-    let dataInput = document.getElementById('data-input')
-    let createSpan = document.createElement('span');
-    createSpan.innerText = value;
-    createSpan.style.display = 'block';
-    dataInput.appendChild(createSpan);
+function showForm(value) {
+  const dataInput = document.getElementById('data-input');
+  const createSpan = document.createElement('span');
+  createSpan.innerText = value;
+  createSpan.style.display = 'block';
+  dataInput.appendChild(createSpan);
 }
 
-let signUpForm = document.forms['signUp-form'];
+const signUpForm = document.forms['signUp-form'];
 
-function validateForm (event) {
-    event.preventDefault();
+function validateForm(event) {
+  event.preventDefault();
 
-    if (signUpForm['firstname'].value === '') {
-        alert('Campos inválidos');
-    } else if (signUpForm['lastname'].value === '') {
-        alert('Campos inválidos');
-    } else if (signUpForm['phone_email'].value === '') {
-        alert('Campos inválidos');
-    } else if (signUpForm['password'].value === '') {
-        alert('Campos inválidos');
-    } else if (signUpForm['birthdate'].value === '') {
-        alert('Campos inválidos');
-    } else if (signUpForm['gender'].value === '') {
-        alert('Campos inválidos');
-    } else {
-        let inputValueName = 'Olá ' + signUpForm['firstname'].value + ' ' + signUpForm['lastname'].value;
-        showForm (inputValueName);
-        let inputValueContact = 'Celular ou email: ' + signUpForm['phone_email'].value;
-        showForm (inputValueContact);
-        let inputValueBday = 'Data de nascimento: ' + signUpForm['birthdate'].value;
-        showForm (inputValueBday);
-        let inputValueGender = 'Gênero: ' + signUpForm['gender'].value;
-        showForm (inputValueGender);
+  const firstNameValue = signUpForm['firstname'].value;
+  const lastNameValue = signUpForm['lastname'].value;
+  const phoneValue = signUpForm['phone_email'].value;
+  const passwordValue = signUpForm['password'].value;
+  const birthdayValue = signUpForm['birthdate'].value;
+  const genderValue = signUpForm['gender'].value;
 
-        document.querySelector('.form-style').style.display = 'none';
-    }
-    
+  if (firstNameValue === '') {
+    alert('Campos inválidos');
+  } else if (lastNameValue === '') {
+    alert('Campos inválidos');
+  } else if (phoneValue === '') {
+    alert('Campos inválidos');
+  } else if (passwordValue === '') {
+    alert('Campos inválidos');
+  } else if (birthdayValue === '') {
+    alert('Campos inválidos');
+  } else if (genderValue === '') {
+    alert('Campos inválidos');
+  } else {
+    const inputValueName = `Olá ${firstNameValue} ${lastNameValue}`;
+    showForm(inputValueName);
+    const inputValueContact = `Celular ou email: ${phoneValue}`;
+    showForm(inputValueContact);
+    const inputValueBday = `Data de nascimento: ${birthdayValue}`;
+    showForm(inputValueBday);
+    const inputValueGender = `Gênero: ${genderValue}`;
+    showForm(inputValueGender);
+
+    document.querySelector('.form-style').style.display = 'none';
+  }
 }
 
-let btSubmit = document.getElementById('facebook-register');
+const btSubmit = document.getElementById('facebook-register');
 
 btSubmit.addEventListener('click', validateForm);
 
 
-let genderOption = document.getElementById('label-gender');
+const genderOption = document.getElementById('label-gender');
 
 genderOption.addEventListener('click', function () {
     // let btGenderPers = document.querySelector('#pers').checked;
     // console.log(btGenderPers);
 
-    let aux = document.forms['signUp-form']['gender'].value;
+  const aux = document.forms['signUp-form']['gender'].value;
 
-    if (aux === 'Personalizado') {
-        let inputPersGender = document.createElement('input');
-        inputPersGender.type = 'text';
-        inputPersGender.name = 'gender-custom';
-        inputPersGender.placeholder = 'Gênero (opcional)';
-        genderOption.appendChild(inputPersGender);
-    }
+  if (aux === 'Personalizado') {
+    const inputPersGender = document.createElement('input');
+    inputPersGender.type = 'text';
+    inputPersGender.name = 'gender-custom';
+    inputPersGender.placeholder = 'Gênero (opcional)';
+    genderOption.appendChild(inputPersGender);
+  }
 });
-

e2faceb11193893fc7daa013d8353ff18f0f376c
Bruno Martins <bruno.martins217@gmail.com>
2020-11-19 21:39:13 -0300

    feat: Adicione um novo campo de texto no formulário, Requisito 19

--- script.js
+++ script.js
@@ -2,12 +2,18 @@ const getButtonLogin = document.getElementById('button-login');
 const getInputLogin = document.getElementById('user-email-phone');
 const getButtonSubmit = document.getElementById('facebook-register');
 const getFormCreate = document.getElementById('form-create');
+const getGenderPersonalizado = document.getElementById('personalizado');
+const getGenderPersonalizadoContainer = document.getElementById('genero-personalizado')
+
+function createInputGenderPersonalizado() {
+  const genderCustom = '<input name="gender-custom type="text" placeholder="Gênero (opcional)"></input>'
+  getGenderPersonalizadoContainer.innerHTML += genderCustom
+}
 
 getButtonLogin.onclick = function () {
   const login = getInputLogin.value;
   alert(login);
 };
-
 getButtonSubmit.onclick = function (event) {
   event.preventDefault()
   const inputForm = document.querySelectorAll('#form-create input');
@@ -21,3 +27,4 @@ getButtonSubmit.onclick = function (event) {
       alert('Campos inválidos')
     }
 }
+getGenderPersonalizado.onclick = createInputGenderPersonalizado

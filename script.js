f345d053c1fafad4af7f52c853a9a6c38c970cf9
victor Farias <victorfarias.tv@gmail.com>
2020-11-20 15:35:13 -0300

    Arrumando codeclimates requisitos 18 e 19. Atualizando função requisito 18

--- script.js
+++ script.js
@@ -2,13 +2,12 @@ const loginButton = document.getElementById('button-login');
 const emailTelefone = document.getElementById('user-email-phone');
 const btnFacebook = document.getElementById('facebook-register');
 const signup = document.getElementById('form-cadastro');
+const signupInputs = document.querySelectorAll('#form-cadastro input');
 const firstName = document.getElementById('name');
 const lastName = document.getElementById('last-name');
 const phoneEmail = document.getElementById('phone');
 const password = document.getElementById('password');
 const dateBorn = document.getElementById('birthdate');
-const radioMan = document.getElementById('masc');
-const radioFemale = document.getElementById('fem');
 const radioPersonalized = document.getElementById('pers');
 
 function login() {
@@ -17,27 +16,22 @@ function login() {
 
 loginButton.addEventListener('click', login);
 
-function submitForm () {
-  if (firstName.value == "" || firstName.value == null) {
-    alert('Campos inválidos');
-  } else if (lastName.value == "" || lastName.value == null) {
-    alert('Campos inválidos');
-  } else if (phoneEmail.value == "" || phoneEmail.value == null) {
-    alert('Campos inválidos');
-  } else if (password.value == "" || password.value == null) {
-    alert('Campos inválidos');
-  } else if (dateBorn.value == "" || dateBorn.value == null) {
-    alert('Campos inválidos');
+function submitForm() {
+  for (i = 0; i < signupInputs.length; i += 1) {
+    if (signupInputs[i].value === '') {
+      alert('Campos inválidos');
+      break;
+    }
   }
 }
 
 btnFacebook.addEventListener('click', submitForm);
 
-function createInput (){
-  let radios = document.getElementsByName('gender');
-  for (let index = 0; index < radios.length; index +=1) {
-    if (radios[index].value == 'Personalizado'){
-      let campo =document.createElement('input');
+function createInput() {
+  const radios = document.getElementsByName('gender');
+  for (let index = 0; index < radios.length; index += 1) {
+    if (radios[index].value === 'Personalizado') {
+      const campo = document.createElement('input');
       signup.appendChild(campo);
       campo = signup.insertBefore(campo, btnFacebook);
       campo.type = 'text';
@@ -48,5 +42,3 @@ function createInput (){
 }
 
 radioPersonalized.addEventListener('click', createInput);
-
-

2855bead1cf3ed6b4d47f5952a238dcc75ed032c
Hugo Andrade Braga <hugobrr@gmail.com>
2020-11-20 16:58:04 -0300

    pagina nao reiniciando agora com 80% dos requisitos com um provavel

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 const buttonEntrar = document.querySelector('#button-login');
 const emailOuTelefone = document.querySelector('#user-email-phone');
 const buttonCadastro = document.querySelector('#facebook-register');
-// const nome = document.querySelector('.firstname');
-// const sobreNome = document.querySelector('.lastname');
-// const telefone = document.querySelector('.phone_email');
-// const novaSenha = document.querySelector('.password');
-// const genero = document.querySelector('.gender');
-// const aniversario = document.querySelector('.birthdate');
-// const rapidoFacil = document.querySelector('.quick-easy');
-// const variaveis = { a: nome, b: sobreNome, c:
-// telefone, d: novaSenha, e: genero, f: aniversario };
+const nome = document.querySelector('.firstname');
+const sobreNome = document.querySelector('.lastname');
+const telefone = document.querySelector('.phone_email');
+const novaSenha = document.querySelector('.password');
+const genero = document.querySelector('.gender');
+const aniversario = document.querySelector('.birthdate');
+const rapidoFacil = document.querySelector('.quick-easy');
+const variaveis = { a: nome, b: sobreNome, c: telefone, d: novaSenha, e: genero, f: aniversario };
 buttonEntrar.addEventListener('click', function () {
   alert(emailOuTelefone.value);
 });
 
-buttonCadastro.addEventListener('click', function () {
-  // const mensagemErro = document.createTextNode('Campos inválidos');
-  // const naoSei = Object.value(variaveis);
-  // for (const valor of naoSei) {
-  //   if (valor.value == null) {
-  //     rapidoFacil.appendChild(mensagemErro);
-  //   }
-  // }
+buttonCadastro.addEventListener('click', function (event) {
+  const mensagemErro = document.createTextNode('Campos inválidos');
+  event.preventDefault();
+  for (const valor in variaveis) {
+    if (valor.value == null) {
+      rapidoFacil.appendChild(mensagemErro);
+    }
+  }
 });

982bbe9b72d7148880386ee6180cd395caca89ce
Felipe Gomes Belarmino <epilefgomes1@gmail.com>
2020-11-19 21:15:14 -0300

    Camel Case e ponto e vírgula

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ document.getElementById('button-login').addEventListener('click', () => {
 document.getElementById('facebook-register').addEventListener('click', () => {
   const nome = document.getElementById('nome').value;
   const sobrenome = document.getElementById('sobrenome').value;
-  const cel_mail = document.getElementById('cel_mail').value;
+  const celMail = document.getElementById('cel_mail').value;
   const senha = document.getElementById('senha').value;
-  const data_nascimento = document.getElementById('birthdate').value;
-  if (!nome || !senha || !sobrenome || !cel_mail || !data_nascimento){
-    alert('Preencha ao menos um campo')
+  const dataNascimento = document.getElementById('birthdate').value;
+  if (!nome || !senha || !sobrenome || !celMail || !dataNascimento) {
+    alert('Campos inválidos');
   }
-})
+});

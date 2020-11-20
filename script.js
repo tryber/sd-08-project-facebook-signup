26b63c0999f39855063eceb65c64fadc7992edab
Ronald Jr <rpintojunior@gmail.com>
2020-11-20 19:38:56 -0300

    Corrige requisito 18,19 e acrescenta 20

--- script.js
+++ script.js
@@ -28,3 +28,26 @@ const mas = document.querySelector('#mas');
 mas.addEventListener('change', checkRadio);
 const fem = document.querySelector('#fem');
 fem.addEventListener('change', checkRadio);
+
+const cadast = document.querySelector("#facebook-register");
+cadast.addEventListener("click", () => {
+  const formsValue = document.querySelectorAll("#register-form")[0];
+      for (let index = 0; index < 5; index += 1) {
+   if (
+      document.querySelectorAll("input").length < 1 ||
+      (inputGender.style.display === "block" && inputGender.value === "") ||
+      formsValue[index].value === ""
+    ) {
+      document.querySelector(".chec-return").style.display = "block";
+    } else {
+      const firstName = document.getElementById("firstname").value;
+      const lastName = document.getElementById("lastname").value;
+      const phoneMail = document.getElementById("phone_email").value;
+      const birthDate = document.getElementById("birthdate").value;
+      const gender = document.querySelector("input[type=radio]:checked").value;
+      const welcomeUser = `Olá, ${firstName} ${lastName} ${gender} ${phoneMail} ${birthDate}`;
+      document.querySelector(".right-content").innerHTML = `${welcomeUser}`;    
+      }
+  }
+  return false;
+});

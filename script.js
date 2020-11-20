83d9540d2d726bdf2bb4328b764eaf7108a29b47
Vitor Nunes <vitornunes200@gmail.com>
2020-11-19 21:57:52 -0300

    Requisito 18 e 19 correcao

--- script.js
+++ script.js
@@ -6,17 +6,26 @@ function alertaLogin() {
 const botaoLogin = document.querySelector('#button-login');
 botaoLogin.addEventListener('click', alertaLogin);
 
-const custom = document.querySelector('#custom');
-custom.addEventListener('click', function () {
-  const customInput = document.createElement('input');
-  customInput.type = 'text';
-  customInput.name = 'gender-custom';
-  customInput.placeholder = 'Gênero (opcional)';
-  document.querySelector('.custom-content').appendChild(customInput);
+const radioContent = document.querySelector('.radio-content');
+const input = radioContent.querySelectorAll('input');
+Array.from(input).forEach(function (input) {
+  input.addEventListener('change', function () {
+    if (input.id == 'custom') {
+      const customInput = document.createElement('input');
+      customInput.type = 'text';
+      customInput.name = 'gender-custom';
+      customInput.placeholder = 'Gênero (opcional)';
+      document.querySelector('.custom-content').appendChild(customInput);
+    } else {
+      const customContent = document.querySelector('.custom-content');
+      customContent.removeChild(customContent.firstChild);
+    }
+  })
 });
 
 function checaValidacao() {
-  const input = document.querySelectorAll('input');
+  const form = document.getElementById('form-singUp');
+  const input = form.querySelectorAll('input');
   for (let i = 0; i < input.length; i += 1) {
     if (input[i].value.length === 0) {
       input[i].setCustomValidity('Campos inválidos');
@@ -24,6 +33,19 @@ function checaValidacao() {
       input[i].setCustomValidity('');
     }
   }
+  verificaRadio();
 }
+
+function verificaRadio() {
+  const radio = document.getElementsByClassName('radio');
+  for (let i = 0; i < radio.length; i += 1) {
+    if (radio[i].checked === true) {
+      input[i].setCustomValidity('');
+      return;
+    }
+  }
+  radio[0].setCustomValidity('Campos inválidos');
+}
+
 const submit = document.querySelector('#facebook-register');
 submit.addEventListener('click', checaValidacao);

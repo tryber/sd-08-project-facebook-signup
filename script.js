2c270bc2b78e9366943c515027fb2e7886ba38df
Lucas Nascimento <lucasmvnascimento@gmail.com>
2020-11-19 22:10:11 -0300

    adequando code climate

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 function checkTexts() {
   const form = document.querySelectorAll('.form input');
   let flag1 = 0;
-  for (let index = 0; index < 5; index += 1){
-    if (form[index].value === ""){
-        flag1 = 1;
+  for (let index = 0; index < 5; index += 1) {
+    if (form[index].value === "") {
+      flag1 = 1;
     }
   }
   return (flag1);
 }
 
 function checkForms(flag1) {
-  let flags = [];
-  let flag2 = 0;
-  flags.push(flag1,flag2);
+  const flags = [];
+  const flag2 = 0;
+  flags.push(flag1, flag2);
   const form = document.querySelectorAll('.form input');
-  for (let index = 5; index < form.length; index += 1){
-    if (form[index].checked === true){
-        flags[1] = 1;
+  for (let index = 5; index < form.length; index += 1) {
+    if (form[index].checked === true) {
+      flags[1] = 1;
     }
   }
   return (flags);

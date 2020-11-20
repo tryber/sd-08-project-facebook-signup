10b4917a5f4189caad952621ef547c4d3c3054b4
mateusvictor7 <mateussantiago.victor@gmail.com>
2020-11-20 12:55:39 -0300

    Requisito 18 concluido

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const buttonLogin = document.getElementById('button-login');
 const inputUserEmailTelefone = document.getElementById('user-email-phone');
+const formAlert = document.getElementById('form-alert');
 
 buttonLogin.addEventListener('click', function () {
   alert(inputUserEmailTelefone.value);
+});
+
+const buttonCadastro = document.getElementById('facebook-register');
+
+buttonCadastro.addEventListener('click', function (event) {
+  event.preventDefault();
+  const fields = document.querySelectorAll('[required]');
+
+  for (let index = 0; index < fields.length; index += 1){
+    const field = fields[index];
+    if (!field.checkValidity()) {
+      formAlert.classList.remove('d-none');
+    }
+  }
 });

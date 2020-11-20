f109e573fa3de0c7e8344eefa7f8844b784a9c4e
Rafael <rafaeljavaro750@gmail.com>
2020-11-20 17:49:56 -0300

    Req 18 - verifica inputs

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ btnLogin.addEventListener('click', () => {
   const email = document.getElementById('user-email-phone').value;
   alert(email);
 });
+
+function verificaInputTexto() {
+  const inputTexto = document.getElementById('formulario-abre-conta');
+  for (let index = 0; index < 4; index += 1) {
+    if (inputTexto[index].value == '') {
+      const camposInvalidos = document.createElement('p');
+      camposInvalidos.innerHTML = 'Campos inválidos';
+      inputTexto.appendChild(camposInvalidos);
+      break;
+    }
+  }
+}
+
+const btnCadastro = document.getElementById('facebook-register');
+btnCadastro.addEventListener('click', verificaInputTexto);

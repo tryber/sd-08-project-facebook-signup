94704b9666bb1dd668b8a6c46789722cab4c7ace
Andre Sampaio <andresampaiolm@gmail.com>
2020-11-20 12:25:58 -0300

    implementado requisito 18: validaçao dos campos do form

--- script.js
+++ script.js
@@ -6,3 +6,41 @@ btnLogin.addEventListener('click', function () {
 
   alert(userEmailPhone);
 });
+
+const btnRegister = document.querySelector('#facebook-register');
+
+function radioBtnValue() {
+  const radioBtns = document.getElementsByName('gender');
+
+  for (let index = 0; index < radioBtns.length; index += 1) {
+    if (radioBtns[index].checked) {
+      return radioBtns[index].value;
+    }
+  }
+
+  return '';
+}
+
+function formValidation(event) {
+  const formInputs = {
+    firstName: document.querySelector('.first-name').value,
+    lastName: document.querySelector('.name input + input').value,
+    phoneEmail: document.querySelector('#phone-email').value,
+    password: document.querySelector('#password').value,
+    birthdate: document.querySelector('#birthdate').value,
+    gender: radioBtnValue(),
+  };
+
+  const span = document.querySelector('.form-span');
+
+  const keys = Object.keys(formInputs);
+
+  keys.forEach(function (key) {
+    if (formInputs[key] === null || formInputs[key] === '') {
+      event.preventDefault();
+      span.style.display = 'block';
+    }
+  });
+}
+
+btnRegister.addEventListener('click', formValidation);

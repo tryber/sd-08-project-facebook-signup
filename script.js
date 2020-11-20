8615cbb9612ad03a0019924375677826b0e9ec76
Felipe Gomes Belarmino <epilefgomes1@gmail.com>
2020-11-20 16:11:46 -0300

    .

--- script.js
+++ script.js
@@ -8,22 +8,24 @@ document.getElementById('facebook-register').addEventListener('click', (event) =
   const celMail = document.getElementById('cel_mail').value;
   const senha = document.getElementById('senha').value;
   const dataNascimento = document.getElementById('birthdate').value;
+  const ola = "Olá, "
+  const sadacao = ola + nome + sobrenome;
   if (!nome || !senha || !sobrenome || !celMail || !dataNascimento) {
     document.getElementById('camposInvalidos').innerText = 'Campos inválidos';
     event.preventDefault();
   } else {
     event.preventDefault();
     document.querySelector('.to-reveal').classList.remove('to-reveal');
-    document.getElementById('nome-reveal').innerHTML = 'Olá, ' + nome + sobrenome;
+    document.getElementById('nome-reveal').innerHTML = sadacao;
     document.getElementById('email-reveal').innerHTML = celMail;
     document.getElementById('data-reveal').innerHTML = dataNascimento;
     if (document.getElementById('feminino').checked) {
-    document.getElementById('gender-reveal').innerHTML = 'Feminino';
-  } else if (document.getElementById('masculino').checked) {
-    document.getElementById('gender-reveal').innerHTML = 'Masculino';
-  } else {
-    document.getElementById('gender-reveal').innerHTML = 'Personalizado';
-  }
+      document.getElementById('gender-reveal').innerHTML = 'Feminino';
+    } else if (document.getElementById('masculino').checked) {
+      document.getElementById('gender-reveal').innerHTML = 'Masculino';
+    } else {
+      document.getElementById('gender-reveal').innerHTML = 'Personalizado';
+    }
     document.querySelector('.to-remove').classList.toggle('to-reveal');
   }
 });

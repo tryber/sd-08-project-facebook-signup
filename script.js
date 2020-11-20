4ad85b7befbb68d9645a03b365bfbbdb1ea4b911
Mario Pardo <mariio.pardojr@gmail.com>
2020-11-20 14:13:09 -0400

    Alteracoes codeclimate

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const inputEmailPhone = document.getElementById('user-email-phone');
 btnLogin.addEventListener('click', function () {
   alert(inputEmailPhone.value);
 });
+
+const birthDate = document.getElementById('birthdate');
+birthDate.addEventListener('keyup', function () {
+  
+  
+})

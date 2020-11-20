ced1cfa54ca290a956048906f68da3f8d6767738
Mario Pardo <mariio.pardojr@gmail.com>
2020-11-20 14:48:58 -0400

    Ajuste de alinhamento right content

--- script.js
+++ script.js
@@ -3,9 +3,3 @@ const inputEmailPhone = document.getElementById('user-email-phone');
 btnLogin.addEventListener('click', function () {
   alert(inputEmailPhone.value);
 });
-
-const birthDate = document.getElementById('birthdate');
-birthDate.addEventListener('keyup', function () {
-  
-  
-})

70d1244e94cf2176708226de101f90075d9d3e6b
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2020-11-20 14:10:45 -0300

    adjusting code climate

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ botaoLogin.addEventListener('click', alertBotao);
 
 const botaoEnviar = document.querySelector('#facebook-register');
 
-botaoEnviar.addEventListener('click', function() {
+botaoEnviar.addEventListener('click', function () {
 
   const inputsText = document.getElementsByClassName('inputs');
 
@@ -28,7 +28,7 @@ const persona = document.querySelector('.botaopersonalizado');
 const radioPerso = document.getElementsByClassName('radioperso');
 
 for (let index = 0; index < radioPerso.length; index += 1) {
-  radioPerso[index].addEventListener('click', function() {
+  radioPerso[index].addEventListener('click', function () {
     if (radioPerso[2].checked === true) {
       persona.style.display = 'block';
     }

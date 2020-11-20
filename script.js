2eb6a0eef8aba6d6612d19aff8452dfc1f664be7
Mateus Coury <mateuscoury@gmail.com>
2020-11-20 17:06:04 -0300

    alteracoes de exercicios

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
-const botaoLogin = document.getElementById('button-login');
+const botaoLogin = document.getElementById("button-login");
 
-botaoLogin.addEventListener('click', function () {
-  alert('Email ou telefone');
+botaoLogin.addEventListener("click", function () {
+  alert("Email ou telefone");
 });
 
+const botaoenviar = document.querySelector("#facebook-register");
+const divvaziapos = document.querySelector("#divvazia");
 
-const botaoenviar = document.querySelector('#facebook-register');
-
-botaoenviar.addEventListener('click', function () {
-
-  let inputsText = document.getElementsByClassName('inputs');
-
+botaoenviar.addEventListener("click", function () {
+  let inputsText = document.getElementsByClassName("inputs");
   for (let index = 0; index < inputsText.length; index++) {
+    let inputstextposi = inputsText[index];
 
-    let value = inputsText[index]
-
-    if (value.required) {
-      if (value.value === "") {
-        alert("Campos invalidos")
-        break
+    if (inputstextposi.required) {
+      if (inputstextposi.value === "") {
+        divvaziapos.innerHTML = "Campos Inválidos";
+      } else {
       }
     }
   }
 });
-
-
-
 
 /* const persona = document.querySelector("#botaopersonalizado")
 
@@ -44,24 +38,28 @@ radiop.addEventListener("click", function (event) {
 
 }) */
 
-let persona = document.querySelector("#botaopersonalizado")
+let persona = document.querySelector("#botaopersonalizado");
 
-let radiop = document.getElementsByClassName("radioperso")
+let radiop = document.getElementsByClassName("radioperso");
+let element = document.createElement("input");
 
 for (let index = 0; index < radiop.length; index++) {
-
   radiop[index].addEventListener("click", function (event) {
-
     if (radiop[2].checked === true) {
+      persona.innerHTML =
+        '<input type="text" placeholder = "Gênero (Opcional)" name= "gender-custom" > ';
 
-      persona.style.display = "block"
+      // element.name = "gender-custom"
+      // element.placeholder = "Gênero (opcional)"
+      // element.type = "text"
+      // persona.appendChild(element)
+    } else {
+      persona.innerHTML = "";
+      // persona.style.display = "none";
     }
-    else {
-      persona.style.display = "none"
-    }
-  })
+  });
 }
 
+const nomesobretext = document.getElementById("nomesobre");
 
-
-
+const namelastname = document.querySelectorAll("#nomesobrenome");

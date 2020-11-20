c7b3281797a4683961b3af6de0a2a14f6503158b
Victor Breno <victor.brns2@gmail.com>
2020-11-20 12:11:08 -0300

    editei função formscheck para o ultimo requisito; organização do html

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ const formsContent = rightContent.querySelectorAll('input');
 function formsCheck() {
   const btnSubmit = document.getElementById('facebook-register');
   btnSubmit.addEventListener('click', function (event) {
+    let verificaVazio = 0;
     for (let i = 0; i < formsContent.length; i += 1) {
       if (formsContent[i].value === '') {
         formsContent[0].placeholder = 'Preencha seu nome';
@@ -25,8 +26,34 @@ function formsCheck() {
           'Campos inválidos';
         document.getElementById('mensagem-campos-invalidos').style.color =
           'red';
+        verificaVazio += 1;
         event.preventDefault();
       }
+    }
+    if (verificaVazio === 0) {
+      const firstName = document.getElementById('first-name').value;
+      const lastName = document.getElementById('last-name').value;
+      const email = document.getElementById('email').value;
+      const birthDate = document.getElementById('birth-date').value;
+      let genderValue = '';
+      const btnMasculino = document.getElementById('btn-masculino');
+      const btnFeminino = document.getElementById('btn-feminino');
+      const btnPersonalizado = document.getElementById('btn-personalizado');
+      if (btnMasculino.checked) {
+        genderValue = btnMasculino.value;
+      } else if (btnFeminino.checked) {
+        genderValue = btnFeminino.value;
+      } else if (btnPersonalizado.checked) {
+        genderValue = btnPersonalizado.value;
+      }
+      let rightSide = document.querySelector('.right-content');
+      rightSide.innerHTML = '';
+      let rightSideText = document.createElement('h1');
+      const text = 'Nome: ' + firstName + ' Sobrenome: ' + lastName +  '\nEmail ou Telefone: ' + email + '\nData de Nascimento: ' + birthDate + '\nGênero: ' + genderValue
+      let rightSideTextContent = document.createTextNode(text);
+      rightSideText.appendChild(rightSideTextContent);
+      rightSide.appendChild(rightSideText);
+      event.preventDefault();
     }
   });
 }
@@ -48,3 +75,10 @@ function personalizeCheck() {
   });
 }
 personalizeCheck();
+
+
+/* const btnSubmit = document.getElementById('facebook-register');
+btnSubmit.addEventListener('click', function changeForm() {
+  alert('sumitado');
+})
+ */

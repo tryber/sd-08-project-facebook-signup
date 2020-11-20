ea056bf371a428790bf83a5105aaa039d553060f
momentts <marcosd3wow@gmail.com>
2020-11-20 11:47:38 -0300

    verificando o requisito 18 tentativa 4

--- script.js
+++ script.js
@@ -6,16 +6,15 @@ function loginCheck() {
   const btnEntrar = document.getElementById('button-login');
   btnEntrar.addEventListener('click', entrar);
 }
-loginCheck();
+function loginCheck();
+const btncadastro = document.getElementById('facebook-register');
 function validar() {
-  const btncadastro = document.getElementById('facebook-register');
-  if (document.getElementById('inputnome').value == null || document.getElementById('inputsobrenome').value == null
-  || document.getElementById('inputcelular').value == null || document.getElementById('inputnovasenha').value == null
-  || document.getElementById('inputnascimento').value == null) {
+  if ((document.getElementById('inputnome').value == null) || (document.getElementById('inputsobrenome').value == null)
+  || (document.getElementById('inputcelular').value == null) || (document.getElementById('inputnovasenha').value == null)
+  || (document.getElementById('inputnascimento').value == null)) {
     alert('Campos inválidos');
   } else {
     return true;
   }
-  btncadastro.addEventListener('click', validar);
 }
-
+btncadastro.addEventListener('click', validar);

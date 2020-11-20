5bbd02209f9df79ff933ab031023f060ebd010cc
momentts <marcosd3wow@gmail.com>
2020-11-20 00:31:40 -0300

    verificando correçoes 2189

--- script.js
+++ script.js
@@ -1,42 +1,9 @@
-const allInputs = document.querySelectorAll('.right-content input');
-function mudarRightContainer() {
-  const nome = allInputs[0].value;
-  const sobrenome = allInputs[1].value;
-  const email = allInputs[2].value;
-  const data = allInputs[4].value;
-  const genero = document.querySelector('input[type="radio"]:checked').value;
-  const rightContainer = document.querySelector('.right-content');
-  rightContainer.innerHTML = `
-      <p>Olá, ${nome} ${sobrenome}</p>
-      <p>Email: ${email}</p>
-      <p>Data nascimento: ${data}</p>
-      <p>Genero: ${genero}</p>
-  `;
+function entrar() {
+  const email = document.getElementById('user-email-phone').value;
+  alert(email);
 }
-function showInvalidos() {
-  document.querySelector('.mensagem-invalidos').style.display = 'block';
+function loginCheck() {
+  const btnEntrar = document.getElementById('button-login');
+  btnEntrar.addEventListener('click', entrar);
 }
-document.querySelector('#button-login').addEventListener('click', () => {
-  alert(document.querySelector('#user-email-phone').value);
-});
-document.querySelector('#facebook-register').addEventListener('click', () => {
-  for (let i = 0; i < allInputs.length - 1; i += 1) {
-    if (allInputs[i].value === '') {
-      showInvalidos();
-      return;
-    }
-  }
-  mudarRightContainer();
-});
-for (let i = 5; i < 7; i += 1) {
-  allInputs[i].addEventListener('change', () => {
-    if (allInputs[i].checked === true) {
-      allInputs[8].style.display = 'none';
-    }
-  });
-}
-allInputs[7].addEventListener('change', () => {
-  if (allInputs[7].checked === true) {
-    allInputs[8].style.display = 'block';
-  }
-});
+loginCheck();

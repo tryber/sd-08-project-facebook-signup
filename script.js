ac93177a1da77dfa542284af9e05cd9a43182b77
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2020-11-20 00:32:03 -0300

    Requisitos 9 e 10

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function alertEntrar() {
   const botao = document.getElementById('button-login');
   botao.addEventListener('click', function () {
-    alert('Email ou telefone');
+    const emailTelefone = document.getElementById('user-email-phone');
+    alert(emailTelefone.value);
   });
 }
 alertEntrar();

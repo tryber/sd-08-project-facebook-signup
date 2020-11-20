f0697202d5a4d744ae37348fc356040413cfff93
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2020-11-20 13:51:32 -0300

    arranging project

--- script.js
+++ script.js
@@ -1,72 +1,42 @@
 const botaoLogin = document.getElementById('button-login');
-const emailOuTelefone =  document.getElementById('user-email-phone');
+const emailOuTelefone = document.getElementById('user-email-phone');
 
-function alertBotao() { 
+function alertBotao () { 
   alert(emailOuTelefone.value);
 }
 botaoLogin.addEventListener('click', alertBotao);
 
+const botaoEnviar = document.querySelector('#facebook-register');
 
+botaoEnviar.addEventListener('click', function () {
 
-const botaoCadastrar = document.getElementById('facebook-register');
+  const inputsText = document.getElementsByClassName('inputs');
 
-const botaoenviar = document.querySelector('#facebook-register');
+  for (let index = 0; index < inputsText.length; index +=1 ) {
 
-botaoenviar.addEventListener('click', function () {
-
-  let inputsText = document.getElementsByClassName('inputs');
-
-  for (let index = 0; index < inputsText.length; index++) {
-
-    let value = inputsText[index]
+    const value = inputsText[index];
 
     if (value.required) {
       if (value.value === '') {
-        alert('Campos invalidos')
-        break
+        alert('Campos invalidos');
+        break;
       }
     }
   }
 });
 
+const persona = document.getElementById('botaopersonalizado');
+const radioPerso = document.getElementsByClassName('radioperso');
 
+for (let index = 0; index < radioPerso.length; index +=1 ) {
+  radioPerso[index].addEventListener('click', function () {
 
+    if (radioPerso[2].checked === true) {
 
-/* const persona = document.querySelector("#botaopersonalizado")
-
-const radiop = document.querySelector("#radioperso")
-
-radiop.addEventListener("click", function (event) {
-  
-  if(event.target.checked === true){
-
-    persona.style.display = "block"
-  }
-  else if (event.target.checked === false){
-    persona.style.display = "none"
- 
-  }
-
-}) */
-
-let persona = document.querySelector('#botaopersonalizado')
-
-let radiop = document.getElementsByClassName('radioperso')
-
-for (let index = 0; index < radiop.length; index++) {
-
-  radiop[index].addEventListener('click', function (event) {
-
-    if (radiop[2].checked === true) {
-
-      persona.style.display = 'block'
+      persona.style.display = 'block';
     }
     else {
-      persona.style.display = 'none'
+      persona.style.display = 'none';
     }
   })
 }
-
-
-
-

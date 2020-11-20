02c6ff6f86edb0d622df1906546b50b6c4ac08e4
Mateus Coury <mateuscoury@gmail.com>
2020-11-19 21:25:46 -0300

    validacao dos campos

--- script.js
+++ script.js
@@ -4,5 +4,25 @@ botaoLogin.addEventListener('click', function () {
   alert('Email ou telefone');
 });
 
-let inputsText = document.querySelectorAll("input");
-console.log(inputsText)
+
+let botaoenviar = document.querySelector("#facebook-register")
+
+botaoenviar.addEventListener("click",function(event){
+
+
+  let inputsText = document.getElementsByClassName("input");
+
+  console.log(inputsText)
+
+  for(let index = 0 ; index <inputsText.length ; index +=1){
+    let input = inputsText[index] ;
+    if(input.required){
+      if(input.value === ""){
+        alert("Campos inválidos")
+      }
+    }
+  }
+  
+
+
+})

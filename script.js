deafe982f42faea65a4cf57cb332908069eef553
Pedro Amaral <amaralph1995@gmail.com>
2020-11-20 14:59:40 -0300

    Corrige codeclimate

---
+++ script.js
@@ -0,0 +1,6 @@
+let buttonLogin = document.getElementById('button-login');
+let user = document.getElementById('user-email-phone');
+let pswd = document.getElementById('user-password');
+buttonLogin.addEventListener('click', function() {
+    alert(user.value, senha.value);
+})

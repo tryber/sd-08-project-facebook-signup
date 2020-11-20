7fd6b8f11086add898adb6acaef8edb1a8929248
Seu nome <luiza.dilly@gmail.com>
2020-11-20 17:25:26 -0300

    REQ 18 - iniciando requisito 18

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ buttonLogin.addEventListener('click', function () {
   const emailAndPhone = document.getElementById('user-email-phone').value;
   alert(emailAndPhone);
 });
+
+function validarCadastro () {
+  const formContent = document.getElementById('form-content');
+  const submitBtn = document.getElementById('facebook-register');
+  submitBtn.addEventListener('click', function (event){
+    for (let index = 0; index < formContent; index += 1){
+      if (formContent[i].value === ''){
+        
+      }
+    }
+  })
+}

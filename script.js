7482b3fd45c8807f4ddfd49b6854f2831fca628d
Nícolas Tozato <ntozato9@gmail.com>
2020-11-20 15:54:52 -0300

    teste prettier

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
-const buttonEnter = document.getElementById('button-login');
-const inputEmailPhone = document.getElementById('user-email-phone');
+const buttonEnter = document.getElementById("button-login");
+const inputEmailPhone = document.getElementById("user-email-phone");
 
 function showAlert() {
-  buttonEnter.addEventListener('click', function () {
+  buttonEnter.addEventListener("click", function () {
     alert(inputEmailPhone.value);
-});
-
-
-
-
-
-
   });
 }
+
 showAlert();
 
 function verifyText() {
-  const formText = document.querySelectorAll('#sign-up input[type=text], #sign-up input[type=password]');
+  const formText = document.querySelectorAll(
+    "#sign-up input[type=text], #sign-up input[type=password]"
+  );
   for (let index = 0; index < formText.length; index += 1) {
-    if (formText[index].value === '' || formText[index].value === null) {
+    if (formText[index].value === "" || formText[index].value === null) {
       return true;
     }
   }
@@ -26,7 +22,7 @@ function verifyText() {
 }
 
 function verifyRadio() {
-  const formRadio = document.querySelectorAll('#sign-up input[type=radio]');
+  const formRadio = document.querySelectorAll("#sign-up input[type=radio]");
   let radioEmpty = 0;
   for (let index = 0; index < formRadio.length; index += 1) {
     if (formRadio[index].checked === false) {
@@ -39,18 +35,18 @@ function verifyRadio() {
   return false;
 }
 
-const signUpForm = document.getElementById('sign-up');
-const errorMessage = document.createElement('p');
+const signUpForm = document.getElementById("sign-up");
+const errorMessage = document.createElement("p");
 signUpForm.appendChild(errorMessage);
 
 function completeSignUp() {
-  const buttonRegister = document.getElementById('facebook-register');
-  buttonRegister.addEventListener('click', function (event) {
+  const buttonRegister = document.getElementById("facebook-register");
+  buttonRegister.addEventListener("click", function (event) {
     event.preventDefault();
     if (verifyText() || verifyRadio()) {
-      errorMessage.innerText = 'Campos inválidos';
+      errorMessage.innerText = "Campos inválidos";
     } else {
-      errorMessage.innerText = '';
+      errorMessage.innerText = "";
     }
   });
 }

a2799cfebe3d0700805086f1b324bdf3768f590e
siebenei5 <siebenei7@gmail.com>
2020-11-19 21:03:22 -0300

    finally

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const buttonID = document.getElementsByClassName('button-login')[0];
+const buttonID = document.getElementById('button-login');
 
 buttonID.addEventListener('click', function () {
   const inputValue = document.getElementById('user-email-phone').value;

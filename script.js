e4eede96a22aa894ee99d1fb51f9b4dfa28451c0
emassaki <massakihirayama@gmail.com>
2020-11-19 22:43:16 -0300

    Adicionado requisito 20

--- script.js
+++ script.js
@@ -8,10 +8,29 @@ registerButton.addEventListener('click', function (event) {
       return;
     }
   }
+
+  const firstName = document.getElementById('firstname').value;
+  const lastName = document.getElementById('lastname').value;
+  const phoneEmail = document.getElementById('phone-email').value;
+  const birthdate = document.getElementById('birthdate').value;
+  const selectedGender = document.querySelector('input[type="radio"]:checked');
+  let gender;
+  if (selectedGender.id === 'custom') {
+    gender = document.getElementById('custom-gender').value;
+  } else {
+    gender = selectedGender.value;
+  }
+
+  const rightContent = document.querySelector('.right-content');
+  rightContent.innerHTML = '';
+  const paragraph = document.createElement('p');
+  paragraph.className = 'login-message';
+  paragraph.innerHTML = `Olá, ${firstName} ${lastName}! <br> Você se cadastrou com o e-mail/telefone ${phoneEmail}, com data de nascimento ${birthdate} e se identifica como ${gender}`;
+  rightContent.appendChild(paragraph);
 });
 
 const gender = document.querySelector('.gender');
-gender.addEventListener('click', function (event) {
+gender.addEventListener('click', function () {
   const radioButtons = document.querySelectorAll('input[type="radio"]')[2];
   if (radioButtons.checked) {
     document.getElementById('custom-gender').style.display = 'block';

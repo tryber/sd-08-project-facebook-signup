a95e669f3d9a24595631b7ab3e9830311d22e048
emassaki <massakihirayama@gmail.com>
2020-11-19 22:11:55 -0300

    Adicionados requisitos 18 e 19 - Verificaçao de campos vazios e aparecimento de um campo para outros generos

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-let registerButton = document.getElementById("facebook-register");
+const registerButton = document.getElementById('facebook-register');
+registerButton.addEventListener('click', function (event) {
+  event.preventDefault();
+  const input = document.querySelectorAll('[required]');
+  for (let index = 0; index < input.length; index += 1) {
+    if (!input[index].value) {
+      alert('Campos inválidos');
+      return;
+    }
+  }
+});
+
+const gender = document.querySelector('.gender');
+gender.addEventListener('click', function (event) {
+  const radioButtons = document.querySelectorAll('input[type="radio"]')[2];
+  if (radioButtons.checked) {
+    document.getElementById('custom-gender').style.display = 'block';
+  } else {
+    document.getElementById('custom-gender').style.display = 'none';
+  }
+});

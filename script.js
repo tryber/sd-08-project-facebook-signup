dd34d5e3313dde31d98d41e1cda853e0ec8f659d
Lucas Nascimento <lucasmvnascimento@gmail.com>
2020-11-19 22:02:19 -0300

    adequando code climate

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
 function checkTexts() {
-  let form = document.querySelectorAll(".form input");
+  const form = document.querySelectorAll('.form input');
   let flag1 = 0;
-  let flag2 = 0;
-  for (let index = 0; index < 5; index+=1){
-      if (form[index].value === ""){
-          flag1 = 1;
-      }
+  for (let index = 0; index < 5; index += 1){
+    if (form[index].value === ""){
+        flag1 = 1;
+    }
   }
-  return ([flag1,flag2])
+  return (flag1);
 }
 
-function checkForms(flags) {
-  let form = document.querySelectorAll(".form input");
-  for (let index = 5; index < form.length; index+=1){
-      if (form[index].checked === true){
-          flags[1] = 1;
-      }
+function checkForms(flag1) {
+  let flags = [];
+  let flag2 = 0;
+  flags.push(flag1,flag2);
+  const form = document.querySelectorAll('.form input');
+  for (let index = 5; index < form.length; index += 1){
+    if (form[index].checked === true){
+        flags[1] = 1;
+    }
   }
   return (flags);
 }
 
 function clickSubmit(event) {
-  let flags = checkTexts();
-  let check = checkForms(flags);
-  if (check[0] === 1 || check[1] === 0){
-    alert("Campos inválidos");
+  const flags = checkTexts();
+  const check = checkForms(flags);
+  if (check[0] === 1 || check[1] === 0) {
+    alert('Campos inválidos');
     event.preventDefault();
   }
 }
-document.querySelector(".facebook-register").addEventListener("click",clickSubmit);
+document.querySelector('.facebook-register').addEventListener('click', clickSubmit);

9b7d825f0ff06703a0f6a88386a50675388ec449
Mario Pardo <mariio.pardojr@gmail.com>
2020-11-20 19:08:53 -0400

    Requisito 19

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ const inputEmailPhone = document.getElementById('user-email-phone');
 btnLogin.addEventListener('click', function () {
   alert(inputEmailPhone.value);
 });
+
+const customRadio = document.querySelectorAll('.checked-gender');
+const inputOption = document.getElementById('option');
+customRadio.forEach((element) => {
+  element.addEventListener('change', function () {
+    if (customRadio[2].checked) {
+      inputOption.classList.remove('input-display');
+      inputOption.setAttribute('required', '');
+    } else {
+      inputOption.classList.add('input-display');
+      inputOption.removeAttribute('required');
+    }
+  });
+});
+
+// const submitButton = document.getElementById('facebook-register');
+// submitButton.addEventListener('click', function () {
+//   const userRegister = document.querySelectorAll('input');
+//   const userName = document.getElementsByClassName('hello-user');
+// });

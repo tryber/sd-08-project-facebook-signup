2581000b0fc4696b2799597df200d46f2982e9a4
jnpefo <jnpefo@gmail.com>
2020-11-20 14:49:12 -0300

    Req 18, tentativa 1

--- script.js
+++ script.js
@@ -5,4 +5,14 @@ function funcaoAlert() {
 document.getElementById('button-login').addEventListener('click', funcaoAlert);
 
 
-// const allInputs = document.querySelectorAll('.right-content input');
+const allInputs = document.querySelectorAll('.right-content input');
+
+document.querySelector('#facebook-register').addEventListener('click', function() {
+  for (let i = 0; i < allInputs.length - 1; i += 1) {
+    if (allInputs[i].value === '') {
+      showInvalidos();
+      return;
+    }
+  }
+  mudarRightContainer();
+});

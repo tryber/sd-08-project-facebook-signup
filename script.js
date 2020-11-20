b0cbbb70178729343aebdeaef0caf092625f365d
Natanael Neto <nesn.gamedev@gmail.com>
2020-11-20 11:29:01 -0300

    C17 - Req 19, campo personalizado, enviado

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const btnLogin = document.getElementById('button-login');
+const rbPers = document.getElementById('personalizado');
+const persInput = document.getElementById('persInput');
 let userId;
 
 btnLogin.addEventListener('click', function () {
   userId = document.getElementById('user-email-phone');
   alert(userId.value);
 });
+
+function DisableInput () {
+  if (rbPers.checked) {
+    persInput.innerHTML = '<input type="text" name="gender-custom" id="genderCustom" placeholder="Gênero (Opcional)">';
+    return;
+  }
+  persInput.innerHTML = '';
+}
+
+rbPers.addEventListener('change', DisableInput);
+
+DisableInput();

f0ff468f0c6b9f6468c6c5847354081ca252377f
wagnerberna <cabelo29007@gmail.com>
2020-11-20 07:28:50 -0300

    req07 função botão

---
+++ script.js
@@ -0,0 +1,6 @@
+function fBtnLogin() {
+  console.log('clicou!!!');
+  let inputUserEmail = document.getElementById('user-email-phone');
+  console.log(inputUserEmail);
+  alert(inputUserEmail.value);
+}

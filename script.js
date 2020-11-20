b6305d24313bcba3a89453f8774da710cf78f2ab
emassaki <massakihirayama@gmail.com>
2020-11-19 23:32:49 -0300

    Realizado ajustes nos requisitos 18 ao 20

--- script.js
+++ script.js
@@ -1,40 +1,33 @@
+function changeRightContainer() {
+  const firstName = document.getElementById('firstname').value;
+  const lastName = document.getElementById('lastname').value;
+  const phoneEmail = document.getElementById('phone-email').value;
+  const birthdate = document.getElementById('birthdate').value;
+  const selectedGender = document.querySelector('input[type="radio"]:checked').value;
+
+  const rightContent = document.querySelector('.right-content');
+  rightContent.innerHTML = `<p>Olá, ${firstName} ${lastName}!</p> <p>E-mail ou telefone: ${phoneEmail}</p> <p>Data de nascimento: ${birthdate}</p> <p>Gênero: ${selectedGender}</p>`;
+}
+
 const registerButton = document.getElementById('facebook-register');
 registerButton.addEventListener('click', function (event) {
   event.preventDefault();
   const input = document.querySelectorAll('[required]');
   for (let index = 0; index < input.length; index += 1) {
     if (!input[index].value) {
-      alert('Campos inválidos');
+      document.querySelector('.invalid').style.display = 'block';
       return;
     }
   }
-
-  const firstName = document.getElementById('firstname').value;
-  const lastName = document.getElementById('lastname').value;
-  const phoneEmail = document.getElementById('phone-email').value;
-  const birthdate = document.getElementById('birthdate').value;
-  const selectedGender = document.querySelector('input[type="radio"]:checked');
-  let gender;
-  if (selectedGender.id === 'custom') {
-    gender = document.getElementById('custom-gender').value;
-  } else {
-    gender = selectedGender.value;
-  }
-
-  const rightContent = document.querySelector('.right-content');
-  rightContent.innerHTML = '';
-  const paragraph = document.createElement('p');
-  paragraph.className = 'login-message';
-  paragraph.innerHTML = `Olá, ${firstName} ${lastName}! <br> Você se cadastrou com o e-mail/telefone ${phoneEmail}, com data de nascimento ${birthdate} e se identifica como ${gender}`;
-  rightContent.appendChild(paragraph);
+  changeRightContainer();
 });
 
 const gender = document.querySelector('.gender');
 gender.addEventListener('click', function () {
   const radioButtons = document.querySelectorAll('input[type="radio"]')[2];
   if (radioButtons.checked) {
-    document.getElementById('custom-gender').style.display = 'block';
+    document.getElementById('gender-custom').style.display = 'block';
   } else {
-    document.getElementById('custom-gender').style.display = 'none';
+    document.getElementById('gender-custom').style.display = 'none';
   }
 });

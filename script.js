92e179a1f3df06f210f2d27994221f42db6c0e0e
Joao <joaocarlitoneto@gmail.com>
2020-11-20 13:51:51 -0300

    Req 18

--- script.js
+++ script.js
@@ -5,38 +5,33 @@ btnLogin.addEventListener('click', function () {
 });
 
 const buttonCadastrar = document.querySelector('#facebook-register');
-const form = document.querySelector('.form-user');
+// const form = document.querySelector('.form-user');
 const right = document.querySelector('.right-content');
 
 function cadastrar() {
   buttonCadastrar.addEventListener('click', function (event) {
     event.preventDefault();
-
     const inputs = document.querySelectorAll('.form-user input[type=text],.box2 input[type=password]');
-    let nome = inputs[0].value;
-    let sobrenome = inputs[1].value;
-    let email = inputs[2].value;
-    let data = inputs[4].value;
-    let genero = document.querySelector('input[type="radio"]:checked').value; 
-
-    let resultadoUsuario = '';
-    let p = document.createElement('p');
+    const nome = inputs[0].value;
+    const sobrenome = inputs[1].value;
+    const email = inputs[2].value;
+    const data = inputs[4].value;
+    const genero = document.querySelector('input[type="radio"]:checked').value;
+    // let resultadoUsuario = '';
+    const p = document.createElement('p');
     for (let index = 0; index < inputs.length; index += 1) {
-      const arrayInput = inputs[index];         
+      const arrayInput = inputs[index];
       if (arrayInput.value === '' || arrayInput.type === 'radio') {
         arrayInput.className = 'erro';
-        arrayInput.placeholder = 'Campos inválidos';        
-        p.innerText = 'Campos inválidos';        
+        arrayInput.placeholder = 'Campos inválidos';
+        p.innerText = 'Campos inválidos';
       } else {
         arrayInput.className = '';
-        resultadoUsuario += arrayInput.value;
-        right.innerHTML = `<p>Olá, ${nome} ${sobrenome}</p> 
-        <p>Email: ${email}</p>
-        <p>Data de Nascimento: ${data}</p>
-        <p>Gênero: ${genero}</p>`;
-      }      
+        // resultadoUsuario += arrayInput.value;
+        right.innerHTML = `<p>Olá, ${nome} ${sobrenome}</p> <p>Email: ${email}</p> <p>Data de Nascimento: ${data}</p> <p>Gênero: ${genero}</p>`;
+      }
     }
-    right.appendChild(p);    
+    right.appendChild(p);
   });
 }
 cadastrar();

616beb2005c85871d2704057216c92cee8905886
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2020-11-20 19:34:39 -0300

    requisito 19 v01

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function loginAlert() {
   alert(email.value);
 }
 loginBtn.addEventListener('click', loginAlert);
+const personalizado = document.getElementById('other');
+const radioWrapper = document.getElementsByClassName('radio-wrapper')[0];
+function generoPersonalizado() {
+  const divDoPersonalizado = document.createElement('div');
+  radioWrapper.appendChild(divDoPersonalizado);
+  const insiraPersonalizado = document.createElement('input');
+  insiraPersonalizado.type = 'text';
+  insiraPersonalizado.name = 'gender-custom';
+  insiraPersonalizado.placeholder = 'Gênero (opcional)';
+  insiraPersonalizado.id = 'insira-personalizado';
+  divDoPersonalizado.appendChild(insiraPersonalizado);
+}
+personalizado.addEventListener('click', generoPersonalizado);

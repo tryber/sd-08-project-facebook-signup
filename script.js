0b949720f33fed31a2950ee97f83dba356f3ab56
Ruben Albertassi <albertassi88@gmail.com>
2020-11-20 19:30:20 -0300

    Alteracao no requisito 18

--- script.js
+++ script.js
@@ -13,14 +13,11 @@ function cadastrar() {
     const nome = inputs[0].value;
     const sobrenome = inputs[1].value;
     const email = inputs[2].value;
-    const data = inputs[4].value;
-    
-   
+    const data = inputs[4].value;   
     const p = document.createElement('p');
-    let resultado = "";
+    let resultado = '';
     const genero = document.querySelector('input[type="radio"]:checked').value;
-    for (let index = 0; index < inputs.length; index += 1) {
-      
+    for (let index = 0; index < inputs.length; index += 1) {      
       const arrayInput = inputs[index];
       if (arrayInput.value === '' || genero === '') {
         arrayInput.className = 'erro';
@@ -29,12 +26,11 @@ function cadastrar() {
       } else {
         arrayInput.className = '';
       }
-      right.appendChild(p);      
+      right.appendChild(p);
     }
-    if (resultado !== ""){
-                 
-    }else {            
-    right.innerHTML = `<p>Olá, ${nome} ${sobrenome}</p> <p>Email: ${email}</p> <p>Data de Nascimento: ${data}</p> <p>Gênero: ${genero}`;
+    if (resultado !== ""){                 
+    } else {            
+      right.innerHTML = `<p>Olá, ${nome} ${sobrenome}</p> <p>Email: ${email}</p> <p>Data de Nascimento: ${data}</p> <p>Gênero: ${genero}`;
     }
   });
 }

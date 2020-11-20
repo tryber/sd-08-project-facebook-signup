00937b088769cc6f6dfb2404b640cab2506521c3
Ronald Jr <rpintojunior@gmail.com>
2020-11-20 20:39:52 -0300

    Corrige requisitos 19 e 20

--- script.js
+++ script.js
@@ -29,25 +29,38 @@ mas.addEventListener('change', checkRadio);
 const fem = document.querySelector('#fem');
 fem.addEventListener('change', checkRadio);
 
+function welcome() {
+  const firstName = document.getElementById('firstname').value;
+  const lastName = document.getElementById('lastname').value;
+  const birthdate = document.getElementById('birthdate').value;
+  const phoneEmail = document.getElementById('phone_email').value;
+  const gender = document.querySelectorAll('input:checked');
+  const welcomeUser = `Olá, ${firstName} ${lastName}, você é do sexo: ${gender[0].value} e seu email/fone é :  ${phoneEmail} e seu aniversario é: ${birthdate}`;
+  const contentReturn = document.querySelector('.right-content');
+  for (let index = 0; index < contentReturn.children.length; index += 1) {
+    contentReturn.children[index].style.display = 'none';
+  }
+  contentReturn.innerHTML = welcomeUser;
+}
+const male = document.querySelector('#mas');
+male.addEventListener('change', checkRadio);
+const female = document.querySelector('#fem');
+female.addEventListener('change', checkRadio);
 const cadast = document.querySelector('#facebook-register');
 cadast.addEventListener('click', () => {
   const formsValue = document.querySelectorAll('#register-form')[0];
-  for (let index = 0; index < 5; index += 1) {
-    if (
-      document.querySelectorAll('input').length < 1 ||
-      (inputGender.style.display === 'block' && inputGender.value === '') ||
-      formsValue[index].value === ''
-    ) {
+  const input = document.querySelectorAll('input:checked');
+  let ok;
+  for (let i = 0; i < 5; i += 1) {
+    if (input.length < 1 ||
+  (inputGender.style.display === 'block' && inputGender.value === '')
+  || formsValue[i].value === '') {
       document.querySelector('.chec-return').style.display = 'block';
     } else {
-      const firstName = document.getElementById('firstname').value;
-      const lastName = document.getElementById('lastname').value;
-      const phoneMail = document.getElementById('phone_email').value;
-      const birthDate = document.getElementById('birthdate').value;
-      const gender = document.querySelector('input[type=radio]:checked').value;
-      const welcomeUser = `Olá, ${firstName} ${lastName} ${gender} ${phoneMail} ${birthDate}`;
-      document.querySelector('.right-content').innerHTML = `${welcomeUser}`;
+      ok = true;
     }
   }
-  return false;
+  if (ok) {
+    welcome();
+  }
 });

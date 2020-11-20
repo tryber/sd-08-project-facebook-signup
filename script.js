27605c4f682d22853c3574651d87e916d6e77379
Mateus Coury <mateuscoury@gmail.com>
2020-11-19 21:35:35 -0300

    alteracoes codeclimate2

--- script.js
+++ script.js
@@ -9,15 +9,12 @@ const botaoenviar = document.querySelector('#facebook-register');
 
 botaoenviar.addEventListener('click', function () {
 
-
   const inputsText = document.getElementsByClassName('input');
 
-  console.log(inputsText)
-  
-  for(let index = 0 ; index <inputsText.length ; index +=1) {
+  for (let index = 0; index < inputsText.length; index += 1) {
     let input = inputsText[index];
-    if(input.required) {
-      if(input.value === '') {
+    if (input.required) {
+      if (input.value === '') {
         alert('Campos inválidos');
       }
     }

e17fdd14f22b840d62be9638be281495e6ab3552
Mateus Coury <mateuscoury@gmail.com>
2020-11-20 12:53:56 -0300

    implementacao do botao persona

--- script.js
+++ script.js
@@ -8,13 +8,60 @@ botaoLogin.addEventListener('click', function () {
 const botaoenviar = document.querySelector('#facebook-register');
 
 botaoenviar.addEventListener('click', function () {
-  const inputsText = document.getElementsByClassName('input');
-  for (let index = 0; index < inputsText.length; index += 1) {
-    const input = inputsText[index];
-    if (input.required) {
-      if (input.value === '') {
-        alert('Campos inválidos');
+
+  let inputsText = document.getElementsByClassName('inputs');
+
+  for (let index = 0; index < inputsText.length; index++) {
+
+    let value = inputsText[index]
+
+    if (value.required) {
+      if (value.value === "") {
+        alert("Campos invalidos")
+        break
       }
     }
   }
 });
+
+
+
+
+/* const persona = document.querySelector("#botaopersonalizado")
+
+const radiop = document.querySelector("#radioperso")
+
+radiop.addEventListener("click", function (event) {
+  
+  if(event.target.checked === true){
+
+    persona.style.display = "block"
+  }
+  else if (event.target.checked === false){
+    persona.style.display = "none"
+ 
+  }
+
+}) */
+
+let persona = document.querySelector("#botaopersonalizado")
+
+let radiop = document.getElementsByClassName("radioperso")
+
+for (let index = 0; index < radiop.length; index++) {
+
+  radiop[index].addEventListener("click", function (event) {
+
+    if (radiop[2].checked === true) {
+
+      persona.style.display = "block"
+    }
+    else {
+      persona.style.display = "none"
+    }
+  })
+}
+
+
+
+

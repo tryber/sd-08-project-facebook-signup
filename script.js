bcfcc8f75de0c325a5eb918bbc16a87c602d93a9
Douglas Ferreira <dodofp.df@gmail.com>
2020-11-20 17:40:27 -0300

    Teste alerta

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
 const entrar = document.getElementById('button-login');
 
 function alerta() {
-  alert('"Email ou telefone"');
+  alert("Email ou telefone");
 }
 entrar.addEventListener('click', alerta);
 
-// const cadastre = document.getElementById('facebook-register');
+const cadastre = document.getElementById('facebook-register');
 
-// const form = document.querySelectorAll('.form input');
+const form = document.querySelectorAll('.form input');
 
 
-// cadastre.addEventListener('click', function () {
-//   for (let index = 0; index < form.children.length ; index +=1 ) {
-//     var contadorBranco = 0
-//     if (form[index].length == 0) {
-//       contadorBranco = contadorBranco + 1;
 
-//     }
-//   }
-//   if(contadorBranco != 0){
-//     alert('Cadastro Efetuado')
-//   } else {
-//     alert('Cadastro inválido')
-//   }
-// })
+cadastre.addEventListener('click', function () {
+  for (let index = 0; index < form.length ; index +=1 ) {
+    var contadorBranco = 0
+    if (form[index].value == 0) {
+      contadorBranco = contadorBranco + 1;
+
+    }
+  }
+  if(contadorBranco != 0){
+    alert('Cadastro Efetuado')
+  } else {
+    alert('Cadastro inválido')
+  }
+})
 
 // cadastre.addEventListener('click', function (){
 //   if(form[0].length < 0 ){

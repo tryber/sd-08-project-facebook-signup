9c10d3a6e8ad8f35128e580d0d1adacb032b822e
Tandy <tandyfw@gmail.com>
2020-11-21 10:44:47 -0300

    Requisito 19 html + js

--- script.js
+++ script.js
@@ -14,12 +14,35 @@ function validateFields() {
     const inputs = document.querySelectorAll('.cadastro input');
     for (let index = 0; index < inputs.length; index += 1) {
       if (inputs[index].value === '') {
-        const p = document.createElement('p');
-        p.innerHTML = 'Campos inválidos';
-        document.querySelector('.cadastro').appendChild(p);
+        const span = document.createElement('span');
+        span.innerHTML = 'Campos inválidos';
+        span.id = 'invalido';
+        span.style.color = 'red';
+        if (document.querySelector('#invalido') === null) {
+          document.querySelector('.cadastro').appendChild(span);
+          break;
+        }
         break;
       }
     }
   });
 }
 validateFields();
+
+function addGender() {
+  const radio = document.querySelectorAll('input[type=radio]');
+  const divGender = document.querySelector('.genderOpcao');
+  divGender.addEventListener('click', function () {
+    if (radio[2].checked) {
+      const input = document.createElement('input');
+      input.type = 'text';
+      input.name = 'gender-custom';
+      input.placeholder = 'Gênero (opcional)';
+      input.id = 'genderPersonalizado'
+      if (document.querySelector('#genderPersonalizado') === null) {
+        divGender.appendChild(input);
+      }
+    }
+  });
+}
+addGender();

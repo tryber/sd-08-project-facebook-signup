433ddb0cd8d78d981c71f409302cf960b4a93fde
Douglas Ferreira <dodofp.df@gmail.com>
2020-11-21 15:57:20 -0300

    Metade requisito 20

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const entrar = document.getElementById('button-login');
 const invalido = document.querySelector('.invalidos');
+const rightContent = document.querySelector('.right-content');
+const mainContent = document.querySelector('.main-content');
 
 entrar.addEventListener('click', function () {
   const email = document.getElementById('user-email-phone');
@@ -22,7 +24,19 @@ const cadastre = document.getElementById('facebook-register');
 
 const form = document.querySelectorAll('.form input');
 
-// const formulario = document.querySelector('.form');
+function eraseRightContent() {
+  rightContent.remove();
+}
+
+function createRightContent() {
+  const newRightDiv = document.createElement('div');
+  newRightDiv.className = 'right-content';
+  mainContent.appendChild(newRightDiv);
+  newRightDiv.innerText = `Olá, ${form[0].value} ${form[1].value} \n
+  ${form[2].value} \n
+  ${form[4].value} \n
+  `;
+}
 
 cadastre.addEventListener('click', function (event) {
   let contador = 0;
@@ -34,5 +48,8 @@ cadastre.addEventListener('click', function (event) {
   if (contador !== 0) {
     event.preventDefault();
     invalido.innerHTML = 'Campos inválidos';
+  } else {
+    eraseRightContent();
+    createRightContent();
   }
 });

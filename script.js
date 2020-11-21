75269042994a0eb2efb11790e121c182a5c298b5
Hugo Andrade Braga <hugobrr@gmail.com>
2020-11-21 15:48:35 -0300

    personalizado com aba adicional funcionando e temos 90% dos requisitos ja completos

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const buttonEntrar = document.querySelector('#button-login');
 const emailOuTelefone = document.querySelector('#user-email-phone');
 const buttonCadastro = document.querySelector('#facebook-register');
 const novoUsuario = document.querySelector('.novoUsuario');
-// const rapidoFacil = document.querySelector('.quick-easy');
+const personalizado = document.querySelector('.personalizado');
+const generos = document.querySelector('.generos');
+let selecionado = document.querySelector('input[name="gender"]:checked').value;
 
 buttonEntrar.addEventListener('click', function () {
   alert(emailOuTelefone.value);
@@ -15,5 +17,14 @@ buttonCadastro.addEventListener('click', function (event) {
   const resultado = novoUsuario.checkValidity();
   if (resultado === false) {
     novoUsuario.appendChild(mensagemErro);
+  }
+});
+
+generos.addEventListener('click', function () {
+  selecionado = document.querySelector('input[name="gender"]:checked').value;
+  if (selecionado === 'Personalizado') {
+    personalizado.style.display = 'flex';
+  } else {
+    personalizado.style.display = 'none';
   }
 });

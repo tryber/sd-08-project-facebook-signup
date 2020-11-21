1bf2e404d377fb7fb469fb763c854c30f0ec3239
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2020-11-21 09:05:06 -0300

    clone após 80% garantido

---
+++ script.js
@@ -0,0 +1,8 @@
+function alerta() {
+  const botao = document.getElementById("button-login");
+  botao.addEventListener("click", function () {
+    const email = document.getElementById("user-email-phone");
+    alert(email.value);
+  });
+}
+alerta();

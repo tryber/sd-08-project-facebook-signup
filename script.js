36a15617845939748d923549f7837f2a4566e7ee
Ivanildo Cândido <systechtecnologia@gmail.com>
2020-11-20 22:19:12 -0300

    Enviando validaçoes

--- script.js
+++ script.js
@@ -19,3 +19,73 @@ function otherGender() {
   });
 }
 otherGender();
+function printResult(user) {
+  const name = document.createElement('h4');
+  const email = document.createElement('h4');
+  const birthdate = document.createElement('h4');
+  const gender = document.createElement('h4');
+  const div = document.createElement('div');
+  name.innerHTML += `Olá, ${user.name} ${user.fullName}`;
+  div.appendChild(name);
+  email.innerHTML += `Email ou Telefone: ${user.email}`;
+  div.appendChild(email);
+  birthdate.innerHTML += `Data de Nascimento: ${user.birthdate}`;
+  div.appendChild(birthdate);
+  gender.innerHTML += `Gênero: ${user.gender}`;
+  div.appendChild(gender);
+  return div;
+}
+
+function getValues() {
+  const name = document.querySelector('.name');
+  const fullName = document.querySelector('.last-name');
+  const email = document.querySelector('.email');
+  const pass = document.querySelector('.pass');
+  const male = document.querySelector('#male');
+  const female = document.querySelector('#female');
+  const personal = document.querySelector('#personal');
+  const birthdate = document.querySelector('#birthdate');
+  const user = {};
+  user.name = name.value;
+  user.fullName = fullName.value;
+  user.email = email.value;
+  user.pass = pass.value;
+  user.birthdate = birthdate.value;
+  if (male.checked) {
+    user.gender = 'Masculino';
+  }
+  if (female.checked) {
+    user.gender = 'Feminino';
+  }
+  if (personal.checked) {
+    user.gender = 'Personalizado';
+  }
+  return user;
+}
+function validate() {
+  const user = Object.values(getValues());
+  let result = true;
+  for (let i = 0; i < user.length; i += 1) {
+    if (user[i] === '') {
+      result = false;
+    }
+  }
+  return result;
+}
+
+function signIn() {
+  const btnRegister = document.getElementById('facebook-register');
+  const form = document.querySelector('.form-register');
+  btnRegister.addEventListener('click', function (event) {
+    const rightContent = document.querySelector('.right-content');
+    event.preventDefault();
+    if (validate() === true) {
+      const user = getValues();
+      rightContent.innerHTML = '';
+      rightContent.appendChild(printResult(user));
+    } else {
+      form.innerHTML += 'Campos inválidos';
+    }
+  });
+}
+signIn();

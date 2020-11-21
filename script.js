d6aa0a3290f76b6b311c24b35567982af420c506
Tandy <tandyfw@gmail.com>
2020-11-21 09:35:13 -0300

    Requisito 18 html + js

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ function btnLogin() {
   });
 }
 btnLogin();
+
+function validateFields() {
+  const btnCadastrar = document.querySelector('#facebook-register');
+  btnCadastrar.addEventListener('click', function (event) {
+    event.preventDefault();
+    const inputs = document.querySelectorAll('.cadastro input');
+    for(let index = 0; index < inputs.length; index += 1) {
+      if (inputs[index].value === "") {
+        alert('Campos inválidos');
+        break;
+      }
+    }
+  });
+}
+validateFields();

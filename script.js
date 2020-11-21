c86f91848dea21a572aa7529fa67b7147f4a8826
Vinicius <viniciussilvaribeiro@gmail.com>
2020-11-21 10:08:25 -0300

    finalizado 18 e tentando 19

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
 function alerta() {
-  const botao = document.getElementById('button-login');
-  botao.addEventListener('click', function () {
-    const email = document.getElementById('user-email-phone');
+  const botao = document.getElementById("button-login");
+  botao.addEventListener("click", function () {
+    const email = document.getElementById("user-email-phone");
     alert(email.value);
   });
 }
 alerta();
 
-function validate () {
+function validate() {
   const formList = document.querySelectorAll("input");
-  let contador = 0
+  let contador = 0;
   let botao = document.getElementById("facebook-register");
-  botao.addEventListener('click', function() {
-    for (let index = 2; index < formList.length; index +=1) {
+  botao.addEventListener("click", function () {
+    for (let index = 2; index < formList.length; index += 1) {
       let valorCampos = formList[index].value;
       if (valorCampos === "") {
         contador += 1;
       }
     }
-    if (contador > 0 ){
-      alert("Campos inválidos")
-    }    
-  })
+    if (contador > 0) {
+      alert("Campos inválidos");
+    }
+  });
 }
 validate();
 
 let personal = document.getElementById("personal");
-personal.addEventListener('click', function(){
+personal.addEventListener("click", function () {
   if (personal.checked === true) {
     let inputPersonal = personal.appendChild(document.createElement("input"));
     inputPersonal.className = "input-personal";
     inputPersonal.type = "text";
     inputPersonal.placeholder = "Gênero (opcional)";
-    inputPersonal.name = "gender-custom"
+    inputPersonal.name = "gender-custom";
   }
-})
+});

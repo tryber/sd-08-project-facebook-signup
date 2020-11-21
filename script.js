3072dd8334aac944b320609b93803a3487236998
Vinicius <viniciussilvaribeiro@gmail.com>
2020-11-21 10:05:33 -0300

    finalizado 18 e tentando 19

--- script.js
+++ script.js
@@ -6,3 +6,32 @@ function alerta() {
   });
 }
 alerta();
+
+function validate () {
+  const formList = document.querySelectorAll("input");
+  let contador = 0
+  let botao = document.getElementById("facebook-register");
+  botao.addEventListener('click', function() {
+    for (let index = 2; index < formList.length; index +=1) {
+      let valorCampos = formList[index].value;
+      if (valorCampos === "") {
+        contador += 1;
+      }
+    }
+    if (contador > 0 ){
+      alert("Campos inválidos")
+    }    
+  })
+}
+validate();
+
+let personal = document.getElementById("personal");
+personal.addEventListener('click', function(){
+  if (personal.checked === true) {
+    let inputPersonal = personal.appendChild(document.createElement("input"));
+    inputPersonal.className = "input-personal";
+    inputPersonal.type = "text";
+    inputPersonal.placeholder = "Gênero (opcional)";
+    inputPersonal.name = "gender-custom"
+  }
+})

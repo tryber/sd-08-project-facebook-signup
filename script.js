1dd3cc8c0d5c6c4d1f5266795fda870e131f6a9f
wagnerberna <cabelo29007@gmail.com>
2020-11-22 08:09:10 -0300

    correção codeclimate04

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ function fBtnLogin() {
   const inputUserEmail = document.getElementById('user-email-phone');
   alert(inputUserEmail.value);
 }
+  const firstname = ('');
+  const lastname = ('');
+  const phoneEmail = ('');
+  const password = ('');
+  const birthdate = ('');
+  const gender = ('');
 document.getElementById('button-login').addEventListener('click', fBtnLogin);
 function fBtnRegister() {
   const firstname = formSignin.firstname.value;

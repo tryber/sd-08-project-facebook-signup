f57ead5fc517216d818007eccfaed86e9481a25e
Mario Pardo <mariio.pardojr@gmail.com>
2020-11-22 14:52:19 -0400

    Ajustes e verificacao de data

--- script.js
+++ script.js
@@ -7,26 +7,23 @@ btnLogin.addEventListener('click', function () {
 });
 // Verifica os campos
 const submitButton = document.getElementById('facebook-register');
-const getInputBirthday = document.querySelector('#birthdate');
 const getErrorWarning = document.getElementById('error-warning');
+function dateVerify() {
+  const getInputBirthday = document.querySelector('#birthdate').value;
+  const arrayDate = getInputBirthday.split('/');
+  const day = arrayDate[0];
+  const month = arrayDate[1];
+  const year = arrayDate[2];
+  if (!(day > 0 && day <= 31 && month > 0 && month <= 12 && year > 0)) {
+    document.querySelector('#birthdate').classList.add('error');
+    getErrorWarning.classList.remove('input-display');
+  }
+}
 submitButton.addEventListener('click', function (event) {
   event.preventDefault();
-  const getInputBirthday = document.querySelector('#birthdate').value;
-  let arrayDate = getInputBirthday.split('/')
-  let day = arrayDate[0];
-  let month = arrayDate[1];
-  let year = arrayDate[2];
-  if (getInputBirthday[2] == '/' && getInputBirthday[5] == '/') {
-    if (!(day > 0 && day <= 31 && month > 0 && month <= 12 && year > 0)) {
-      document.querySelector('#birthdate').classList.add('error');
-      getErrorWarning.classList.remove('input-display');
-    }
-  }
+  dateVerify();
 });
-
-
-
-// Monstra um input de texto ao escolher personalizado no campo de gênero
+// Mostra um input de texto ao escolher personalizado no campo de gênero
 
 const customRadio = document.querySelectorAll('.checked-gender');
 const inputOption = document.getElementById('option');
@@ -41,7 +38,6 @@ customRadio.forEach((element) => {
     }
   });
 });
-
 // const submitButton = document.getElementById('facebook-register');
 // submitButton.addEventListener('click', function () {
 //   const userRegister = document.querySelectorAll('input');

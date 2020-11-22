94520c0a82236c018e065ef036e0eb40cbd2fc54
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2020-11-22 13:13:19 -0300

    adding requisit 20

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ botaoLogin.addEventListener('click', alertBotao);
 
 const botaoEnviar = document.querySelector('#facebook-register');
 const divVazia = document.getElementById('div-vazia');
-botaoEnviar.addEventListener('click', function () {
+botaoEnviar.addEventListener('click', function (event) {
   const inputsText = document.getElementsByClassName('inputs');
+  event.preventDefault();
+  divVazia.innerHTML = '';
   for (let index = 0; index < inputsText.length; index += 1) {
     const campoInput = inputsText[index];
     if (campoInput.required) {
@@ -18,6 +20,7 @@ botaoEnviar.addEventListener('click', function () {
       }
     }
   }
+  if(divVazia.innerHTML === '') exibirDados();
 });
 
 const botaoPer = document.querySelector('#botaopersonalizado');
@@ -26,9 +29,42 @@ const radioPerso = document.getElementsByClassName('radioperso');
 for (let index = 0; index < radioPerso.length; index += 1) {
   radioPerso[index].addEventListener('click', function () {
     if (radioPerso[2].checked === true) {
-      botaoPer.innerHTML = '<input type="text" name="gender-custom" placeholder="Gênero (opcional)">';
+      botaoPer.innerHTML =
+        '<input type="text" name="gender-custom" placeholder="Gênero (opcional)">';
     } else {
       botaoPer.innerHTML = '';
     }
   });
+}
+
+const rightContent = document.querySelector('.right-content');
+const nome = document.querySelector('[name=firstname]');
+const sobreNome = document.querySelector('[name=lastname]');
+const mostreNome = document.createElement('p');
+const emailOuTel = document.querySelector('[name=phone_email]');
+const dataNasc = document.querySelector('[name=birthdate]');
+const genero = document.getElementsByClassName('radioperso');
+
+
+function pegarRadioValor(name){
+  
+  const rads = document.getElementsByName(name);
+  for(let index = 0; i < rads.length; i += 1){
+    if(rads[index].checked){
+      return rads[index].value;
+    }  
+  }
+    
+  return null;
+}
+
+function exibirDados() {
+
+  let genero = pegarRadioValor('gender');
+  let str = ''
+  str += `Olá, ${nome.value} ${sobreNome.value}<br><br>`;
+  str += `${emailOuTel.value} <br><br>`;
+  str += `${dataNasc.value} <br><br>`;
+  str += `${genero}`;
+  rightContent.innerHTML = str;
 }

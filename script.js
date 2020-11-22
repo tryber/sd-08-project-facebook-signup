36f2e30ba5633320afe19ac71e400cb73fb65797
ThamirysSantos <thamirysgoncalves.prog@gmail.com>
2020-11-22 20:39:59 -0300

    frase de cadastro

--- script.js
+++ script.js
@@ -4,8 +4,22 @@ const emailPhone = document.getElementById('user-email-phone');
 const genderUndefined = document.getElementById('undefined');
 const genderForm = document.querySelector('.radio-form');
 const buttonCadastro = document.getElementById('facebook-register');
-const required = document.querySelectorAll('[required]');
 const rightContentForm = document.getElementById('right-content-form');
+const rightContentDiv = document.querySelector('.right-content');
+const genderInputs = genderForm.getElementsByTagName('input');
+const allInputs = rightContentForm.getElementsByTagName('input');
+let gender = '';
+
+function atribuiValorGenders (genderInputs) {
+  for (let index = 0; index < genderInputs.length; index +=1) {
+    if (genderInputs[index].checked == true) {
+      gender = genderInputs[index].value;
+    }
+  }
+  return gender;
+};
+
+atribuiValorGenders (genderInputs);
 
 buttonLogin.addEventListener('click', function (event) {
   event.preventDefault();
@@ -20,12 +34,32 @@ genderUndefined.addEventListener('click', function () {
   genderForm.appendChild(genderCustom);
 });
 
-buttonCadastro.addEventListener('click', function () {
+buttonCadastro.addEventListener('click', function (event) {
+  event.preventDefault();
   const invalidInput = document.createElement('p');
-  for (let index = 0; index < required.length; index += 1) {
-    if (required[index].value === '') {
-      rightContentForm.appendChild(invalidInput);
-      invalidInput.innerText = 'Campos inválidos';
+  let auxiliar = false;
+  const inputValues = [];
+
+  for (let index = 0; index < allInputs.length; index += 1) {
+    if (allInputs[index].name !== 'gender' && allInputs[index].name !=='password') {
+      if (allInputs[index].value === '') {
+        rightContentForm.appendChild(invalidInput);
+        invalidInput.innerText = 'Campos inválidos';
+        auxiliar = true;
+      }
+      inputValues.push(allInputs[index].value);
     }
+  }
+
+  if (auxiliar == false) {
+    rightContentForm.innerHTML = '';
+    inputValues.push(gender);
+    let fullName = '';
+    const name = inputValues[0];
+    const lastname = inputValues[1];
+    const email = inputValues[2];
+    const date = inputValues[3];
+    const genero = inputValues[4];
+    rightContentForm.innerHTML = `<p>Olá, ${name} ${lastname}</p> <p>Email: ${email}</p> <p>Data de Nascimento: ${date}</p> <p>Gênero: ${genero}`;
   }
 });

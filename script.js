02e5977f77d60e26814ad8edc0af1391a7708520
Tiago Lunardi <tiagaoalb@gmail.com>
2020-11-21 22:01:48 -0300

    rearranged some variables in script

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const buttonID = document.getElementById('button-login');
 const findError = document.getElementById('Error');
 const inputsRegister = document.getElementsByClassName('inputsSign');
+const buttonRegister = document.getElementById('facebook-register');
+const buttonPersonalizado = document.getElementById('personalizado');
+const createInput = document.createElement('input');
 
 buttonID.addEventListener('click', function () {
   const inputValue = document.getElementById('user-email-phone').value;
-
   alert(inputValue);
 });
 
+buttonRegister.addEventListener('click', ErrorMessage());
 function ErrorMessage() {
   for (let index = 0; index < inputsRegister.length; index += 1) {
     if (inputsRegister[index].value === '') {
@@ -17,12 +20,7 @@ function ErrorMessage() {
   }
 }
 
-const buttonRegister = document.getElementById('facebook-register');
-buttonRegister.addEventListener('click', ErrorMessage());
-
-const buttonPersonalizado = document.getElementById('personalizado');
 buttonPersonalizado.addEventListener('click', function () {
-  const createInput = document.createElement('input');
   createInput.name = 'gender-custom';
   createInput.type = 'text';
   createInput.placeholder = 'Gênero (opcional)';

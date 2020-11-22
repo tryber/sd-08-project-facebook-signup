f2d11f5982cdb1b7bf2be05e9e2b33e99fda6ab9
Uilton Rocha <itoclick@gmail.com>
2020-11-22 19:13:47 -0300

    requisitos css[A

---
+++ script.js
@@ -0,0 +1,6 @@
+let btn = document.getElementById('button-login');
+btn.addEventListener('click', bntclick)
+function bntclick(){
+let btnentrar = document.getElementById('user-email-phone').value;
+alert(btnentrar);
+};

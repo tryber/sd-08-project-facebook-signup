7ed216e4ce99a3dd9e629befca5aa8148d63e2e4
Erick Santos <erickgaves@gmail.com>
2020-11-22 20:04:49 -0300

    entrega de requisitos ate o 9

---
+++ script.js
@@ -0,0 +1,7 @@
+function buttonEnter () {
+    let selectButton = document.getElementById('button-login')
+    selectButton.addEventListener('click', event=> {
+        alert('Email ou telefone')
+    })
+}
+buttonEnter();

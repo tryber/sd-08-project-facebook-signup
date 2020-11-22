e7510c32eeaa27d33f9d79c1c3f6a072531df1dd
Mario Pardo <mariio.pardojr@gmail.com>
2020-11-22 11:24:15 -0400

    Algumas estilizacoes

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
+// Exibe um alert com email ou número do usuário
+
 const btnLogin = document.getElementById('button-login');
 const inputEmailPhone = document.getElementById('user-email-phone');
 btnLogin.addEventListener('click', function () {
   alert(inputEmailPhone.value);
 });
+// Verifica os campos
+const submitButton = document.getElementById('facebook-register');
+const getInputBirthday = document.querySelector('#birthdate');
+const getErrorWarning = document.getElementById('error-warning');
+submitButton.addEventListener('click', function (event) {
+  event.preventDefault();
+  const getInputBirthday = document.querySelector('#birthdate').value;
+  let arrayDate = getInputBirthday.split('/')
+  let day = arrayDate[0];
+  let month = arrayDate[1];
+  let year = arrayDate[2];
+  if (getInputBirthday[2] == '/' && getInputBirthday[5] == '/') {
+    if (!(day > 0 && day <= 31 && month > 0 && month <= 12 && year > 0)) {
+      document.querySelector('#birthdate').classList.add('error');
+      getErrorWarning.classList.remove('input-display');
+    }
+  }
+});
+
+
+
+// Monstra um input de texto ao escolher personalizado no campo de gênero
 
 const customRadio = document.querySelectorAll('.checked-gender');
 const inputOption = document.getElementById('option');

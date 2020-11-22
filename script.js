fc6e68896bf9740fca0b2a4f6a15a30500e2347e
wagnerberna <cabelo29007@gmail.com>
2020-11-22 07:49:56 -0300

    correção codeclimate01

--- script.js
+++ script.js
@@ -4,21 +4,21 @@ function fBtnLogin() {
 }
 document.getElementById('button-login').addEventListener('click', fBtnLogin);
 document.getElementById('facebook-register').addEventListener('click', fBtnRegister);
-function fBtnRegister() { 
-  let firstname = formSignin.firstname.value;
-  let lastname = formSignin.lastname.value;
-  let phoneEmail = formSignin.phone_email.value;
-  let password = formSignin.password.value;
-  let birthdate = formSignin.birthdate.value;
-  let gender = formSignin.gender.value;
-  let checkFirstname = firstname !== '';
-  let checkLastname = lastname !== '';
-  let checkPhoneEmail = phoneEmail !== '';
-  let checkPassword = password !== '';
-  let checkBirthdate = birthdate !== '';
-  let checkGender = gender !== '';
-  let checkAll = (checkFirstname && checkLastname && checkPhoneEmail && checkPassword && checkBirthdate && checkGender);
+function fBtnRegister() {
+  const firstname = formSignin.firstname.value;
+  const lastname = formSignin.lastname.value;
+  const phoneEmail = formSignin.phone_email.value;
+  const password = formSignin.password.value;
+  const birthdate = formSignin.birthdate.value;
+  const gender = formSignin.gender.value;
+  const checkFirstname = firstname !== '';
+  const checkLastname = lastname !== '';
+  const checkPhoneEmail = phoneEmail !== '';
+  const checkPassword = password !== '';
+  const checkBirthdate = birthdate !== '';
+  const checkGender = gender !== '';
+  const checkAll = (checkFirstname && checkLastname && checkPhoneEmail && checkPassword && checkBirthdate && checkGender);
   if (checkAll === false) {
     alert('Campos inválidos');
-  } 
+  }
 }

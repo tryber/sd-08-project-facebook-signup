5b6a3e0f863e0e9cb80ed22367a96d80ef8a4f61
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2020-11-22 16:03:02 -0300

    criação das validações no js

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const buttonLogin = document.getElementById('button-login');
 const emailPhone = document.getElementById('user-email-phone');
-
+const genderUndefined = document.getElementById('undefined');
+const genderForm = document.querySelector('.radio-form');
+const buttonCadastro = document.getElementById('facebook-register');
+const required = document.querySelectorAll('[required]');
 buttonLogin.addEventListener('click', function (event) {
   event.preventDefault();
   alert(emailPhone.value);
+});
+genderUndefined.addEventListener('click', function () {
+  let genderCustom = document.createElement('input');
+  genderCustom.name = 'gender-custom';
+  genderCustom.placeholder = 'Gênero (opcional)';
+  genderForm.appendChild(genderCustom);
+});
+buttonCadastro.addEventListener('click', function () {
+  for (let key in required) {
+    if (required[key].value == '') {
+      alert('Campos inválidos');
+    }
+  }
 });

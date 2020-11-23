4aa385bf9cd3cd5352855c4ddda607fc19bc6c34
Uilton Rocha <itoclick@gmail.com>
2020-11-23 08:09:48 -0300

    requisitos css

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ function bntclick(){
 let btnentrar = document.getElementById('user-email-phone').value;
 alert(btnentrar);
 };
+
+let radioot =document.getElementById("other")
+radioot.addEventListener("click", radiocheck)
+
+let radiofm =document.getElementById("female")
+radiofm.addEventListener("click", radiocheck)
+
+let radioma =document.getElementById("male")
+radioma.addEventListener("click", radiocheck)
+
+
+function radiocheck(){
+  if(radioot.checked === true){
+    document.getElementById("genop").style.visibility = "visible";
+  }
+  else {document.getElementById("genop").style.visibility = "hidden"}
+  } 

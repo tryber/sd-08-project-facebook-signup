4554ae2cc659f80b5d3ec88d13bc9d4342ed74d8
wend3ll-souza <wend3llsouza@gmail.com>
2020-11-23 12:16:04 -0300

    :up: 18 Aviso de Campos Invalidos

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 const button = document.getElementById('button-login');
 const label = document.getElementById('user-email-phone');
-
+const cadastro = document.getElementById('cadastro');
+const required = document.querySelectorAll("[required]");
+const erro = document.createElement('section');
+const erroButton = document.createElement('button');
 addEventListener('click', (event) => {
   if (event.target === button) {
     alert(label.value);
-  }
+  };
+  if(event.target.id === 'facebook-register'){
+    for (key in required) {
+      let input = required[key];
+      if (input.required) {
+        if (input.value === "") {
+            erro.innerText = 'Campos inválidos';
+            erro.className = 'campos-invalidos';
+            erro.id = 'campos-invalidos'
+            erroButton.innerText = 'OK';
+            erroButton.id = 'erro-button';
+            cadastro.appendChild(erro);
+            erro.appendChild(erroButton);
+         
+        };
+      };
+    };
+  };
+  if(event.target.id === 'erro-button'){
+    erro.remove();
+  };
 });

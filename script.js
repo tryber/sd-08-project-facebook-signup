7a4516e410f4b546cf11e69e838f425e7907403c
Uilton Rocha <itoclick@gmail.com>
2020-11-23 10:19:00 -0300

    requisitos css

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//botão entrar mostrando o valor do input nome-email
 let btn = document.getElementById('button-login');
 btn.addEventListener('click', bntclick)
 function bntclick(){
@@ -5,19 +6,34 @@ let btnentrar = document.getElementById('user-email-phone').value;
 alert(btnentrar);
 };
 
-let radioot =document.getElementById("other")
-radioot.addEventListener("click", radiocheck)
+//tres escutas para os tipos de genero
+let radioot =document.getElementById("other");
+radioot.addEventListener("click", radiocheck);
 
-let radiofm =document.getElementById("female")
-radiofm.addEventListener("click", radiocheck)
+let radiofm =document.getElementById("female");
+radiofm.addEventListener("click", radiocheck);
 
-let radioma =document.getElementById("male")
-radioma.addEventListener("click", radiocheck)
+let radioma =document.getElementById("male");
+radioma.addEventListener("click", radiocheck);
 
-
+//se for outros mostra campo se não invisivel
 function radiocheck(){
   if(radioot.checked === true){
     document.getElementById("genop").style.visibility = "visible";
   }
-  else {document.getElementById("genop").style.visibility = "hidden"}
-  } 
+  else {document.getElementById("genop").style.visibility = "hidden"
+  }
+}; 
+
+  //botão cadastre-se checando valores antes
+let submete =document.getElementById("facebook-register")
+submete.addEventListener("click", checksubmit)
+
+function checksubmit(){
+ let frn = document.getElementsByName("firstname").value;
+  if (frn === ""){ alert('Campos inválidos')}
+  
+  let scn = document.getElementsByName("fastname").value;
+  if (scn === ""){ alert('Campos inválidos')}
+
+};

9c4d2b42079ed24b4d2c31e7fb32f2aa0ff280b3
amandaos <amanda.775.souza@gmail.com>
2020-11-23 16:25:11 -0300

    itens javascript

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 function alertButton() {
   alert(document.getElementById('user-email-phone').value);
 }
+
+function getRadioValueFromInputs(genders) {
+  for (let gender = 0; gender < genders.lenght; gender += 1) {
+    if (genders[gender].checked) {
+      return genders[gender].value;
+    }
+  }
+  return '';
+}
+
 function checkForm() {
-  const nome = document.querySelector(".nome").value;
+  event.preventDefault();
+  const name = document.getElementById('name-input').value;
+  const lastname = document.getElementById('lastname-input').value;
+  const phoneEmail = document.getElementById('phone-email').value;
+  const password = document.getElementById('password-input').value;
+  const genders = document.querySelectorAll('input[name="gender"]').value;
+  const selectedGenderValue = getRadioValueFromInputs(genders);
 
+  if (name === '' || lastname === '' || phoneEmail === '' || password === '' || selectedGenderValue === '') {
+    alert('Por favor preencha todos os campos.');
 
-  //Check input Fields Should not be blanks.
-  if (nome === '') {
-    alert("Por favor preencha todos os campos.");
+    name.focus();
+  } else {
+    document.getElementById('form-registry').submit();
   }
 }
+
 
 window.onload = () => {
   const botaoEnvia = document.getElementById('button-login');
@@ -17,4 +36,4 @@ window.onload = () => {
 
   const botaoCadastro = document.getElementById('facebook-register');
   botaoCadastro.onclick = checkForm;
-}
+};

ff4e81ef76fbc7a04e2acf241a8a5738446df9b5
rodolfo_oliveira <rod.vgo@gmail.com>
2020-11-23 11:58:43 -0300

    requisito 19: adiciona input de genero personalizado

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const alertButton = document.getElementById('button-login');
 const userInput = document.getElementById('user-email-phone');
+const genderInput = document.getElementById('custom-gender');
+const inputDestination = document.querySelector('.gender-type');
 
 function alertLogin() {
   if (userInput.value === '') {
@@ -9,4 +11,20 @@ function alertLogin() {
   }
 }
 
+
+
 alertButton.addEventListener('click', alertLogin);
+
+function customInput() {
+  if (genderInput.value === 'personalizado') {
+    const newInput = document.createElement('input')
+    newInput.name = 'gender-custom';
+    newInput.placeholder = 'Gênero (opcional)';
+    newInput.id = 'newInput';
+    newInput.className = 'newInput';
+    inputDestination.appendChild(newInput);
+  }
+}
+
+genderInput.addEventListener('click', customInput);
+

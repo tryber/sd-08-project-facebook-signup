0068f2909a48e3b66adca4c77588e0d037376c2a
wend3ll-souza <wend3llsouza@gmail.com>
2020-11-23 12:34:23 -0300

    :beetle: 18 Aviso de Campos Invalidos - Corrigindo Indentação

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
 const button = document.getElementById('button-login');
 const label = document.getElementById('user-email-phone');
 const cadastro = document.getElementById('cadastro');
-const required = document.querySelectorAll("[required]");
+const required = document.querySelectorAll('[required]');
 const erro = document.createElement('section');
 const erroButton = document.createElement('button');
 addEventListener('click', (event) => {
   if (event.target === button) {
     alert(label.value);
-  };
-  if(event.target.id === 'facebook-register'){
-    for (key in required) {
+  } else if (event.target.id === 'facebook-register') {
+    for (let key in required) {
       let input = required[key];
       if (input.required) {
-        if (input.value === "") {
-            erro.innerText = 'Campos inválidos';
-            erro.className = 'campos-invalidos';
-            erro.id = 'campos-invalidos'
-            erroButton.innerText = 'OK';
-            erroButton.id = 'erro-button';
-            cadastro.appendChild(erro);
-            erro.appendChild(erroButton);
-         
-        };
-      };
-    };
-  };
-  if(event.target.id === 'erro-button'){
+        if (input.value === '') {
+          erro.innerText = 'Campos inválidos';
+          erro.className = 'campos-invalidos';
+          erro.id = 'campos-invalidos';
+          erroButton.innerText = 'OK';
+          erroButton.id = 'erro-button';
+          cadastro.appendChild(erro);
+          erro.appendChild(erroButton);
+        }
+      }
+    }
+  } else if (event.target.id === 'erro-button') {
     erro.remove();
-  };
+  }
 });

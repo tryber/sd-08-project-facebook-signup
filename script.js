26bf3b16153283cd15525630d0a5038912173f71
Rafael <rafaelmaltez89@gmail.com>
2020-11-23 13:32:59 -0300

    Codeclimate correction

--- script.js
+++ script.js
@@ -20,14 +20,14 @@ function addGenderText() {
 addGenderText();
 
 function substituteFormContainer() {
-  const rigthcontent = document.getElementById('rightcontent');  
+  const rigthcontent = document.getElementById('rightcontent');
   const name = document.getElementById('nome').value;
   const sobrenome = document.getElementById('sobrenome').value;
   const phonemail = document.getElementById('phonemail').value;
   const birthdate = document.getElementById('birthdate').value;
   const mensagem = `Olá, ${name} ${sobrenome}\n`;
   const mensagemTwo = document.createElement('span');
-  mensagemTwo.innerText= `${phonemail}\n${birthdate}`;  
+  mensagemTwo.innerText= `${phonemail} \n ${birthdate}`;
   rigthcontent.innerHTML = mensagem;
   rigthcontent.appendChild(mensagemTwo);
 }
@@ -39,20 +39,20 @@ function validateForm() {
   const form = document.getElementById('register');
   const button = document.getElementById('facebook-register');
   let mistakes = 0;
-  button.addEventListener('click', function (event) { 
-  event.preventDefault();   
-  const input = document.querySelectorAll('.cadastro input[type="text"]');    
-  for (let index = 0; index < input.length; index += 1) {
-    if (input[index].value === '') {
-      mistakes = mistakes + 1;
-    };
-  }  
-  if (mistakes != 0) {
-    form.appendChild(paragraph);
-  } else {
-    substituteFormContainer();
-  }
-});
+  button.addEventListener('click', function (event) {
+    event.preventDefault();
+    const input = document.querySelectorAll('.cadastro input[type="text"]');
+    for (let index = 0; index < input.length; index += 1) {
+      if (input[index].value === '') {
+        mistakes += 1;
+      }
+    }
+    if (mistakes !== 0) {
+      form.appendChild(paragraph);
+    } else {
+      substituteFormContainer();
+    }
+  });
 }
 validateForm();
 // Ideia da variavel mistakes veio de uma dica da Rosiele.

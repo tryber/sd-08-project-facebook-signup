53c95c0b11c0c87057d68459e5e739834ae86bc5
vivianeflowt <vivianeflowt@gmail.com>
2020-11-23 00:40:02 -0300

    aaa

--- script.js
+++ script.js
@@ -7,12 +7,6 @@ document.getElementById('button-login').addEventListener('click', () => {
   //
 });
 
-// document.getElementById('gender-personalized').addEventListener('click', () => {
-//   document.getElementById('gender-personalized-input').classList.remove('hidden');
-//   console.log('ssss');
-//   //
-// });
-
 document.getElementById('gender-select').addEventListener('click', (e) => {
   if (e.target.classList.contains('sel-gender')) {
     document.getElementById('gender-personalized-input').classList.add('hidden');

7be21199091435b95d29380ec9e60e1f28399c3f
Pedro Amaral <amaralph1995@gmail.com>
2020-11-23 17:16:56 -0300

    Tenta corrigir itens 5 e 6

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const buttonLogin = document.getElementById('button-login');
 const user = document.getElementById('user-email-phone');
 const pswd = document.getElementById('user-password');
-buttonLogin.addEventListener('click', function() {
+buttonLogin.addEventListener('click', function () {
   alert(user.value, pswd.value);
 });

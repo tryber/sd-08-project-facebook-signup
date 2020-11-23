7108fee453d2695ab09e83b212235abb32adb1d2
vivianeflowt <vivianeflowt@gmail.com>
2020-11-23 01:14:25 -0300

    update

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
 window.onload = () => {
-  // document.getElementById('gender-personalized-input').hidden = true;
+  // document.getElementById('gender-personalized-input').classList.add('hidden');
 };
 
 document.getElementById('button-login').addEventListener('click', () => {
   alert(document.getElementById('user-email-phone').value);
-  //
 });
 
 document.getElementById('gender-select').addEventListener('click', (e) => {
+  const genderPersonalizedInput = document.getElementById('gender-personalized-input');
   if (e.target.classList.contains('sel-gender')) {
-    document.getElementById('gender-personalized-input').classList.add('hidden');
+    genderPersonalizedInput.classList.add('hidden');
     if (e.target.classList.contains('gender-personalized')) {
-      document.getElementById('gender-personalized-input').classList.remove('hidden');
+      genderPersonalizedInput.classList.remove('hidden');
     }
   }
+});
+
+const validate = (e) => {
+  const registerError = document.getElementById('error');
+  if (e.value.trim() === '') {
+    registerError.classList.remove('hidden');
+    setTimeout(() => {
+      registerError.classList.add('hidden');
+    }, 5000);
+  }
+};
+
+document.getElementById('facebook-register').addEventListener('click', (e) => {
+  e.preventDefault();
+  validate(document.forms.signup.firstname);
+  validate(document.forms.signup.lastname);
+  validate(document.forms.signup.phone_email);
+  validate(document.forms.signup.password);
+  validate(document.forms.signup.birthdate);
+  validate(document.forms.signup['gender-selection']);
 });

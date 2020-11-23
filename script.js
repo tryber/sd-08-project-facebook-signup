07a74317b3ec2c0b5d61260d7be0189a0cc3e910
Uilton Rocha <itoclick@gmail.com>
2020-11-23 11:20:40 -0300

    requisitos css

--- script.js
+++ script.js
@@ -7,33 +7,33 @@ const btn = document.getElementById('button-login');
 btn.addEventListener('click', bntclick);
 
 // se for outros mostra campo se não invisivel
-let radioot =document.getElementById("other");
+let radioot =document.getElementById('other');
 function radiocheck() {
-  if (radioot.checked === true){
-    document.getElementById("genop").style.visibility = "visible";
+  if (radioot.checked === true) {
+    document.getElementById('genop').style.visibility = 'visible';
   }
-  else {document.getElementById("genop").style.visibility = "hidden"
+  else {document.getElementById('genop').style.visibility = 'hidden'
   }
 };
 
 // tres escutas para os tipos de genero
-radioot.addEventListener("click", radiocheck);
+radioot.addEventListener('click', radiocheck);
 
-let radiofm =document.getElementById("female");
-radiofm.addEventListener("click", radiocheck);
+let radiofm =document.getElementById('female');
+radiofm.addEventListener('click', radiocheck);
 
-let radioma =document.getElementById("male");
-radioma.addEventListener("click", radiocheck);
+let radioma =document.getElementById('male');
+radioma.addEventListener('click', radiocheck);
 
 // botão cadastre-se checando valores antes
-let submete =document.getElementById("facebook-register")
-submete.addEventListener("click", checksubmit)
+let submete =document.getElementById('facebook-register')
+submete.addEventListener('click', checksubmit)
 
-function checksubmit(){
-  let frn = document.getElementsByName("firstname").value;
-  if (frn === ""){ alert('Campos inválidos')}
+function checksubmit() {
+  let frn = document.getElementsByName('firstname').value;
+  if (frn === '') { alert('Campos inválidos')}
 
-  let scn = document.getElementsByName("fastname").value;
-  if (scn === ""){ alert('Campos inválidos')}
+  let scn = document.getElementsByName('fastname').value;
+  if (scn === '') { alert('Campos inválidos')}
 
 };

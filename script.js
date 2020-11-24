6f6e1e67b04f03e2338e57ed8723fd6ac2c4d637
Bidfca <bruninhakjurugat11@gmail.com>
2020-11-24 08:51:39 -0300

    AGORA VAI AMEM

--- script.js
+++ script.js
@@ -2,25 +2,23 @@ const form = document.getElementsByClassName('facebook-signin')[0];
 const input = document.querySelector('.gender-custom');
 const radios = document.querySelectorAll('input[type=radio]');
 const rightContent = document.getElementsByClassName('right-content')[0];
-const div = document.createElement('div');
 const welcome = document.createElement('p');
 
 function showInfo() {
   const formData = new FormData(form);
+  rightContent.innerHTML = '';
   Array.from(formData.entries()).forEach((entrie) => {
     const field = document.createElement('p');
     if (entrie[0] === 'firstname') {
       welcome.innerText = `Olá, ${entrie[1]}`;
     } else if (entrie[0] === 'lastname') {
       welcome.innerText = `${welcome.innerText} ${entrie[1]}`;
-      div.appendChild(welcome);
+      rightContent.appendChild(welcome);
     } else if (entrie[0] !== 'password') {
       field.innerText = entrie[1];
-      div.appendChild(field);
+      rightContent.appendChild(field);
     }
   });
-  rightContent.innerHTML = '';
-  rightContent.appendChild(div);
 }
 
 function fBtnLogin() {

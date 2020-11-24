b370524e1862660a3f6b9e3614552161b23fd623
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2020-11-24 18:25:36 -0300

    Adiciona código que limpa o right content

--- script.js
+++ script.js
@@ -6,6 +6,47 @@ function alertBotao() {
 }
 botaoLogin.addEventListener('click', alertBotao);
 
+function alertBotao() {
+  alert(emailOuTelefone.value);
+}
+botaoLogin.addEventListener('click', alertBotao);
+
+function pegarRadioValor(name) {
+  const rads = document.getElementsByName(name);
+  for (let index = 0; index < rads.length; index += 1) {
+    if (rads[index].checked) {
+      return rads[index].value;
+    }
+  }
+  return null;
+}
+
+function exibirDados() {
+  const nome = document.querySelector('[name=firstname]').value;
+  const sobreNome = document.querySelector('[name=lastname]').value;
+  const emailOuTel = document.querySelector('[name=phone_email]').value;
+  const dataNasc = document.querySelector('[name=birthdate]').value;
+  const genero = pegarRadioValor('gender');
+  const formulario = document.querySelector('.right-content');
+  formulario.innerHTML = '';
+
+  const p1 = document.createElement('p');
+  p1.innerHTML = `Olá, ${nome} ${sobreNome}`;
+  formulario.appendChild(p1);
+
+  const p2 = document.createElement('p');
+  p2.innerHTML = emailOuTel;
+  formulario.appendChild(p2);
+
+  const p3 = document.createElement('p');
+  p3.innerHTML = dataNasc;
+  formulario.appendChild(p3);
+
+  const p4 = document.createElement('p');
+  p4.innerHTML = genero;
+  formulario.appendChild(p4);
+}
+
 const botaoEnviar = document.querySelector('#facebook-register');
 const divVazia = document.getElementById('div-vazia');
 botaoEnviar.addEventListener('click', function (event) {
@@ -21,7 +62,7 @@ botaoEnviar.addEventListener('click', function (event) {
       }
     }
   }
-  divVazia.innerHTML = 'Campos válidos';
+  exibirDados();
 });
 
 const botaoPer = document.querySelector('#gender-custom');

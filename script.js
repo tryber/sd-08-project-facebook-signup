7f0b6453e6de38e6bf4bb4b67475f8d18216238c
Bidfca <bruninhakjurugat11@gmail.com>
2020-11-24 08:35:15 -0300

    fix bug

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const form = document.getElementsByClassName('facebook-signin')[0];
 const input = document.querySelector('.gender-custom');
 const radios = document.querySelectorAll('input[type=radio]');
-const formData = new FormData(form);
 const rightContent = document.getElementsByClassName('right-content')[0];
 const div = document.createElement('div');
 const welcome = document.createElement('p');
 
 function showInfo() {
+  const formData = new FormData(form);
   Array.from(formData.entries()).forEach((entrie) => {
     const field = document.createElement('p');
     if (entrie[0] === 'firstname') {

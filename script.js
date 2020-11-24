4244707b1e1c7345995699980f242053b5ae1b20
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2020-11-24 20:02:18 -0300

    Testando se o problema estava no header

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-const botaoLogin = document.querySelector('#button-login');
-const emailOuTelefone = document.querySelector('#user-email-phone');
+const botaoLogin = document.getElementById('button-login');
+const emailOuTelefone = document.getElementById('user-email-phone');
 
 function alertBotao() {
   alert(emailOuTelefone.value);
 }
 botaoLogin.addEventListener('click', alertBotao);
 
+const nome = document.querySelector('[name=firstname]');
+const sobreNome = document.querySelector('[name=lastname]');
+const emailOuTel = document.querySelector('[name=phone_email]');
+const dataNasc = document.querySelector('[name=birthdate]');
 function pegarRadioValor(name) {
   const rads = document.getElementsByName(name);
   for (let index = 0; index < rads.length; index += 1) {
@@ -17,55 +21,45 @@ function pegarRadioValor(name) {
 }
 
 function exibirDados() {
-  const nome = document.querySelector('[name=firstname]').value;
-  const sobreNome = document.querySelector('[name=lastname]').value;
-  const emailOuTel = document.querySelector('[name=phone_email]').value;
-  const dataNasc = document.querySelector('[name=birthdate]').value;
   const genero = pegarRadioValor('gender');
   const formulario = document.querySelector('.right-content');
+  const h1 = document.querySelector('h1');
+  const h2 = document.querySelector('h2');
+  document.querySelector('.right-content').removeChild(h1);
+  document.querySelector('.right-content').removeChild(h2);
   formulario.innerHTML = '';
-
-  const p1 = document.createElement('p');
-  p1.innerHTML = `Olá, ${nome} ${sobreNome}`;
-  formulario.appendChild(p1);
-
-  const p2 = document.createElement('p');
-  p2.innerHTML = emailOuTel;
-  formulario.appendChild(p2);
-
-  const p3 = document.createElement('p');
-  p3.innerHTML = dataNasc;
-  formulario.appendChild(p3);
-
-  const p4 = document.createElement('p');
-  p4.innerHTML = genero;
-  formulario.appendChild(p4);
+  formulario.innerHTML += `<p> Olá, ${nome.value} ${sobreNome.value}</p>`;
+  formulario.innerHTML += `<p> ${emailOuTel.value}</p>`;
+  formulario.innerHTML += `<p> ${dataNasc.value}</p>`;
+  formulario.innerHTML += `<p> ${genero} </p>`;
 }
 
 const botaoEnviar = document.querySelector('#facebook-register');
-const divVazia = document.querySelector('#div-vazia');
+const divVazia = document.getElementById('div-vazia');
 botaoEnviar.addEventListener('click', function (event) {
   event.preventDefault();
-  const requiredInputs = document.querySelector('#limpa-forms').querySelectorAll('[required]');
-  for (let index = 0; index < requiredInputs.length; index += 1) {
-    const currentInput = requiredInputs[index];
-    if (!currentInput.checkValidity()) {
-      divVazia.innerHTML = 'Campos inválidos';
-      return;
+  const inputsText = document.getElementsByClassName('inputs');
+  divVazia.innerHTML = '';
+  for (let index = 0; index < inputsText.length; index += 1) {
+    const campoInput = inputsText[index];
+    if (campoInput.required) {
+      if (campoInput.value === '') {
+        divVazia.innerHTML = 'Campos inválidos';
+      }
     }
   }
-  exibirDados();
+  if (divVazia.innerHTML === '') exibirDados();
 });
 
-const botaoPer = document.querySelector('#gender-custom');
-const radioPerso = document.getElementsByName('gender');
+const botaoPer = document.querySelector('#botaopersonalizado');
+const radioPerso = document.getElementsByClassName('radioperso');
 
 for (let index = 0; index < radioPerso.length; index += 1) {
   radioPerso[index].addEventListener('click', function () {
     if (radioPerso[2].checked === true) {
-      botaoPer.classList.remove('hidden');
+      botaoPer.innerHTML = '<input type="text" name="gender-custom" placeholder="Gênero (opcional)">';
     } else {
-      botaoPer.classList.add('hidden');
+      botaoPer.innerHTML = '';
     }
   });
 }

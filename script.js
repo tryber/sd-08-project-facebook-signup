05c907ecabdf0f565da931797998eb0dc5027948
Samuel <samuelvanguarda@gmail.com>
2020-11-25 10:22:08 -0300

    Requisito 18 - Correção 2

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let index; 
-const allInputs = document.querySelectorAll('.borderStyle');
+const textInputs = document.querySelectorAll('.text');
+const radioInputs = document.querySelectorAll('.radio');
+let buttonSignUp = document.getElementById('facebook-register');
+const alert = document.createElement('p');
+const form = document.querySelector('.form');
+ 
 
 function alertLogin() {
     const emailCelular = document.getElementById('user-email-phone');
@@ -7,11 +12,39 @@ function alertLogin() {
   }
 
 
-const validateInputs = (inputs) => {
-  for (index = 0; index < allInputs.length; index ++){
-    if allInputs[index] = ''{
-        alert("Campos inválidos");
+const validateTextInputs = () => {
+  for (index = 0; index < textInputs.length; index ++){
+    if (textInputs[index].value == ''){
+      alert.innerHTML = 'Campos Inválidos';
+      form.appendChild(alert);
+      break;
     }
   }
 }
+
+let counting = 0;
+const validateRadioInputs = () => {
+  for (index = 0; index < radioInputs.length; index ++){
+    if (radioInputs[index].checked){
+      counting += 1;
+    }
+  }
+}
+
+const checkRadio = () => {
+  if (counting === 1){
+    alert.innerHTML = '';
+  }else{
+    alert.innerHTML = 'Campos Inválidos';
+    form.appendChild(alert);
+  }
+}
+
+buttonSignUp.addEventListener('click',function(event){
+  validateRadioInputs();
+  validateTextInputs();
+  checkRadio();
+  event.preventDefault();
+})
+
 

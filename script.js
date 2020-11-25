13410613b4666784cd20f9eeb252e2097d480aa4
Antônio Schappo <antonioschappo@gmail.com>
2020-11-24 21:22:15 -0300

    Try req. 20

--- script.js
+++ script.js
@@ -3,16 +3,17 @@ const emailTelefone = document.getElementById('user-email-phone');
 const inputArray = document.querySelectorAll('.right-content input');
 const customGender = document.getElementById('custom-gender');
 const form = document.getElementsByClassName('form-right-content')[0];
+const registerButton = document.getElementById('facebook-register');
 
 loginButton.addEventListener('click', function () {
   alert(emailTelefone.value);
 });
-const registerButton = document.getElementById('facebook-register');
 
 registerButton.addEventListener('click', function () {
+  let check;
   for (let index = 0; index < inputArray.length; index += 1) {
     const input = inputArray[index];
-    const check = input.checkValidity();
+    check = input.checkValidity();
     if (check) {
       input.setCustomValidity('');
     } else {
@@ -22,6 +23,29 @@ registerButton.addEventListener('click', function () {
       form.appendChild(message);
       break;
     }
+  };
+  if (check) {
+    const rightContent = document.getElementById('right-section');
+    const firstName = document.getElementById('first').value;
+    const lastName = document.getElementById('last').value;
+    const phoneMail = document.getElementById('phone').value;
+    const birthDate = document.getElementById('birthdate').value;
+    const female = document.getElementById('female-gender');
+    const male = document.getElementById('male-gender');
+    const custom = document.getElementById('custom-gender');
+    let gender;
+    if (female.checked) {
+      gender = female.value;
+    } else if (male.checked) {
+      gender = male.value;
+    } else {
+      gender = custom.value;
+    }
+
+    rightContent.innerHTML = `<p> Olá, ${firstName} ${lastName}.
+    Seu e-mail é o ${phoneMail}.
+    Sua data de nascimento é ${birthDate}.
+    Você se identifica como alguém do sexo ${gender}.<\p>`;
   }
 });
 

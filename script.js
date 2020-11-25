dc859faf0e74d5a4770dcfe8e739224af02f03ed
Samuel <samuelvanguarda@gmail.com>
2020-11-25 09:00:14 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-
+let index; 
+const allInputs = document.querySelectorAll('.borderStyle');
 
 function alertLogin() {
     const emailCelular = document.getElementById('user-email-phone');
     alert(emailCelular.value);
   }
+
+
+const validateInputs = (inputs) => {
+  for (index = 0; index < allInputs.length; index ++){
+    if allInputs[index] = ''{
+        alert("Campos inválidos");
+    }
+  }
+}
+

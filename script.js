254d611f52210d70410fdc2448405f6f555569ee
Pedro Amaral <amaralph1995@gmail.com>
2020-11-24 23:46:35 -0300

    Req 7 - modela botão entrar

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const buttonLogin = document.getElementById('button-login');
 const user = document.getElementById('user-email-phone');
 const pswd = document.getElementById('user-password');
 buttonLogin.addEventListener('click', function () {
-  alert(user.value, pswd.value);
+  alert(user.value);
 });

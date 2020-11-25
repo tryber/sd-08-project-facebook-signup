a3f81604f1846905193cce4facbae5529410b94b
Pedro Amaral <amaralph1995@gmail.com>
2020-11-24 23:51:56 -0300

    Corrige code climate

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const buttonLogin = document.getElementById('button-login');
 const user = document.getElementById('user-email-phone');
-const pswd = document.getElementById('user-password');
 buttonLogin.addEventListener('click', function () {
   alert(user.value);
 });

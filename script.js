4987ce5e70d6fd6a5dd3de78872081177e5d1863
Erick Santos <erickgaves@gmail.com>
2020-11-25 19:54:13 -0300

    corrigindo codeclimate requisito 19

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ buttonEnter();
 function validarCampos() {
   const buttonCadastrar = document.querySelector('#facebook-register');
   buttonCadastrar.addEventListener('click', (event) => {
-  event.preventDefault();
-  const selectInput=document.querySelectorAll('.create-account input');
-  for (let index=0; index < selectInput.length; index+=1) {
+    event.preventDefault();
+    const selectInput = document.querySelectorAll('.create-account input');
+    for (let index = 0; index < selectInput.length; index += 1) {
     const input = selectInput[index];
     if (input.value === '') {
-      const span=document.createElement('span');
+      const span = document.createElement('span');
       span.innerText = 'Campos inválidos';
       span.id = 'span-validator';
       if (document.querySelector('#span-validator') === null) {
@@ -27,5 +27,5 @@ function validarCampos() {
   }
   }
   });
-  }
+}
 validarCampos();

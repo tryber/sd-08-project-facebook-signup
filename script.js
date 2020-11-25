e4760140ec4c65389d5ac92162e3d7ef88fb954e
WagnerMor <wagnermor@outlook.com>
2020-11-25 03:14:19 -0300

    requisito 20 adiciona valor do input ao radio id=personalizado

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ button.addEventListener('click', () => {
 const buttonRegister = document.querySelector('#facebook-register');
 
 buttonRegister.addEventListener('click', (event) => {
+  event.preventDefault();//movendo essa linha evitamos duplicidade
   const nome = document.getElementById('nome');
   const sobreNome = document.getElementById('sobrenome');
   const email = document.getElementById('email');
@@ -16,12 +17,24 @@ buttonRegister.addEventListener('click', (event) => {
   const rightContent = document.querySelector('.right-content').children[2];
   const div = document.querySelector('.erro');
 
+  const radioPersonalizado = document.getElementById('personalizado'); // adicionei essas duas referencias diretas para adicionar
+  const inputPersonalizado = document.getElementById('input-personalizado'); // o value do input personalizado ao genero
+
   if (nome.value === '' || sobreNome.value === '' || email.value === '' || birthdate.value === '') {
-    event.preventDefault();
+    // event.preventDefault(); pode ser apagado
     div.innerText = 'Campos inválidos';
   } else {
+    // <-------------------<< esse trecho verifica se o gênero personalizado foi digitado
+    const generoPersonalizado = inputPersonalizado.value;
+    if(generoPersonalizado !== '') {
+      radioPersonalizado.value = generoPersonalizado;
+    } else {
+      radioPersonalizado.value = 'Prefiro não responder';
+    }
+    // >>------------------->
+    console.log(generoPersonalizado);
     for (let index = 0; index < genero.length; index += 1) {
-      event.preventDefault();
+      // event.preventDefault(); pode ser apagado
       if (genero[index].checked) {
         rightContent.innerText = `Olá, ${nome.value} ${sobreNome.value}.
 
@@ -37,12 +50,13 @@ buttonRegister.addEventListener('click', (event) => {
 const personalizado = document.getElementsByName('gender');
 const newField = document.querySelector('.new-field');
 const campoPersonalizado = document.createElement('input');
+campoPersonalizado.id = 'input-personalizado';
 campoPersonalizado.name = 'gender-custom';
 campoPersonalizado.placeholder = 'Gênero (opcional)';
 
 for (let index = 0; index < personalizado.length; index += 1) {
   personalizado[index].addEventListener('click', () => {
-    if (personalizado[index].value === 'Personalizado') {
+    if (personalizado[index].id === 'personalizado') {//alterei a referencia para o id
       newField.appendChild(campoPersonalizado);
     }
   });

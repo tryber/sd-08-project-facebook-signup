b9d3d6cf277566988a41c24a9f5f397ddcc61ebf
Pedro Amaral <amaralph1995@gmail.com>
2020-11-25 01:19:56 -0300

    Req 18 - cria validação para campos

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ const user = document.getElementById('user-email-phone');
 buttonLogin.addEventListener('click', function () {
   alert(user.value);
 });
+
+const buttonRegister = document.getElementById('facebook-register');
+const inputs = document.getElementsByClassName('inputs');
+const radio = document.querySelector('.radio');
+console.log(inputs)
+buttonRegister.addEventListener('click', () => {
+  for (input of inputs) {
+    console.log(input.value.length)
+    if (input.value.length == 0) {
+      alert('Campos inválidos')
+      return;
+    }
+  }
+})

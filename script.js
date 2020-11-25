d9caedf50b7c57c6a58cad0e8697e7c6f3fb4538
Miguel Dantas <miiguelalb@gmail.com>
2020-11-25 20:28:45 -0300

    Corrigido erros codeclimate Script.js.

--- script.js
+++ script.js
@@ -1,60 +1,77 @@
-let index; 
-const textInputs = document.querySelectorAll('.text');
-const radioInputs = document.querySelectorAll('.radio');
-let buttonSignUp = document.getElementById('facebook-register');
-const alert = document.createElement('p');
+const buttonSignUp = document.getElementById('facebook-register');
+const missing = document.createElement('p');
 const form = document.querySelector('.form');
 let counting = 0;
 const inputCustomGender = document.createElement('input');
-inputCustomGender.name = "gender-custom";
-inputCustomGender.placeholder = "Gênero (opcional)";
-inputCustomGender.type = "text";
-inputCustomGender.id = "gender";
-const formGender = document.querySelector(".form-gender");
-const optionCustom = document.querySelector("#personal")
- 
+inputCustomGender.name = 'gender-custom';
+inputCustomGender.placeholder = 'Gênero (opcional)';
+inputCustomGender.type = 'text';
+inputCustomGender.id = 'gender';
+const formGender = document.querySelector('.form-gender');
+const optionCustom = document.querySelector('#personal');
+const firstName = document.querySelector('.firstName');
+const lastName = document.querySelector('.lastName');
+const email = document.querySelector('.email');
+const birthDate = document.querySelector('.birthdate');
+const textInputs = document.querySelectorAll('.input');
 
-function alertLogin() {
+/* const alertLogin = () => {
     const emailCelular = document.getElementById('user-email-phone');
     alert(emailCelular.value);
-  }
+} */
 
-
-const validateTextInputs = () => {
-  for (index = 0; index < textInputs.length; index ++){
-    if (textInputs[index].value == ''){
-      alert.innerHTML = 'Campos Inválidos';
-      form.appendChild(alert);
-      break;
-    }
-  }
-}
-
-const validateRadioInputs = () => {
-  for (index = 0; index < radioInputs.length; index ++){
-    if (radioInputs[index].checked){
+const inputRadio = document.querySelectorAll('inputRadio');
+function contagem() {
+  for (let index = 0; index < inputRadio.length; index += 1) {
+    if (inputRadio[index].checked) {
       counting += 1;
     }
   }
 }
 
-const checkRadio = () => {
-  if (counting === 1){
-    alert.innerHTML = '';
-  }else{
-    alert.innerHTML = 'Campos Inválidos';
-    form.appendChild(alert);
+function checkRadio() {
+  if (counting === 1) {
+    missing.innerHTML = '';
+  } else {
+    missing.innerHTML = 'Campos Inválidos';
+    form.appendChild(missing);
   }
 }
 
-buttonSignUp.addEventListener('click',function(event){
-  validateRadioInputs();
-  validateTextInputs();
-  checkRadio();
+const validateTextInputs = () => {
+  for (let index = 0; index < textInputs.length; index += 1) {
+    if (textInputs[index].value === '') {
+      missing.innerHTML = 'Campos Inválidos';
+      form.appendChild(missing);
+    } else {
+      const pName = document.createElement('p');
+      pName.innerHTML = `Olá, ${firstName.value} ${lastName.value}`;
+      const pEmail = document.createElement('p');
+      pEmail.innerHTML = email.value;
+      const pBirthDate = document.createElement('p');
+      pBirthDate.innerHTML = birthDate.value;
+      const gender = document.createElement('p');
+      gender.innerText = document.querySelector('input[type=radio]:checked').value;
+      document.querySelector('.main-content').removeChild(document.querySelector('.right-content'));
+      const data = document.createElement('div');
+      data.className = 'right-content';
+      document.querySelector('.main-content').appendChild(data);
+      data.appendChild(pName);
+      data.appendChild(pEmail);
+      data.appendChild(pBirthDate);
+      data.appendChild(gender);
+    }
+  }
+};
+
+buttonSignUp.addEventListener('click', function (event) {
   event.preventDefault();
-})
+  contagem();
+  checkRadio();
+  validateTextInputs();
+});
 
-optionCustom.addEventListener('click', function(){
+optionCustom.addEventListener('click', function () {
   formGender.appendChild(inputCustomGender);
-})
-
+},
+);

d80aaacde662c8dca57b79a8037b75417d02c4aa
Erick Santos <erickgaves@gmail.com>
2020-11-25 19:55:57 -0300

    corrigindo codeclimate requisito 19

--- script.js
+++ script.js
@@ -14,13 +14,13 @@ function validarCampos() {
     event.preventDefault();
     const selectInput = document.querySelectorAll('.create-account input');
     for (let index = 0; index < selectInput.length; index += 1) {
-    const input = selectInput[index];
-    if (input.value === '') {
-      const span = document.createElement('span');
-      span.innerText = 'Campos inválidos';
-      span.id = 'span-validator';
-      if (document.querySelector('#span-validator') === null) {
-        document.querySelector('.create-account').appendChild(span);
+      const input = selectInput[index];
+      if (input.value === '') {
+        const span = document.createElement('span');
+        span.innerText = 'Campos inválidos';
+        span.id = 'span-validator';
+        if (document.querySelector('#span-validator') === null) {
+          document.querySelector('.create-account').appendChild(span);
         break;
       }
       break;

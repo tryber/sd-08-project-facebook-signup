fa78cd2b6d3c9ccd2d05bcc84b286ea2677c73bd
Erick Santos <erickgaves@gmail.com>
2020-11-25 17:36:59 -0300

    corrigindo requisito 18

--- script.js
+++ script.js
@@ -7,24 +7,3 @@ function buttonEnter() {
 }
 buttonEnter();
 
-function validarForms() {
-  const buttonCadastrar = document.querySelector('#facebook-register');
-  buttonCadastrar.addEventListener('click', event => {
-    event.preventDefault();
-    const selectImputs = document.querySelectorAll('.create-account input');
-    for (let index = 0; index < selectImputs.length; index += 1) {
-      if (selectImputs[index].value === '') {
-        const span = document.createElement('span');
-        span.innerHTML = 'Campos inválidos';
-        span.id = 'invalido';
-        if (document.querySelector('#invalido') === null) {
-          document.querySelector('.creat-account').appendChild(span);
-          break;
-        }
-        break;
-      }
-    }
-
-  });
-}
-validarForms();

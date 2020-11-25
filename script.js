6e1a042c257f63a2e8a248c459393051403d13eb
Samuel <samuelvanguarda@gmail.com>
2020-11-25 20:03:49 -0300

    Correção de Requisitos - Parte 2

--- script.js
+++ script.js
@@ -1,29 +1,50 @@
-
-const buttonSignUp = document.getElementById('facebook-register');
-const missing = document.createElement('p');
-const form = document.querySelector('.form');
+let buttonSignUp = document.getElementById('facebook-register');
+let missing = document.createElement('p');
+let form = document.querySelector('.form');
+let counting = 0;
 const inputCustomGender = document.createElement('input');
-inputCustomGender.name = 'gender-custom';
-inputCustomGender.placeholder = 'Gênero (opcional)';
-inputCustomGender.type = 'text';
-inputCustomGender.id = 'gender';
-const formGender = document.querySelector('.form-gender');
-const optionCustom = document.querySelector('#personal');
+inputCustomGender.name = "gender-custom";
+inputCustomGender.placeholder = "Gênero (opcional)";
+inputCustomGender.type = "text";
+inputCustomGender.id = "gender";
+const formGender = document.querySelector(".form-gender");
+const optionCustom = document.querySelector("#personal");
 const firstName = document.querySelector('.firstName');
 const lastName = document.querySelector('.lastName');
 const email = document.querySelector('.email');
 const birthDate = document.querySelector('.birthdate');
-const textInputs = document.querySelectorAll('.input');
+let textInputs = document.querySelectorAll('.input');
 
-const alertLogin = () => {
-  const emailCelular = document.getElementById('user-email-phone');
-  alert(emailCelular.value);
-};
-alertLogin();
+
+
+/*const alertLogin = () => {
+    const emailCelular = document.getElementById('user-email-phone');
+    alert(emailCelular.value);
+}*/
+
+
+
+const inputRadio = document.querySelectorAll('inputRadio');
+function contagem() {
+  for(let index = 0; index < inputRadio.length; index +=1) {
+    if (inputRadio[index].checked){
+      counting +=1;
+    }
+  }
+}
+
+function checkRadio() {
+  if(counting === 1) {
+    missing.innerHTML = '';
+  }else {
+    missing.innerHTML = 'Campos Inválidos'
+    form.appendChild(missing);
+  }
+}
 
 const validateTextInputs = () => {
-  for (let index = 0; index < textInputs.length; index += 1) {
-    if (textInputs[index].value === '') {
+  for (index = 0; index < textInputs.length; index ++){
+    if (textInputs[index].value === ''){
       missing.innerHTML = 'Campos Inválidos';
       form.appendChild(missing);
     } else {
@@ -34,7 +55,7 @@ const validateTextInputs = () => {
       const pBirthDate = document.createElement('p');
       pBirthDate.innerHTML = birthDate.value;
       const gender = document.createElement('p');
-      gender.innerHTML = document.querySelector('input[type = radio]:checked').value;
+      gender.innerText = document.querySelector('input[type=radio]:checked').value;
       document.querySelector('.main-content').removeChild(document.querySelector('.right-content'));
       const data = document.createElement('div');
       data.className = 'right-content';
@@ -45,13 +66,15 @@ const validateTextInputs = () => {
       data.appendChild(gender);
     }
   }
-};
+}
 
-buttonSignUp.addEventListener('click', function (event) {
+buttonSignUp.addEventListener('click',function(event){
   event.preventDefault();
+  contagem();
+  checkRadio();
   validateTextInputs();
-});
+})
 
-optionCustom.addEventListener('click', function () {
+optionCustom.addEventListener('click', function(){
   formGender.appendChild(inputCustomGender);
-});
+})

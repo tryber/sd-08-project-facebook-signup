27079930d5e0287de7b32386c03239c3ab65cf2f
Maurice Alves <maurice.estevam@gmail.com>
2020-12-21 16:35:45 -0300

    Requisito - 19.

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ const login = document.getElementById('user-email-phone');
 button.addEventListener('click', function () {
   alert(login.value);
 });
+
+/* function checkInputs() {
+  const input = document.querySelectorAll('.checked')
+
+  input.forEach(param => {
+    if (param.value == '') {
+      document.querySelector('p').style.display = 'block'
+    }
+  })
+};
+
+document.querySelector('#facebook-register').addEventListener('click', checkInputs); */
+
+document.querySelector('#personalized').addEventListener('click', () => {
+  const customGender = document.createElement('input');
+  customGender.type = 'text';
+  customGender.name = 'gender-custom';
+  customGender.placeholder = 'Gênero (opcional)';
+  document.querySelector('.gender').appendChild(customGender);
+});
